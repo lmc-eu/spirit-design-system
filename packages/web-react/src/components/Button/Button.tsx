6788a23987ddf881ceb56e94022232523ce5b00c
literat <tomas@litera.me>
2021-09-02 10:24:09 +0200

    DS-41 Refactor: Migrate Button to Typescript

---
+++ packages/web-react/src/components/Button/Button.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import classNames from 'classnames';
+
+type Color = 'primary' | 'secondary' | 'tertiary';
+
+const getButtonColorClassname = (color: Color): string => `lmc-Button--${color}`;
+
+interface ButtonsProps {
+  /**
+   * Emotion color variant.
+   */
+  color: Color;
+  /**
+   * Button label.
+   */
+  label: string;
+  /**
+   * Function to be called on button click.
+   */
+  onClick: () => void;
+  /**
+   * HTML `type` attribute.
+   */
+  type: 'button' | 'submit';
+}
+
+export const Button = ({
+  color,
+  label,
+  onClick,
+  type,
+}: ButtonsProps): JSX.Element => (
+  <button
+    className={classNames('lmc-Button', getButtonColorClassname(color))}
+    onClick={onClick}
+    type={type}
+  >
+    {label}
+  </button>
+);
+
+Button.defaultProps = {
+  color: 'primary',
+  type: 'button',
+};
+
+export default Button;
