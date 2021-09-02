cbf1ced4721d00834d0dd41739f29d7bee15590b
literat <tomas@litera.me>
2021-09-02 17:02:07 +0200

    DS-41 Refactor: Split webpack configuration into specific files
      * use webpack-merge to assemble final configuration

---
+++ packages/web-react/config/webpack.browser.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const webpack = require('webpack');
+const { merge } = require('webpack-merge');
+const BundleAnalyzerPlugin =
+  require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const commonConfig = require('./webpack.common.js');
+
+const libName = 'web-react.browser';
+
+module.exports = (env, argv) =>
+  merge(commonConfig, {
+    output: {
+      // Puts the output at the root of the dist folder
+      path: path.join(__dirname, '../dist/_bundles'),
+      filename: `${libName}${
+        argv.mode === 'production' ? '.umd.min.js' : '.umd.js'
+      }`,
+      libraryTarget: 'umd',
+      umdNamedDefine: true,
+      library: 'webReact',
+    },
+    plugins: [
+      new BundleAnalyzerPlugin({
+        statsFilename: 'browser-stats.json',
+        generateStatsFile: true,
+        analyzerMode: 'disabled',
+      }),
+    ],
+  });
