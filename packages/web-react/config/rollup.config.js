d2b8e800c5e931f03b7a0cc984946932bcda0917
literat <tomas@litera.me>
2021-11-19 14:54:56 +0100

    Chore(web-react): Refactor bundling to make cleaner dist structure
      * with both module and common js exports

---
+++ packages/web-react/config/rollup.config.js
@@ -0,0 +1,134 @@
+import nodeResolve from '@rollup/plugin-node-resolve';
+import { terser as minify } from 'rollup-plugin-terser';
+import path from 'path';
+
+const entryPoints = require('../scripts/entryPoints');
+const distDir = './dist';
+
+function isExternal(id, parentId, entryPointsAreExternal = true) {
+  // Rollup v2.26.8 started passing absolute id strings to this function, thanks
+  // apparently to https://github.com/rollup/rollup/pull/3753, so we relativize
+  // the id again in those cases.
+  if (path.isAbsolute(id)) {
+    const posixId = toPosixPath(id);
+    const posixParentId = toPosixPath(parentId);
+    id = path.posix.relative(
+      path.posix.dirname(posixParentId),
+      posixId,
+    );
+    if (!id.startsWith(".")) {
+      id = "./" + id;
+    }
+  }
+
+  const isRelative =
+    id.startsWith("./") ||
+    id.startsWith("../");
+
+  if (!isRelative) {
+    return true;
+  }
+
+  if (entryPointsAreExternal &&
+      entryPoints.check(id, parentId)) {
+    return true;
+  }
+
+  return false;
+}
+
+// Adapted from https://github.com/meteor/meteor/blob/devel/tools/static-assets/server/mini-files.ts
+function toPosixPath(p) {
+  // Sometimes, you can have a path like \Users\IEUser on windows, and this
+  // actually means you want C:\Users\IEUser
+  if (p[0] === '\\') {
+    p = process.env.SystemDrive + p;
+  }
+
+  p = p.replace(/\\/g, '/');
+  if (p[1] === ':') {
+    // Transform "C:/bla/bla" to "/c/bla/bla"
+    p = '/' + p[0] + p.slice(2);
+  }
+
+  return p;
+}
+
+function prepareCJS(input, output) {
+  return {
+    input,
+    external(id, parentId) {
+      return isExternal(id, parentId, false);
+    },
+    output: {
+      file: output,
+      format: 'cjs',
+      sourcemap: true,
+      exports: 'named',
+      externalLiveBindings: false,
+    },
+    // plugins: [
+    //   nodeResolve(),
+    // ],
+  };
+}
+
+function prepareCJSMinified(input) {
+  return {
+    input,
+    output: {
+      file: input.replace('.cjs', '.min.cjs'),
+      format: 'cjs',
+    },
+    plugins: [
+      minify({
+        mangle: {
+          toplevel: true,
+        },
+        compress: {
+          toplevel: true,
+          global_defs: {
+            '@__DEV__': 'false',
+          },
+        },
+      }),
+    ],
+  };
+}
+
+function prepareBundle({
+  dirs,
+  bundleName = dirs[dirs.length - 1],
+  extensions,
+}) {
+  const dir = path.join(distDir, ...dirs);
+
+  return {
+    input: `${dir}/index.js`,
+    external(id, parentId) {
+      return isExternal(id, parentId, true);
+    },
+    output: {
+      file: `${dir}/${bundleName}.cjs`,
+      format: 'cjs',
+      sourcemap: true,
+      exports: 'named',
+      externalLiveBindings: false,
+    },
+    // plugins: [
+    //   extensions ? nodeResolve({ extensions }) : nodeResolve(),
+    // ],
+  };
+}
+
+export default [
+  ...entryPoints.map(prepareBundle),
+  // Convert the ESM entry point to a single CJS bundle.
+  prepareCJS(
+    './dist/index.js',
+    './dist/spirit-web-react.cjs',
+  ),
+  prepareCJSMinified(
+    './dist/spirit-web-react.cjs',
+  ),
+];
---
+++ packages/web-react/scripts/entryPoints.js
@@ -0,0 +1,94 @@
+const entryPoints = [
+  { dirs: [], bundleName: "main" },
+  { dirs: ['components'] },
+  { dirs: ['components', 'Button'] },
+];
+
+const lookupTrie = Object.create(null);
+entryPoints.forEach(info => {
+  let node = lookupTrie;
+  info.dirs.forEach(dir => {
+    const dirs = node.dirs || (node.dirs = Object.create(null));
+    node = dirs[dir] || (dirs[dir] = { isEntry: false });
+  });
+  node.isEntry = true;
+});
+
+exports.forEach = function(callback, context) {
+  entryPoints.forEach(callback, context);
+};
+
+exports.map = function map(callback, context) {
+  return entryPoints.map(callback, context);
+};
+
+const pathPosix = require("path").posix;
+
+exports.check = function (id, parentId) {
+  const resolved = pathPosix.resolve(pathPosix.dirname(parentId), id);
+  const importedParts = partsAfterDist(resolved);
+
+  if (importedParts) {
+    const entryPointIndex = lengthOfLongestEntryPoint(importedParts);
+    if (entryPointIndex === importedParts.length) {
+      return true;
+    }
+
+    if (entryPointIndex >= 0) {
+      const parentParts = partsAfterDist(parentId);
+      const parentEntryPointIndex = lengthOfLongestEntryPoint(parentParts);
+      const sameEntryPoint =
+        entryPointIndex === parentEntryPointIndex &&
+        arraysEqualUpTo(importedParts, parentParts, entryPointIndex);
+
+      // If the imported ID and the parent ID have the same longest entry
+      // point prefix, then this import is safely confined within that
+      // entry point. Returning false lets Rollup know this import is not
+      // external, and can be bundled into the CJS bundle that we build
+      // for this shared entry point.
+      if (sameEntryPoint) {
+        return false;
+      }
+
+      console.warn(`Risky cross-entry-point nested import of ${id} in ${
+        partsAfterDist(parentId).join("/")
+      }`);
+    }
+  }
+
+  return false;
+};
+
+function partsAfterDist(id) {
+  const parts = id.split(pathPosix.sep);
+  const distIndex = parts.lastIndexOf("dist");
+  if (distIndex >= 0) {
+    return parts.slice(distIndex + 1);
+  }
+}
+
+exports.getEntryPointDirectory = function (file) {
+  const parts = partsAfterDist(file) || file.split(pathPosix.sep);
+  const len = lengthOfLongestEntryPoint(parts);
+  if (len >= 0) return parts.slice(0, len).join(pathPosix.sep);
+};
+
+function lengthOfLongestEntryPoint(parts) {
+  let node = lookupTrie;
+  let longest = -1;
+  for (let i = 0; node && i < parts.length; ++i) {
+    if (node.isEntry) longest = i;
+    node = node.dirs && node.dirs[parts[i]];
+  }
+  if (node && node.isEntry) {
+    return parts.length;
+  }
+  return longest;
+}
+
+function arraysEqualUpTo(a, b, end) {
+  for (let i = 0; i < end; ++i) {
+    if (a[i] !== b[i]) return false;
+  }
+  return true;
+}
