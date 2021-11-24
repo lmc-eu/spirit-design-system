c5b849a9ab3f50ff48c6032cff57bb718204e6d0
Tomáš Janíček <tomas.janicek@lmc.eu>
2021-11-24 14:50:22 +0100

    Connect params into twig

--- src/Compiler/ComponentLexer.php
+++ src/Compiler/ComponentLexer.php
@@ -4,12 +4,24 @@ declare(strict_types=1);
 
 namespace Lmc\TwigComponentsBundle\Compiler;
 
+use Twig\Environment;
 use Twig\Lexer;
 use Twig\Source;
 use Twig\TokenStream;
 
 class ComponentLexer extends Lexer
 {
+    /**
+     * @var string
+     */
+    private $twigPathAlias;
+
+    public function __construct(Environment $env, array $options, string $twigPathAlias)
+    {
+        parent::__construct($env, $options);
+        $this->twigPathAlias = $twigPathAlias;
+    }
+
     public function tokenize(Source $source): TokenStream
     {
         $preparsed = $this->preparse($source->getCode());
@@ -25,6 +37,6 @@ class ComponentLexer extends Lexer
 
     protected function preparse(string $value): string
     {
-        return (new ComponentTagCompiler($value))->compile();
+        return (new ComponentTagCompiler($value, $this->twigPathAlias))->compile();
     }
 }
