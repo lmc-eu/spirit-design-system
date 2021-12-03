e20ffcbc60ac752ea97e3f609e248dbb0133af7a
Tomáš Janíček <tomas.janicek@lmc.eu>
2021-12-03 14:01:47 +0100

    DS-65 Feat: Update react implementation of Button

--- packages/web-react/src/components/Button/Button.tsx
+++ packages/web-react/src/components/Button/Button.tsx
@@ -1,49 +1,78 @@
-import React from 'react';
+import React, { MouseEvent } from 'react';
 import classNames from 'classnames';
+import { WithChildren } from '../../types/main';
 
 type Color = 'primary' | 'secondary' | 'tertiary';
 
 const getButtonColorClassname = (color: Color): string => `Button--${color}`;
 
-export interface ButtonProps {
+export interface ButtonProps extends WithChildren {
   /**
    * Emotion color variant.
    */
   color: Color;
   /**
-   * Button label.
-   */
-  label: string;
-  /**
    * Function to be called on button click.
    */
-  onClick: () => void;
+  onClick?: (event: MouseEvent) => void;
   /**
    * HTML `type` attribute.
    */
   type: 'button' | 'submit';
+  /**
+   * Block alignment modification
+   */
+  block: boolean;
+  /**
+   * Disabling button
+   */
+  disabled: boolean;
+  /**
+   * Aria label
+   */
+  ariaLabel?: string;
 }
 
 export const Button = ({
   color,
-  label,
   onClick,
   type,
-}: ButtonProps): JSX.Element => (
-  /* eslint-disable react/button-has-type */
-  // Because of dynamic assigment in react @see https://github.com/yannickcr/eslint-plugin-react/issues/1555
-  <button
-    className={classNames('Button', getButtonColorClassname(color))}
-    onClick={onClick}
-    type={type}
-  >
-    {label}
-  </button>
-);
+  block,
+  ariaLabel,
+  disabled,
+  ...restAttributes
+}: ButtonProps): JSX.Element => {
+  const handleClick = (event: MouseEvent) => {
+    if (disabled) {
+      event.preventDefault();
+
+      return;
+    }
+
+    if (typeof onClick === 'function') {
+      onClick(event);
+    }
+  };
+
+  return (
+    <button
+      {...restAttributes}
+      className={classNames('Button', getButtonColorClassname(color), {
+        'Button--block': block,
+      })}
+      onClick={handleClick}
+      type={type}
+      disabled={disabled}
+      aria-label={ariaLabel || undefined}
+    />
+  );
+};
 
 Button.defaultProps = {
   color: 'primary',
   type: 'button',
+  block: false,
+  disabled: false,
 };
 
 export default Button;
--- packages/web/src/components/Button/README.md
+++ packages/web/src/components/Button/README.md
@@ -1,3 +1,15 @@
 # Button
 
-Button docs.
+Available variants are:
+* primary
+* secondary
+* tertiary
+
+```html
+<button class="Button Button--primary">Primary Button</button>
+<button class="Button Button--secondary">Secondary Button</button>
+<button class="Button Button--tertiary">Tertiary Button</button>
+```
+
+Create block level buttons—those that span the full width of a parent—by adding `.Button--block`.
+
