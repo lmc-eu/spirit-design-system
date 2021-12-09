d68aff44d582c0f5d697546455fc40a5864df4c6
literat <tomas@litera.me>
2021-12-09 15:14:31 +0100

    Style: Do not let prettier format javascript code
      * use eslint and prettier configuration and reformat code with eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,26 @@
-module.exports = {
+const { prettierConfig } = require('./.prettierrc');
 
+module.exports = {
   parserOptions: {
     ecmaVersion: 'latest',
   },
 
   ignorePatterns: ['node_modules', '!.*.js', 'packages/web-react'],
 
-  extends: ['@lmc-eu/eslint-config-base', '@lmc-eu/eslint-config-base/optional', 'prettier'],
+  extends: [
+    '@lmc-eu/eslint-config-base',
+    '@lmc-eu/eslint-config-base/optional',
+    'prettier',
+  ],
 
   plugins: ['prettier'],
+
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        ...prettierConfig,
+      },
+    ],
+  },
 };
--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,14 @@
 const path = require('path');
 
 module.exports = {
-  stories: ['../packages/**/*.stories.mdx', '../packages/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  stories: [
+    '../packages/**/*.stories.mdx',
+    '../packages/**/*.stories.@(js|jsx|ts|tsx)'
+  ],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials'
+  ],
   webpackFinal: async (config) => {
     config.module.rules.push({
       test: /\.scss$/,
