683e13fa639aa7eaa5c19d5ccdf7485cfbce4a6f
literat <tomas@litera.me>
2021-12-23 16:06:19 +0100

    Style(web-react): Use of @lmc-eu/eslint-config-react

--- packages/web-react/.eslintrc.js
+++ packages/web-react/.eslintrc.js
@@ -1,54 +1,37 @@
 module.exports = {
-  root: true,
-  env: {
-    es6: true,
-    browser: true,
-  },
+  extends: [
+    '../../.eslintrc',
+    '@lmc-eu/eslint-config-react',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'plugin:prettier/recommended',
+  ],
 
   parser: '@typescript-eslint/parser', // the TypeScript parser we installed earlier
 
-  ignorePatterns: ['node_modules', '!.*.js'],
-
   parserOptions: {
+    ecmaVersion: 'latest',
     project: './config/tsconfig.eslint.json',
-    ecmaFeatures: { jsx: true }, // Allows for the parsing of JSX
-    sourceType: 'module',
   },
 
   settings: {
     'import/resolver': {
       node: {
-        extensions: ['.ts', '.tsx'],
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
       },
-    },
-    react: {
-      version: 'detect',
     },
   },
 
-  extends: [
-    '../../.eslintrc.js',
-    'plugin:react/recommended', // eslint react rules (github.com/yannickcr/eslint-plugin-react)
-    'plugin:jsx-a11y/recommended', // accessibility plugin
-    'plugin:prettier/recommended', // prettier plugin and recommended rules
-  ],
-
-  plugins: ['promise', 'react', '@typescript-eslint'],
+  plugins: ['promise', 'react', '@typescript-eslint', 'prettier'],
   rules: {
+    'no-use-before-define': 'off',
     'react/jsx-filename-extension': ['error', { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
-    'react/prop-types': 'off', // We turn off prop-types rule, as we will use TypeScript's types instead.
-    'react/react-in-jsx-scope': 'off',
-    'react/prefer-stateless-function': 'off', // PureComponents ftw.
-    'jsx-a11y/anchor-is-valid': [
-      'error',
-      {
-        components: ['Link'],
-        specialLink: ['to', 'hrefLeft', 'hrefRight'],
-        aspects: ['invalidHref', 'preferButton'],
-      },
-    ],
     'react/jsx-props-no-spreading': 'off', // Used inside HOC, that is fine.
+    'react/function-component-definition': 'off',
+    'react/display-name': 'off',
+    'react/button-has-type': 'off',
     '@typescript-eslint/no-use-before-define': 'warn',
+    '@typescript-eslint/no-var-requires': 'off',
     'import/extensions': 'off',
     'no-plusplus': ['error', { allowForLoopAfterthoughts: true }],
   },
