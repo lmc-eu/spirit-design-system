2fc017fff2ef9f370f166e85e5c2c770cf9858ef
literat <tomas@litera.me>
2022-01-10 12:19:55 +0100

    Test(web-react): Introduce test setup for react components #DS-104

---
+++ packages/web-react/config/jest/config.js
@@ -0,0 +1,25 @@
+const config = {
+  rootDir: '../../',
+  automock: false,
+  setupFiles: ['<rootDir>/config/jest/setup.js'],
+  testPathIgnorePatterns: ['<rootDir>/dist/', '<rootDir>/node_modules/'],
+  coverageDirectory: './.coverage',
+  reporters: [
+    'default',
+    [
+      'jest-junit',
+      {
+        suiteName: 'jest tests',
+        outputDirectory: './.coverage',
+      },
+    ],
+  ],
+  verbose: false,
+  testURL: 'http://localhost/',
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/../../node_modules/babel-jest',
+  },
+};
+
+module.exports = config;
