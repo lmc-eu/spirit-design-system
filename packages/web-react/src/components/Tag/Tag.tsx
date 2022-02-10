2fee37dafc6e13245cb38ea87bb07f87fad51ac8
literat <tomas@litera.me>
2022-02-10 10:41:47 +0100

    Refactor(web-react): Use tag for element definition and constant for component class (refs #DS-104)

--- packages/web-react/src/components/Tag/Tag.tsx
+++ packages/web-react/src/components/Tag/Tag.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { ElementType } from 'react';
 import classNames from 'classnames';
 import { WithChildren } from '../../types/main';
 import { compose } from '../../utils/compose';
@@ -10,31 +10,36 @@ type Color = 'default' | 'informative' | 'success' | 'warning' | 'danger';
 type Theme = 'light' | 'dark';
 
 export interface TagProps extends WithChildren {
+  tag: ElementType;
   color: Color;
   theme: Theme;
   className?: string;
-  componentClassName: string;
 }
 
-// `${componentClassName}--${color}-${theme}`;
-const getTagColorAndThemeClassname = (componentClassName: string, color: Color, theme: Theme): string =>
-  compose(applyTheme<Theme>(theme), applyColor<Color>(color))(componentClassName);
+const defaultProps = {
+  color: 'default',
+  theme: 'dark',
+  tag: 'span',
+};
 
-export const Tag = ({ color, theme, componentClassName, className, children }: TagProps): JSX.Element => {
+// `${componentClassName}--${color}-${theme}`;
+const getTagColorAndThemeClassname = (className: string, color: Color, theme: Theme): string =>
+  compose(applyTheme<Theme>(theme), applyColor<Color>(color))(className);
+
+export const Tag = ({ tag: Tag, color, theme, className, children, ...restProps }: TagProps): JSX.Element => {
+  const tagClass = 'Tag';
   const classNamePrefix = useClassNamePrefix();
-  const mainClassName = applyClassNamePrefix(classNamePrefix)(componentClassName);
+  const mainClassName = applyClassNamePrefix(classNamePrefix)(tagClass);
+
+  const classes = classNames(mainClassName, getTagColorAndThemeClassname(mainClassName, color, theme), className);
 
   return (
-    <span className={classNames(mainClassName, getTagColorAndThemeClassname(mainClassName, color, theme), className)}>
+    <Tag {...restProps} className={classes}>
       {children}
-    </span>
+    </Tag>
   );
 };
 
-Tag.defaultProps = {
-  color: 'default',
-  theme: 'dark',
-  componentClassName: 'Tag',
-};
+Tag.defaultProps = defaultProps;
 
 export default Tag;
