4d7aaac67c8c9e237d814f5ecad93887ee0969d9
Jan Kryšpín <crishpeen@gmail.com>
2022-02-10 16:09:15 +0100

    Chore: Remove doubled stylelint config and add prettier to the correct one

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,6 +1,7 @@
 module.exports = {
   extends: [
     '@lmc-eu/stylelint-config',
-    '@lmc-eu/stylelint-config/optional'
+    '@lmc-eu/stylelint-config/optional',
+    'stylelint-config-prettier'
   ]
 }
