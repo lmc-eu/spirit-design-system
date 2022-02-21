3d29a2467b100de1152ac6badee2d15b624de05d
literat <tomas@litera.me>
2022-02-21 14:45:49 +0100

    Refactor(web-react): Move Grid class building logic to react hook

---
+++ packages/web-react/src/types/grid.ts
@@ -0,0 +1,30 @@
+import { ElementType, JSXElementConstructor } from 'react';
+import { ChildrenProps, StyleProps } from './shared';
+
+export type GridColumns = 1 | 2 | 3 | 4 | 6 | 12;
+export type GridLayouts = 'narrow';
+
+export interface GridElementTypeProps<T extends ElementType = 'div'> {
+  /**
+   * The HTML element or React element used to render the Grid, e.g. 'div'.
+   * @default 'div'
+   */
+  elementType?: T | JSXElementConstructor<any>;
+}
+
+export interface GridCustomLayoutProps {
+  cols?: GridColumns;
+  tablet?: GridColumns;
+  desktop?: GridColumns;
+}
+
+export interface GridLayoutProps {
+  layout?: GridLayouts;
+}
+
+export interface GridProps<T extends ElementType = 'div'>
+  extends GridElementTypeProps<T>,
+    GridCustomLayoutProps,
+    GridLayoutProps {}
+
+export interface SpiritGridProps<T extends ElementType = 'div'> extends GridProps<T>, ChildrenProps, StyleProps {}
