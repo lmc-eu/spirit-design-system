3e8f751601f5d8082c45fb967269c37df73dc40d
literat <tomas@litera.me>
2022-02-23 14:05:03 +0100

    Chore: Do not ignore dependency update commits by commit lint
      * we are not using dependabot anymore

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -3,13 +3,5 @@ module.exports = {
   ignores: [
     (commit) => commit.includes('[ci-skip]'),
     (commit) => commit.includes('Pull request'),
-    /**
-     * Ignore Dependabot commits because they are violating sentence case rule
-     * and cannot be configured or forced to respect commitlint configuration.
-     *
-     * @see https://github.com/dependabot/dependabot-core/issues/1666
-     * @see https://github.com/dependabot/dependabot-core/issues/406
-     */
-    (commit) => commit.includes('Deps: Bump'),
   ],
 };
