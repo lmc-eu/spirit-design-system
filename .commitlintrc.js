f373894024a34de6385aacdf33e20f9accf8c864
literat <tomas@litera.me>
2022-02-23 14:12:24 +0100

    Chore: Introduce scope enum rule for commit lint

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -4,4 +4,20 @@ module.exports = {
     (commit) => commit.includes('[ci-skip]'),
     (commit) => commit.includes('Pull request'),
   ],
+  rules: {
+    'scope-enum': [
+      1,
+      'always',
+      [
+        // Use when committing changes/additions/removals to exact package
+        'design-tokens',
+        'web',
+        'web-react',
+        // Use when affecting CI process
+        'ci',
+        // Use for anything that does not directly affect packages, ie. updating repo-wide
+        'repo',
+      ],
+    ],
+  },
 };
