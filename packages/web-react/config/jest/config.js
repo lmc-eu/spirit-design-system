cc31b075386374611a7d36ff46167bc4c38408df
literat <tomas@litera.me>
2022-02-25 13:50:06 +0100

    Test(web-react): Set absolute path for lcov reporter
      * remove unused coverage reporters
      * add links to jest documentation

--- packages/web-react/config/jest/config.js
+++ packages/web-react/config/jest/config.js
@@ -1,10 +1,42 @@
 const config = {
+  // The root directory that Jest should scan for tests and modules within.
+  // https://jestjs.io/docs/configuration#rootdir-string
   rootDir: '../../',
+
+  // This option tells Jest that all imported modules in your tests should be mocked automatically.
+  // https://jestjs.io/docs/configuration#automock-boolean
   automock: false,
+
+  // Indicates whether each individual test should be reported during the run.
+  // https://jestjs.io/docs/configuration#verbose-boolean
+  verbose: false,
+
+  // A list of paths to modules that run some code to configure or set up the testing environment.
+  // https://jestjs.io/docs/configuration#setupfiles-array
   setupFiles: ['<rootDir>/config/jest/setup.js'],
+
+  // A list of paths to modules that run some code to configure or set up the testing framework before each test file in the suite is executed.
+  // https://jestjs.io/docs/configuration#setupfilesafterenv-array
   setupFilesAfterEnv: ['<rootDir>/config/jest/setupTestingLibrary.js'],
+
+  // A map from regular expressions to paths to transformers
+  // https://jestjs.io/docs/configuration#transform-objectstring-pathtotransformer--pathtotransformer-object
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/../../node_modules/babel-jest',
+  },
+
+  // The test environment that will be used for testing.
+  // https://jestjs.io/docs/configuration#testenvironment-string
+  testEnvironment: 'jsdom',
+
+  // An array of regexp pattern strings that are matched against all test paths before executing the test
+  // https://jestjs.io/docs/configuration#testpathignorepatterns-arraystring
   testPathIgnorePatterns: ['<rootDir>/dist/', '<rootDir>/node_modules/'],
+
+  // The directory where Jest should output its coverage files.
+  // https://jestjs.io/docs/configuration#coveragedirectory-string
   coverageDirectory: './.coverage',
+
   // An array of glob patterns indicating a set of files for which coverage information should be collected.
   // https://jestjs.io/docs/configuration#collectcoveragefrom-array
   collectCoverageFrom: ['<rootDir>/src/**/*.{js,jsx,ts,tsx}', '!<rootDir>/src/**/*.d.ts'],
@@ -12,22 +44,10 @@ const config = {
   // An array of regexp pattern strings that are matched against all file paths before executing the test.
   // https://jestjs.io/docs/configuration#coveragepathignorepatterns-arraystring
   coveragePathIgnorePatterns: ['__fixtures__', '.*.stories.*'],
-  reporters: [
-    'default',
-    [
-      'jest-junit',
-      {
-        suiteName: 'jest tests',
-        outputDirectory: './.coverage',
-      },
-    ],
-  ],
-  verbose: false,
-  testURL: 'http://localhost/',
-  testEnvironment: 'jsdom',
-  transform: {
-    '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/../../node_modules/babel-jest',
-  },
+
+  // A list of reporter names that Jest uses when writing coverage reports. Any istanbul reporter can be used.
+  // https://jestjs.io/docs/configuration#coveragereporters-arraystring--string-options
+  coverageReporters: ['text', 'text-summary', ['lcov', { projectRoot: '../../' }]],
 };
 
 module.exports = config;
