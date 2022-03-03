d1bb3b1bb99de35fc6ef518882f4df360c80d1c0
Jan Kryšpín <crishpeen@gmail.com>
2022-03-03 17:10:13 +0100

    Feat(exporter-scss): Change typography generation, based on breakpoints

--- exporters/scss/src/js/functions.js
+++ exporters/scss/src/js/functions.js
@@ -188,13 +188,22 @@ Pulsar.registerFunction("generateSimple", function(tokens, groups = {}, sortByNu
   return `${vars.join('\n')}\n${typesPrint}`;
 });
 
-Pulsar.registerFunction("generateTypography", function(tokens = [], defaultFontSize, fontFamilyFallback) {
-  const vars = [];
-  tokens.map((token) => {   
+Pulsar.registerFunction("generateTypography", function(tokens = [], defaultFontSize, fontFamilyFallback, breakpointsString = '') {
+  const breakpoints = breakpointsString.trim().split(',');
+  const styles = {};
+  tokens.map((token) => {
     let name = cleanName(token.name);
     if (token.origin) {
       name = cleanName(token.origin.name);
     }
+    let nameWithoutBreakpoint = name;
+    let breakpoint = breakpoints[0];
+    breakpoints.map((bp) => {
+      if (nameWithoutBreakpoint.includes(bp)) {
+        breakpoint = bp;
+        nameWithoutBreakpoint = nameWithoutBreakpoint.replace(`-${bp}`, '');
+      }
+    });
     const subfamily = token.value.font.subfamily.toLowerCase();
     const fontSize = printUnit(Math.round((token.value.fontSize.measure / defaultFontSize) * 1000) / 1000, 'rem');
     let fontStyle = 'normal';
@@ -209,20 +218,52 @@ Pulsar.registerFunction("generateTypography", function(tokens = [], defaultFontS
     const textDecoration = token.value.textDecoration.toLowerCase();
     const paragraphIndent = printUnit(token.value.paragraphIndent.measure, token.value.paragraphIndent.unit);
     const textTransform = token.value.textCase === 'Original' ? 'none' : token.value.textCase.toLowerCase()
-
-    const printLetterSpacing = letterSpacing !== 0 ? `\n    letter-spacing: ${letterSpacing},` : '';
-    const printTextDecoration = textDecoration !== 'none' ? `\n    text-decoration: ${textDecoration},` : '';
-    const printParagraphIndent = paragraphIndent !== 0 ? `\n    text-indent: ${paragraphIndent},` : '';
-    const printTextTransform = textTransform !== 'none' ? `\n    text-transform: ${textTransform},` : '';
-
-    vars.push(`$${name}: (
-    font-family: "'${token.value.font.family}'${fontFamilyFallback}",
-    font-size: ${fontSize},
-    font-style: ${fontStyle},
-    font-weight: ${getWeight(fontWeight)},
-    line-height: ${lineHeight},${printLetterSpacing}${printTextDecoration}${printParagraphIndent}${printTextTransform}
+    const tokenVals = {
+      fontFamily: `'${token.value.font.family}'${fontFamilyFallback}`,
+      fontSize,
+      fontStyle,
+      fontWeight: getWeight(fontWeight),
+      lineHeight,
+      letterSpacing,
+      textDecoration,
+      paragraphIndent,
+      textTransform,
+    };
+    if (styles[nameWithoutBreakpoint] !== undefined) {
+      styles[nameWithoutBreakpoint][breakpoint] = tokenVals;
+    } else {
+      styles[nameWithoutBreakpoint] = {
+        [breakpoint]: tokenVals,
+      };
+    }
+  });
+  const vars = [];
+  const list = [];
+  Object.entries(styles).forEach(([styleName, styleBreakpoints]) => {
+    list.push(`${styleName}: $${styleName}`)
+    const breakpointValues = [];
+    breakpoints.map((breakpoint) => {
+      const breakpointVal = styleBreakpoints[breakpoint];
+      if(breakpointVal !== undefined) {
+        const printLetterSpacing = breakpointVal.letterSpacing !== 0 ? `\n    letter-spacing: ${breakpointVal.letterSpacing},` : '';
+        const printTextDecoration = breakpointVal.textDecoration !== 'none' ? `\n    text-decoration: ${breakpointVal.textDecoration},` : '';
+        const printParagraphIndent = breakpointVal.paragraphIndent !== 0 ? `\n    text-indent: ${breakpointVal.paragraphIndent},` : '';
+        const printTextTransform = breakpointVal.textTransform !== 'none' ? `\n    text-transform: ${breakpointVal.textTransform},` : '';
+        breakpointValues.push(`${breakpoint}: (
+        font-family: ${breakpointVal.fontFamily},
+        font-size: ${breakpointVal.fontSize},
+        font-style: ${breakpointVal.fontStyle},
+        font-weight: ${breakpointVal.fontWeight},
+        line-height: ${breakpointVal.lineHeight},${printLetterSpacing}${printTextDecoration}${printParagraphIndent}${printTextTransform}
+    ),`);
+      }
+    })
+    vars.push(`$${styleName}: (
+    ${breakpointValues.join('\n    ')}
 ) !default;\n`);
   });
-  
-  return vars.join('\n');
+  const listPrint = `$styles: (
+    ${list.join(',\n    ')}
+) !default;`
+  return `${vars.join('\n')}\n${listPrint}`;
 });
