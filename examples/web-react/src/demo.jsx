944b54de31a9ce9634998a01e2f70027540e9b17
Adam Kudrna <adam.kudrna@lmc.eu>
2022-03-04 19:16:45 +0100

    Feat(web): Introduce foundation CSS layer #DS-173

--- examples/web-react/src/demo.jsx
+++ examples/web-react/src/demo.jsx
@@ -1,25 +1,28 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Button } from '@lmc-eu/spirit-web-react/components/Button';
+import { Container } from '@lmc-eu/spirit-web-react/components/Container';
 
 const Demo = () => (
   <section>
-    <h2>Button</h2>
-    <Button color="primary" onClick={() => console.log('Primary button clicked!')}>
-      Primary button
-    </Button>
-    <Button color="secondary" onClick={() => console.log('Secondary button clicked!')}>
-      Secondary Button
-    </Button>
-    <Button color="tertiary" onClick={() => console.log('Tertiary button clicked!')}>
-      Tertiary button
-    </Button>
-    <Button color="inverted" onClick={() => console.log('Inverted button clicked!')}>
-      Inverted button
-    </Button>
-    <Button color="danger" onClick={() => console.log('Danger button clicked!')}>
-      Danger button
-    </Button>
+    <Container>
+      <h2 className="typography-heading-medium-text">Button</h2>
+      <Button color="primary" onClick={() => console.log('Primary button clicked!')}>
+        Primary button
+      </Button>
+      <Button color="secondary" onClick={() => console.log('Secondary button clicked!')}>
+        Secondary Button
+      </Button>
+      <Button color="tertiary" onClick={() => console.log('Tertiary button clicked!')}>
+        Tertiary button
+      </Button>
+      <Button color="inverted" onClick={() => console.log('Inverted button clicked!')}>
+        Inverted button
+      </Button>
+      <Button color="danger" onClick={() => console.log('Danger button clicked!')}>
+        Danger button
+      </Button>
+    </Container>
   </section>
 );
 
