57c63f8084e4fb3d7891113e64811e636236554c
literat <tomas@litera.me>
2022-03-10 15:29:45 +0100

    Docs(web-react): Introduce missing button variants in storybook

--- packages/web-react/src/components/Button/Button.stories.tsx
+++ packages/web-react/src/components/Button/Button.stories.tsx
@@ -23,10 +23,53 @@ export default {
 
 const Template = (args: SpiritButtonProps<'button'>) => <Button {...args} />;
 
-export const ExampleButton = Template.bind({});
-ExampleButton.args = {
+export const PrimaryButton = Template.bind({});
+PrimaryButton.args = {
   color: 'primary',
   children: 'Button',
-  disabled: false,
-  block: false,
+};
+
+export const SecondaryButton = Template.bind({});
+SecondaryButton.args = {
+  color: 'secondary',
+  children: 'Button',
+};
+
+export const TertiaryButton = Template.bind({});
+TertiaryButton.args = {
+  color: 'tertiary',
+  children: 'Button',
+};
+
+export const DangerButton = Template.bind({});
+DangerButton.args = {
+  color: 'danger',
+  children: 'Button',
+};
+
+export const InvertedButton = Template.bind({});
+InvertedButton.args = {
+  color: 'inverted',
+  children: 'Button',
+};
+
+export const DisabledButton = Template.bind({});
+DisabledButton.args = {
+  color: 'primary',
+  children: 'Button',
+  disabled: true,
+};
+
+export const BlockButton = Template.bind({});
+BlockButton.args = {
+  color: 'primary',
+  children: 'Button',
+  block: true,
+};
+
+export const SquareButton = Template.bind({});
+SquareButton.args = {
+  color: 'primary',
+  children: '🚀',
+  isSquare: true,
 };
