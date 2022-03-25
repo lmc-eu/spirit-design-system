4c114ba8f925f9e911b5c33879390e54c40adb82
literat <tomas@litera.me>
2022-03-25 15:26:07 +0100

    Docs(web-react): Introduce Login Form example in storybook

---
+++ packages/web-react/docs/stories/examples/LoginForm.stories.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { TextField, CheckboxField, Button, Container, Grid, Stack } from '../../../src/components';
+
+export default {
+  title: 'Examples/Forms',
+};
+
+export const LoginForm = () => (
+  <Container>
+    <Grid layout="narrow">
+      <Stack>
+        <TextField type="text" id="name" label="Name" isFluid />
+        <TextField type="password" id="password" label="Password" isFluid />
+        <CheckboxField label="Stay Logged In" />
+        <Button isBlock>Login</Button>
+      </Stack>
+    </Grid>
+  </Container>
+);
