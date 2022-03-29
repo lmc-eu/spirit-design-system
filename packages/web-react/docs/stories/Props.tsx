02f961dcf1bc12915888bc94815f8a37d125ed2d
literat <tomas@litera.me>
2022-03-29 10:37:37 +0200

    Docs(web-react): Introduce more detailed Alert documentation
      * using examples and props helper
      * add utilities and helper classes to assets

---
+++ packages/web-react/docs/stories/Props.tsx
@@ -0,0 +1,35 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import React, { ComponentType } from 'react';
+import { nanoid } from 'nanoid';
+import { ArgsTable } from '@storybook/addon-docs';
+
+interface InfoProps {
+  component: ComponentType<any>;
+}
+
+const Info = ({ component }: InfoProps) => (
+  <div>
+    <h5>{component.name}</h5>
+    <ArgsTable of={component} />
+  </div>
+);
+
+interface PropsProps {
+  component: ComponentType<any>;
+  components?: ComponentType<any>[];
+}
+
+const Props = ({ component, components }: PropsProps) => (
+  <div>
+    {component ? <Info component={component} /> : undefined}
+    {components.map((componentItem, index) => (
+      <Info key={nanoid()} component={componentItem} />
+    ))}
+  </div>
+);
+
+Props.defaultProps = {
+  components: [],
+};
+
+export default Props;
---
+++ packages/web-react/src/components/Alert/stories/AlertProps.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { Alert } from '../Alert';
+import Props from '../../../../docs/stories/Props';
+
+const Story = () => <Props component={Alert} />;
+
+export default Story;
