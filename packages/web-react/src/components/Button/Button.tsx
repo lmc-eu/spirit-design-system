53c1d9f1feacb78142623761089e545a59f766f6
literat <tomas@litera.me>
2022-03-29 11:38:22 +0200

    Style(web-react): Remove unused prop

--- packages/web-react/src/components/Button/Button.tsx
+++ packages/web-react/src/components/Button/Button.tsx
@@ -14,7 +14,7 @@ const defaultProps = {
 };
 
 export const Button = <T extends ElementType = 'button'>(props: SpiritButtonProps<T>): JSX.Element => {
-  const { elementType: ElementTag = 'button', className, children, ...restProps } = props;
+  const { elementType: ElementTag = 'button', children, ...restProps } = props;
   const { buttonProps } = useButtonAriaProps(props);
   const { classProps } = useButtonStyleProps(restProps);
 
