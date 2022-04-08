181725ac2adf4687b4f84972ed3f1b09a481af23
literat <tomas@litera.me>
2022-04-08 17:11:12 +0200

    Test(web-react): Suppress annoying warning about new React render API (refs #258)

--- packages/web-react/config/jest/setupTestingLibrary.js
+++ packages/web-react/config/jest/setupTestingLibrary.js
@@ -1,1 +1,17 @@
+/* eslint-disable no-console */
 import '@testing-library/jest-dom';
+
+const originalError = console.error;
+
+beforeAll(() => {
+  console.error = (...args) => {
+    if (/Warning: ReactDOM.render is no longer supported in React 18./.test(args[0])) {
+      return;
+    }
+    originalError.call(console, ...args);
+  };
+});
+
+afterAll(() => {
+  console.error = originalError;
+});
