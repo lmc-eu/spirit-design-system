b2a35c238185bc022a03fc9eb58d011917d954ec
literat <tomas@litera.me>
2022-04-29 09:55:02 +0200

    Fix(web-react): Add missing default prop size for Heading (refs #277)

--- packages/web-react/src/components/Heading/Heading.tsx
+++ packages/web-react/src/components/Heading/Heading.tsx
@@ -3,6 +3,10 @@ import { useHeadingStyleProps } from './useHeadingStyleProps';
 import { SpiritHeadingProps } from '../../types';
 import { filterProps } from '../../utils/filterProps';
 
+const defaultProps = {
+  size: 'medium',
+};
+
 export const Heading = <T extends ElementType = 'div'>(props: SpiritHeadingProps<T>): JSX.Element => {
   const { elementType: ElementTag = 'div', children, ...restProps } = props;
   const { classProps, props: modifiedProps } = useHeadingStyleProps(restProps);
@@ -13,5 +17,7 @@ export const Heading = <T extends ElementType = 'div'>(props: SpiritHeadingProps
     </ElementTag>
   );
 };
+
+Heading.defaultProps = defaultProps;
 
 export default Heading;
