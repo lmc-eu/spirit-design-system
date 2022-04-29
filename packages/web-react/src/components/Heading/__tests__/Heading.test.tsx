ef9fef8924fa06e16f249514085cac7529689668
literat <tomas@litera.me>
2022-04-29 10:03:05 +0200

    Test(web-react): Refactor Heading component test to use test provider
      * we have dedicated test provider for testing prefixes

--- packages/web-react/src/components/Heading/__tests__/Heading.test.tsx
+++ packages/web-react/src/components/Heading/__tests__/Heading.test.tsx
@@ -2,24 +2,16 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import Heading from '../Heading';
-import { ClassNamePrefixProvider } from '../../../context/ClassNamePrefixContext';
 import headingSizeDataProvider from './headingSizeDataProvider';
 import { HeadingSize } from '../../../types';
+import { classNamePrefixProviderTest } from '../../../../tests/providerTests/classNamePrefixProviderTest';
 
 describe('Heading', () => {
+  classNamePrefixProviderTest(Heading, 'typography-heading-medium-text');
+
   it.each(headingSizeDataProvider)('should have for size %s an expected class %s', (size, expectedClassName) => {
     const dom = render(<Heading size={size as HeadingSize} />);
 
     expect(dom.container.querySelector('div')).toHaveClass(expectedClassName);
-  });
-
-  it('should have classname with lmc prefix', () => {
-    const dom = render(
-      <ClassNamePrefixProvider value="lmc">
-        <Heading size="xlarge" />
-      </ClassNamePrefixProvider>,
-    );
-
-    expect(dom.container.querySelector('div')).toHaveClass('lmc-typography-heading-xlarge-text');
   });
 });
