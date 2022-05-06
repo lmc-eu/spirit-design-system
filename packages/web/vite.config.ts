d4feb8bfe2eade8531b09c35989ee7593e6aa987
literat <tomas@litera.me>
2022-05-06 12:33:35 +0200

    Chore(web): Create and extend layout template in component examples
      * using Handlebars and vite plugin

--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
+import handlebars from 'vite-plugin-handlebars';
 
 export default defineConfig({
+  plugins: [
+    handlebars({
+      partialDirectory: resolve(__dirname, 'partials'),
+    }),
+  ],
   css: {
     preprocessorOptions: {
       scss: {
