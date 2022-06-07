51a962c4c8439d05dacf32c1d7333303e057187c
literat <tomas@litera.me>
2022-06-07 16:27:56 +0200

    Feat(web): Introduce Javascript plugin for Tabs component (refs #DS-231)

--- packages/web/src/js/index.esm.ts
+++ packages/web/src/js/index.esm.ts
@@ -1,3 +1,4 @@
 export { default as Header } from './Header';
 export { default as Modal } from './Modal';
 export { default as Password } from './Password';
+export { default as Tabs } from './Tabs';
--- packages/web/src/js/index.umd.ts
+++ packages/web/src/js/index.umd.ts
@@ -1,5 +1,6 @@
 import Header from './Header';
 import Modal from './Modal';
 import Password from './Password';
+import Tabs from './Tabs';
 
-export default { Header, Modal, Password };
+export default { Header, Modal, Password, Tabs };
