fb3ffafb013227e95af0fddd8b96c00513c4160b
literat <tomas@litera.me>
2022-07-28 13:26:52 +0200

    Feat(web-twig): Enable glob function pattersn in a paths

--- packages/web-twig/src/DependencyInjection/CompilerPass/OverrideServiceCompilerPass.php
+++ packages/web-twig/src/DependencyInjection/CompilerPass/OverrideServiceCompilerPass.php
@@ -8,6 +8,7 @@ use Lmc\SpiritWebTwigBundle\DependencyInjection\SpiritWebTwigExtension;
 use Lmc\TwigXBundle\Compiler\ComponentLexer;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
 use Symfony\Component\DependencyInjection\Reference;
 
 class OverrideServiceCompilerPass implements CompilerPassInterface
@@ -29,24 +30,33 @@ class OverrideServiceCompilerPass implements CompilerPassInterface
         $iconsPaths = $container->getParameter(SpiritWebTwigExtension::PARAMETER_ICONS_PATHS);
         $iconsPathAlias = $container->getParameter(SpiritWebTwigExtension::PARAMETER_ICONS_PATH_ALIAS);
 
-        $twigLoaderDefinition->addMethodCall('addPath', [SpiritWebTwigExtension::DEFAULT_PARTIALS_PATH, SpiritWebTwigExtension::DEFAULT_PARTIALS_ALIAS]);
+        $this->addGlobPath($twigLoaderDefinition, SpiritWebTwigExtension::DEFAULT_PARTIALS_PATH, SpiritWebTwigExtension::DEFAULT_PARTIALS_ALIAS);
 
         foreach ($paths as $path) {
-            $twigLoaderDefinition->addMethodCall('addPath', [$path, $pathAlias]);
+            $this->addGlobPath($twigLoaderDefinition, $path, $pathAlias);
 
             if ($path === SpiritWebTwigExtension::DEFAULT_COMPONENTS_PATH) {
-                $twigLoaderDefinition->addMethodCall('addPath', [$path, SpiritWebTwigExtension::DEFAULT_PATH_ALIAS]);
+                $this->addGlobPath($twigLoaderDefinition, $path, SpiritWebTwigExtension::DEFAULT_PATH_ALIAS);
             }
         }
 
         foreach ($iconsPaths as $iconPath) {
-            $twigLoaderDefinition->addMethodCall('addPath', [$iconPath, $iconsPathAlias]);
+            $this->addGlobPath($twigLoaderDefinition, $iconPath, $iconsPathAlias);
         }
 
         $twigDefinition->addMethodCall('addGlobal', [self::GLOBAL_PREFIX_TWIG_VARIABLE, $classPrefix]);
 
         if ($isLexer) {
             $twigDefinition->addMethodCall('setLexer', [new Reference(ComponentLexer::class)]);
+        }
+    }
+
+    private function addGlobPath(Definition $twigLoaderDefinition, string $path, string $alias): void
+    {
+        if ($paths = glob($path, GLOB_ONLYDIR)) {
+            foreach ($paths as $path) {
+                $twigLoaderDefinition->addMethodCall('addPath', [$path, $alias]);
+            }
         }
     }
 }
