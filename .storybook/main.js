08043d516c62b190dbf5533d95a4ac474257b450
literat <tomas@litera.me>
2022-08-24 16:04:46 +0200

    Docs(web-react): Enable SVG React components in Storybook using SVGR

--- .storybook/main.js
+++ .storybook/main.js
@@ -41,6 +41,21 @@ module.exports = {
       },
     });
 
+    /**
+     * Enable import of SVG files like React Components
+     * eg.: import Icon from 'icon.svg'
+     * and use as <Icon />
+     */
+    // Default rule for images /\.(svg|ico|jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|cur|ani|pdf)(\?.*)?$/
+    const fileLoaderRule = config.module.rules.find(rule => rule.test && rule.test.test('.svg'));
+    fileLoaderRule.exclude = /\.svg$/;
+
+    config.module.rules.unshift({
+      test: /\.svg$/,
+      enforce: 'pre',
+      use: ['@svgr/webpack'],
+    });
+
     return config;
   },
 };
