372e80c53bdefcf5423f590d46ab05448b0fc027
literat <tomas@litera.me>
2022-08-29 10:06:59 +0200

    Test(web-react): Allow prop tests to render async components
      * testing lazy component in Icon

--- packages/web-react/tests/providerTests/classNamePrefixProviderTest.tsx
+++ packages/web-react/tests/providerTests/classNamePrefixProviderTest.tsx
@@ -1,11 +1,11 @@
-import { render } from '@testing-library/react';
+import { render, waitFor } from '@testing-library/react';
 import React, { ComponentType } from 'react';
 import { ClassNamePrefixProvider } from '../../src/context/ClassNamePrefixContext';
 import getElement from '../testUtils/getElement';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const classNamePrefixProviderTest = (Component: ComponentType<any>, className: string, testId?: string) => {
-  it('renders with class name prefix', () => {
+  it('renders with class name prefix', async () => {
     const prefix = 'lmc';
     const dom = render(
       <ClassNamePrefixProvider value={prefix}>
@@ -13,14 +13,18 @@ export const classNamePrefixProviderTest = (Component: ComponentType<any>, class
       </ClassNamePrefixProvider>,
     );
 
-    const element = getElement(dom, testId);
-    expect(element).toHaveClass(`${prefix}-${className}`);
+    await waitFor(() => {
+      const element = getElement(dom, testId);
+      expect(element).toHaveClass(`${prefix}-${className}`);
+    });
   });
 
-  it('renders without class name prefix', () => {
+  it('renders without class name prefix', async () => {
     const dom = render(<Component />);
 
-    const element = getElement(dom, testId);
-    expect(element).toHaveClass(className);
+    await waitFor(() => {
+      const element = getElement(dom, testId);
+      expect(element).toHaveClass(className);
+    });
   });
 };
--- packages/web-react/tests/providerTests/restPropsTest.tsx
+++ packages/web-react/tests/providerTests/restPropsTest.tsx
@@ -1,13 +1,15 @@
-import { render } from '@testing-library/react';
+import { render, waitFor } from '@testing-library/react';
 import React, { ComponentType } from 'react';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const restPropsTest = (Component: ComponentType<any>, selector: string) => {
-  it('should pass rest props to main element', () => {
+  it('should pass rest props to main element', async () => {
     const testId = 'testRestProp';
     const dom = render(<Component data-testid={testId} />);
 
-    const element = dom.container.querySelector(selector) as HTMLElement;
-    expect(element).toHaveAttribute('data-testid', testId);
+    await waitFor(() => {
+      const element = dom.container.querySelector(selector) as HTMLElement;
+      expect(element).toHaveAttribute('data-testid', testId);
+    });
   });
 };
