a3f4256de785a149370d074cba52ef05915adf04
Adam Kudrna <adam.kudrna@lmc.eu>
2022-09-15 19:20:19 +0200

    BREAKING CHANGES(web-react): Do not provide non-existing CSS class `Tabs-content` in `Tabs` hook
    Migration: Delete `content` class name provided by the `useTabsStyleProps` hook.
    There is no styling applied to it.

--- packages/web-react/src/components/Tabs/TabContent.tsx
+++ packages/web-react/src/components/Tabs/TabContent.tsx
@@ -1,13 +1,8 @@
 import React from 'react';
 import { ChildrenProps, TransferProps } from '../../types';
-import { useTabsStyleProps } from './useTabsStyleProps';
 
 type TabContentProps = ChildrenProps & TransferProps;
 
-const TabContent = ({ children }: TabContentProps): JSX.Element => {
-  const { classProps } = useTabsStyleProps();
-
-  return <div className={classProps.content}>{children}</div>;
-};
+const TabContent = ({ children }: TabContentProps): JSX.Element => <div>{children}</div>;
 
 export default TabContent;
--- packages/web-react/src/components/Tabs/__tests__/TabContent.test.tsx
+++
@@ -1,7 +0,0 @@
-import '@testing-library/jest-dom';
-import { classNamePrefixProviderTest } from '../../../../tests/providerTests/classNamePrefixProviderTest';
-import TabContent from '../TabContent';
-
-describe('Tab', () => {
-  classNamePrefixProviderTest(TabContent, 'Tabs-content');
-});
