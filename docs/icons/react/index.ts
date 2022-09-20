d14220999c01888c0a4c7caa28d744253c4d748e
crishpeen <crishpeen@gmail.com>
2022-09-20 13:25:25 +0000

    Deploying to gh-pages from @ lmc-eu/spirit-design-system@4ef2756f2ebf20c03e53cad84932f3b8c88d4992 🚀

--- docs/icons/react/index.ts
+++ docs/icons/react/index.ts
@@ -1,10 +1,7 @@
 /* eslint-disable */
 /* tslint:disable */
 export {default as AddIcon} from './AddIcon';
-export {default as CheckCheckedIcon} from './CheckCheckedIcon';
-export {default as CheckIndeterminateIcon} from './CheckIndeterminateIcon';
 export {default as CheckPlainIcon} from './CheckPlainIcon';
-export {default as CheckUncheckedIcon} from './CheckUncheckedIcon';
 export {default as ChevronDownIcon} from './ChevronDownIcon';
 export {default as ChevronLeftIcon} from './ChevronLeftIcon';
 export {default as ChevronRightIcon} from './ChevronRightIcon';
@@ -16,13 +13,9 @@ export {default as InfoIcon} from './InfoIcon';
 export {default as LinkIcon} from './LinkIcon';
 export {default as MoreIcon} from './MoreIcon';
 export {default as ProfileIcon} from './ProfileIcon';
-export {default as RadioCheckedIcon} from './RadioCheckedIcon';
-export {default as RadioUncheckedIcon} from './RadioUncheckedIcon';
 export {default as ReloadIcon} from './ReloadIcon';
 export {default as SearchIcon} from './SearchIcon';
 export {default as SpriteIcon} from './SpriteIcon';
-export {default as ToggleOffIcon} from './ToggleOffIcon';
-export {default as ToggleOnIcon} from './ToggleOnIcon';
 export {default as VisibilityOffIcon} from './VisibilityOffIcon';
 export {default as VisibilityOnIcon} from './VisibilityOnIcon';
 export {default as WarningIcon} from './WarningIcon';
