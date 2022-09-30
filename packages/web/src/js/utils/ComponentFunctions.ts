8fb6503b3c7da89493b7f7cc8e7def530f6009a4
literat <tomas@litera.me>
2022-09-30 15:29:04 +0200

    Refactor(web): Compose toggle triggers from event handlers

--- packages/web/src/js/utils/ComponentFunctions.ts
+++ packages/web/src/js/utils/ComponentFunctions.ts
@@ -8,43 +8,47 @@ type DataTriggerAttribute = 'data-toggle' | 'data-dismiss';
 const ATTRIBUTE_DATA_TOGGLE = `data-toggle`;
 const ATTRIBUTE_DATA_DISMISS = `data-dismiss`;
 
+const onClickHandler = (element: HTMLElement, component: typeof BaseComponent, method: string, event: Event) => {
+  EventHandler.on(element, 'click', function handleClick() {
+    const target = getElement(this);
+    const instance = component.getOrCreateInstance(target);
+
+    // No index signature with a parameter of type 'string' was found on type 'Document | HTMLElement | Window | BaseComponent'
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    instance[method](target, event);
+  });
+};
+
+const onLoadHandler = (element: HTMLElement, component: typeof BaseComponent) => {
+  component.getOrCreateInstance(element);
+};
+
 const enableDataTrigger = (
   dataTriggerAttribute: DataTriggerAttribute,
   component: typeof BaseComponent,
+  eventHandler: (element: HTMLElement, component: typeof BaseComponent, method: string, event: Event) => void,
   method = 'toggle',
 ) => {
   const name = component.NAME;
 
   EventHandler.on(window, 'DOMContentLoaded', (event: Event) => {
     SelectorEngine.findAll(`[${dataTriggerAttribute}="${name}"]`).forEach((toggleEl) => {
-      EventHandler.on(toggleEl, 'click', function handleClick() {
-        const target = getElement(this);
-        const instance = component.getOrCreateInstance(target);
-
-        // No index signature with a parameter of type 'string' was found on type 'Document | HTMLElement | Window | BaseComponent'
-        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-        // @ts-ignore
-        instance[method](target, event);
-      });
-    });
-  });
-};
-
-const instanceAutoloader = (dataAttributeKey: string, component: typeof BaseComponent) => {
-  const name = component.NAME;
-  EventHandler.on(window, 'DOMContentLoaded', () => {
-    SelectorEngine.findAll(`[data-${dataAttributeKey}="${name}"]`).forEach((element) => {
-      component.getOrCreateInstance(element);
+      eventHandler(toggleEl, component, method, event);
     });
   });
 };
 
 const enableToggleTrigger = (component: typeof BaseComponent, method = 'toggle') => {
-  enableDataTrigger(ATTRIBUTE_DATA_TOGGLE, component, method);
+  enableDataTrigger(ATTRIBUTE_DATA_TOGGLE, component, onClickHandler, method);
 };
 
 const enableDismissTrigger = (component: typeof BaseComponent, method = 'dismiss') => {
-  enableDataTrigger(ATTRIBUTE_DATA_DISMISS, component, method);
+  enableDataTrigger(ATTRIBUTE_DATA_DISMISS, component, onClickHandler, method);
 };
 
-export { enableToggleTrigger, enableDismissTrigger, instanceAutoloader };
+const enableToggleAutoloader = (component: typeof BaseComponent, method = 'toggle') => {
+  enableDataTrigger(ATTRIBUTE_DATA_TOGGLE, component, onLoadHandler, method);
+};
+
+export { enableToggleTrigger, enableDismissTrigger, enableToggleAutoloader };
