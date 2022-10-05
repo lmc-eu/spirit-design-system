3ec5de7fb9cae1bd11b9686a9e6cb64481f0133a
literat <tomas@litera.me>
2022-10-05 14:07:31 +0200

    Refactor(web): Rename `Header` plugin to `Offcanvas` (refs #DS-240)
    Feat(web): Introduce Drawer componennt (refs #DS-240)

--- packages/web/src/js/index.esm.ts
+++ packages/web/src/js/index.esm.ts
@@ -1,6 +1,6 @@
 export { default as Collapse } from './Collapse';
-export { default as Header } from './Header';
 export { default as Modal } from './Modal';
+export { default as Offcanvas } from './Offcanvas';
 export { default as Password } from './Password';
 export { default as Tabs } from './Tabs';
 export { default as Tooltip } from './Tooltip';
--- packages/web/src/js/index.umd.ts
+++ packages/web/src/js/index.umd.ts
@@ -1,8 +1,8 @@
 import Collapse from './Collapse';
-import Header from './Header';
 import Modal from './Modal';
+import Offcanvas from './Offcanvas';
 import Password from './Password';
 import Tabs from './Tabs';
 import Tooltip from './Tooltip';
 
-export default { Collapse, Header, Modal, Password, Tabs, Tooltip };
+export default { Collapse, Modal, Offcanvas, Password, Tabs, Tooltip };
