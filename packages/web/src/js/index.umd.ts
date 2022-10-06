f3569559b783525f753b7045e41c62c9ab8ec4dd
literat <tomas@litera.me>
2022-10-06 13:12:39 +0200

    Feat(web): Introduce new `Offcanvas` plugin derived from the `Header` plugin (refs #DS-240)

--- packages/web/src/js/index.umd.ts
+++ packages/web/src/js/index.umd.ts
@@ -3,11 +3,25 @@ import Collapse from './Collapse';
 import Dropdown from './Dropdown';
 import Header from './Header';
 import Modal from './Modal';
+import Offcanvas from './Offcanvas';
 import Password from './Password';
 import Tabs from './Tabs';
 import Tooltip from './Tooltip';
 import * as constants from './constants';
-import * as utils from './utils';
 import * as dom from './dom';
+import * as utils from './utils';
 
-export default { BaseComponent, Collapse, Dropdown, Header, Modal, Password, Tabs, Tooltip, constants, utils, dom };
+export default {
+  BaseComponent,
+  Collapse,
+  Dropdown,
+  Header,
+  Modal,
+  Offcanvas,
+  Password,
+  Tabs,
+  Tooltip,
+  constants,
+  dom,
+  utils,
+};
