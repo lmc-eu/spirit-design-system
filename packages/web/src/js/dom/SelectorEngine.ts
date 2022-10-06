ebbc47e2bc35ce159c057ec9cd9ef4103373d20f
literat <tomas@litera.me>
2022-10-06 13:12:39 +0200

    BREAKING CHANGES(web): Use native html dialog tag backdrop (refs #DS-206)

--- packages/web/src/js/dom/SelectorEngine.ts
+++ packages/web/src/js/dom/SelectorEngine.ts
@@ -3,7 +3,7 @@ const SelectorEngine = {
     return [].concat(...Element.prototype.querySelectorAll.call(element, selector));
   },
 
-  findOne(selector: string | null, element: SpiritElement = document.documentElement): HTMLElement | null {
+  findOne(selector: string | null | undefined, element: SpiritElement = document.documentElement): HTMLElement | null {
     return Element.prototype.querySelector.call(element, selector);
   },
 };
