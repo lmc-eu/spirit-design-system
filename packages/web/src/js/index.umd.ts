6de0eb43b476c85fc6a01bcb83d08bb03dc7b805
Tomas Sychra <tomas.sychra@almamedia.com>
2022-10-06 13:26:41 +0200

    Feat(web): Collapse web implementation #DS-275

--- packages/web/src/js/index.umd.ts
+++ packages/web/src/js/index.umd.ts
@@ -1,7 +1,8 @@
+import Collapse from './Collapse';
 import Header from './Header';
 import Modal from './Modal';
 import Password from './Password';
 import Tabs from './Tabs';
 import Tooltip from './Tooltip';
 
-export default { Header, Modal, Password, Tabs, Tooltip };
+export default { Collapse, Header, Modal, Password, Tabs, Tooltip };
