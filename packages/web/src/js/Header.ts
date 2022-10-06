53c20bbc84d4afcf9b785a54139d9173fbf76e56
literat <tomas@litera.me>
2022-10-06 13:12:39 +0200

    BREAKING CHANGES(web): Use native html dialog tag backdrop (refs #DS-206)

--- packages/web/src/js/Header.ts
+++ packages/web/src/js/Header.ts
@@ -1,20 +1,15 @@
 import BaseComponent from './BaseComponent';
 import EventHandler from './dom/EventHandler';
 import SelectorEngine from './dom/SelectorEngine';
-import { reflow } from './utils';
 import { enableToggleTrigger } from './utils/ComponentFunctions';
 
 const NAME = 'header';
 const HEADER_TOGGLE_SELECTOR = '[data-toggle="header"]';
-const HEADER_DISMISS_ATTRIBUTE = 'data-dismiss';
 const HEADER_BREAKPOINT = 1280;
 const OPEN_CLASSNAME = 'is-open';
-const BACKDROP_TAG_NAME = 'div';
-const BACKDROP_CLASSNAME = 'Header__backdrop';
 
 class Header extends BaseComponent {
   isShown: boolean;
-  backdrop: HTMLElement;
 
   static get NAME() {
     return NAME;
@@ -24,26 +19,13 @@ class Header extends BaseComponent {
     super(element);
 
     this.isShown = false;
-    this.backdrop = Header.initBackdrop(element);
-  }
-
-  static initBackdrop(target: HTMLElement) {
-    const backdropEl = document.createElement(BACKDROP_TAG_NAME);
-    backdropEl.classList.add(BACKDROP_CLASSNAME);
-
-    if (!SelectorEngine.findAll(`[class*="${BACKDROP_CLASSNAME}"]`, target).length) {
-      target.appendChild(backdropEl);
-      reflow(backdropEl);
-    }
-
-    return backdropEl;
   }
 
   // Using `unknown` - Object is possibly 'null'.
   // Using `Element | Window` - Property 'hasAttribute' does not exist on type 'EventTarget'.
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   onClick(event: Event & { target: any }) {
-    if (event.target.hasAttribute(HEADER_DISMISS_ATTRIBUTE) || event.target.classList.contains(BACKDROP_CLASSNAME)) {
+    if (event.target === this.element || event.target.dataset.dismiss) {
       event.preventDefault();
       event.stopPropagation();
       this.hide(event);
@@ -56,31 +38,26 @@ class Header extends BaseComponent {
     }
   }
 
-  onKeyPress(event: KeyboardEvent) {
+  onDialogClose(event: KeyboardEvent) {
     // Do nothing if the event was already processed.
     if (event.defaultPrevented) {
       return;
     }
 
-    if (event.key === 'Esc' || event.key === 'Escape') {
-      this.hide(event);
+    this.hide(event);
 
-      // Cancel the default action to avoid it being handled twice.
-      event.preventDefault();
-    }
-
-    // @TODO: retain focus when using tab
-    // @see: https://github.com/lmc-eu/spirit-design-system/issues/278
+    // Cancel the default action to avoid it being handled twice.
+    event.preventDefault();
   }
 
   addEventListeners() {
-    EventHandler.on(document, 'keydown', (event: KeyboardEvent) => this.onKeyPress(event));
+    EventHandler.on(this.element, 'close', (event: KeyboardEvent) => this.onDialogClose(event));
     EventHandler.on(window, 'resize', (event: Event & { target: Window }) => this.onWindowResize(event));
     EventHandler.on(window, 'click', (event: Event & { target: Window }) => this.onClick(event));
   }
 
   removeEventListeners() {
-    EventHandler.off(document, 'keydown', (event: KeyboardEvent) => this.onKeyPress(event));
+    EventHandler.off(this.element, 'close', (event: KeyboardEvent) => this.onDialogClose(event));
     EventHandler.off(window, 'resize', (event: Event & { target: Window }) => this.onWindowResize(event));
     EventHandler.off(window, 'click', (event: Event & { target: Window }) => this.onClick(event));
   }
@@ -91,6 +68,7 @@ class Header extends BaseComponent {
     }
 
     this.element.classList.add(OPEN_CLASSNAME);
+    this.element.showModal();
     relatedTarget.setAttribute('aria-expanded', 'true');
 
     this.addEventListeners();
@@ -103,20 +81,21 @@ class Header extends BaseComponent {
   hide(event: Event & { target: any }) {
     let target;
     // hiding by resizing
-    if (!event.target.dataset) {
+    if (Object.keys(event.target.dataset).length === 0) {
       target = this.element;
       // hiding by clicking
     } else if (event.target.dataset.target) {
       target = SelectorEngine.findOne(event.target.dataset.target);
-      // hiding by keyboard
+      // hiding by dialog close
     } else {
-      target = event.target.parentNode;
+      target = this.element;
     }
 
     const navEl = this.element || target;
     const headerEl = navEl.parentElement;
     const toggleEl = SelectorEngine.findOne(HEADER_TOGGLE_SELECTOR, headerEl);
     target.classList.remove(OPEN_CLASSNAME);
+    target.close();
     toggleEl?.setAttribute('aria-expanded', 'false');
 
     this.removeEventListeners();
