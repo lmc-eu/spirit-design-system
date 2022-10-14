fa368a0b82c16b526dcc2971cd92d8ff828191a6
Tomas Sychra <tomas.sychra@almamedia.com>
2022-10-14 10:06:55 +0200

    Feat(web-react): Updates by comments #DS-275

--- packages/web-react/src/components/Collapse/__tests__/useCollapseAriaProps.test.ts
+++ packages/web-react/src/components/Collapse/__tests__/useCollapseAriaProps.test.ts
@@ -1,16 +1,37 @@
 import { renderHook } from '@testing-library/react-hooks';
-import { useCollapseAriaProps } from '../useCollapseAriaProps';
+import { useCollapseAriaProps, CollapseAriaPropsProps } from '../useCollapseAriaProps';
 
 describe('useCollapseAriaProps', () => {
   it('should return defaults', () => {
     const props = {
       id: 'test-collapse-id',
       isCollapsed: true,
+      onClick: () => null,
+      toggleHandler: () => null,
     };
     const { result } = renderHook(() => useCollapseAriaProps(props));
 
-    expect(result.current.wrapperProps).toBeTruthy();
-    expect(result.current.contentProps).toBeTruthy();
-    expect(result.current.triggerProps).toBeTruthy();
+    expect(result.current.wrapperProps).toBeDefined();
+    expect(result.current.wrapperProps.style).toBeDefined();
+    expect(result.current.wrapperProps['data-breakpoint']).toBeUndefined();
+    expect(result.current.contentProps).toBeDefined();
+    expect(result.current.contentProps.ref).toBeDefined();
+    expect(result.current.triggerProps).toBeDefined();
+    expect(result.current.triggerProps['aria-expanded']).toBeDefined();
+    expect(result.current.triggerProps['aria-controls']).toBeDefined();
+    expect(result.current.triggerProps.onClick).toBeDefined();
+  });
+
+  it('should return breakpoints', () => {
+    const props = {
+      id: 'test-collapse-id',
+      isCollapsed: false,
+      toggleHandler: () => null,
+      collapsibleToBreakpoint: 'mobile',
+    } as CollapseAriaPropsProps;
+    const { result } = renderHook(() => useCollapseAriaProps(props));
+
+    expect(result.current.wrapperProps).toBeDefined();
+    expect(result.current.wrapperProps['data-breakpoint']).toBe('mobile');
   });
 });
