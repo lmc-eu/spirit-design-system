d0e5ff687511a13d2252436651af92b031d1dc5a
literat <tomas@litera.me>
2022-11-25 12:52:22 +0100

    Feat(web-react): Add missing `breakpoint` prop to Dropdown component

--- packages/web-react/src/components/Dropdown/Dropdown.stories.tsx
+++ packages/web-react/src/components/Dropdown/Dropdown.stories.tsx
@@ -10,3 +10,4 @@ export { default as Dropdown } from './stories/Dropdown';
 export { default as DisableAutoClose } from './stories/DisableAutoClose';
 export { default as DropdownFullWidth } from './stories/DropdownFullWidth';
 export { default as DropdownPlacements } from './stories/DropdownPlacements';
+export { default as DropdownBreakpoints } from './stories/DropdownBreakpoints';
--- packages/web-react/src/components/Dropdown/useDropdownAriaProps.ts
+++ packages/web-react/src/components/Dropdown/useDropdownAriaProps.ts
@@ -1,7 +1,8 @@
-import { Booleanish, ClickEvent } from '../../types';
+import { Booleanish, ClickEvent, DropdownBreakpoint } from '../../types';
 
 const NAME_ARIA_EXPANDED = 'aria-expanded';
 const NAME_ARIA_CONTROLS = 'aria-controls';
+const NAME_DATA_BREAKPOINT = 'data-breakpoint';
 
 export interface UseDropdownAriaPropsProps {
   /** element ID */
@@ -10,12 +11,15 @@ export interface UseDropdownAriaPropsProps {
   isOpen: boolean;
   /** toggle callback */
   toggleHandler: (event: ClickEvent) => void;
+  /** breakpoint */
+  breakpoint: DropdownBreakpoint | undefined;
 }
 
 export interface UseDropdownAriaPropsReturn {
   /** content returned props */
   contentProps: {
     id: string;
+    [NAME_DATA_BREAKPOINT]: DropdownBreakpoint | undefined;
   };
   /** trigger returned props */
   triggerProps: {
@@ -26,7 +30,7 @@ export interface UseDropdownAriaPropsReturn {
 }
 
 export const useDropdownAriaProps = (props: UseDropdownAriaPropsProps): UseDropdownAriaPropsReturn => {
-  const { id, isOpen, toggleHandler } = props;
+  const { breakpoint, id, isOpen, toggleHandler } = props;
 
   const triggerProps = {
     id,
@@ -34,7 +38,7 @@ export const useDropdownAriaProps = (props: UseDropdownAriaPropsProps): UseDropd
     [NAME_ARIA_CONTROLS]: String(id),
     onClick: toggleHandler,
   };
-  const contentProps = { id };
+  const contentProps = { id, [NAME_DATA_BREAKPOINT]: breakpoint };
 
   return {
     contentProps,
