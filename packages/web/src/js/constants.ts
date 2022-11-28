f9f3b46cd98364f1ca7470d73f8a7a6124a902c5
Tomáš Sychra <tomas.sychra@almamedia.com>
2022-11-28 10:33:32 +0100

    Fix(web): Collapse implementation fixes
    Fix(web): Collapse component scripts polish
    
    Fix(web): Removing unnecessary variables from Collapse scripts
    
    Fix(web): Collapse component parent(s) variable fix
    
    Fix(web): Improvements of Accordion styles and documentation

--- packages/web/src/js/constants.ts
+++ packages/web/src/js/constants.ts
@@ -1,6 +1,7 @@
 export const ARIA_EXPANDED_ATTRIBUTE = 'aria-expanded';
 export const ARIA_CONTROLS_ATTRIBUTE = 'aria-controls';
 
+export const NAME_DATA_TOGGLE = 'data-toggle';
 export const NAME_DATA_TARGET = 'data-target';
 
 export const CLASSNAME_EXPANDED = 'is-expanded';
