b136c5f327c9def1587188b53668889dd5f0d362
Jan Kryšpín <crishpeen@gmail.com>
2022-12-30 15:23:39 +0000

    Fix(web-react): Make CheckboxField implementation same as in other packages and update itemPropsTest

--- packages/web-react/tests/providerTests/itemPropsTest.tsx
+++ packages/web-react/tests/providerTests/itemPropsTest.tsx
@@ -2,12 +2,12 @@ import { render, waitFor } from '@testing-library/react';
 import React, { ComponentType } from 'react';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const itemPropsTest = (Component: ComponentType<any>, selector: string) => {
+export const itemPropsTest = (Component: ComponentType<any>) => {
   it('should have item classname', async () => {
     const dom = render(<Component isItem />);
 
     await waitFor(() => {
-      const element = dom.container.querySelector(selector) as HTMLElement;
+      const element = dom.container.querySelector('label') as HTMLElement;
       expect(element.getAttribute('class')).toContain('--item');
     });
   });
