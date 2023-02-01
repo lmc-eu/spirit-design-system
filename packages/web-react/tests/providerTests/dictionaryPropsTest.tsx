830aa2c7700a7d8a1fd93df2a444ed71aa3ab80b
Jan Kryšpín <crishpeen@gmail.com>
2023-02-01 14:31:40 +0000

    fixup! fixup! Feat(web-react): Define size dictionaries and use them in Text component #DS-450

--- packages/web-react/tests/providerTests/dictionaryPropsTest.tsx
+++ packages/web-react/tests/providerTests/dictionaryPropsTest.tsx
@@ -1,12 +1,12 @@
 import React, { ComponentType } from 'react';
 import { render, waitFor } from '@testing-library/react';
-import { DictionarySizeExtendedType, DictionarySizeType } from '../../src';
+import { SizeExtended, Size } from '../../src';
 import getElement from '../testUtils/getElement';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const sizePropsTest = (Component: ComponentType<any>, testId?: string) => {
   it.each([['small'], ['medium'], ['large']])('should render size %s', async (size) => {
-    const dom = render(<Component size={size as DictionarySizeType} />);
+    const dom = render(<Component size={size as Size<string>} />);
 
     await waitFor(() => {
       const element = getElement(dom, testId);
@@ -18,7 +18,7 @@ export const sizePropsTest = (Component: ComponentType<any>, testId?: string) =>
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const sizeExtendedPropsTest = (Component: ComponentType<any>, testId?: string) => {
   it.each([['xsmall'], ['xlarge']])('should render extended size %s', async (size) => {
-    const dom = render(<Component size={size as DictionarySizeExtendedType} />);
+    const dom = render(<Component size={size as SizeExtended<string>} />);
 
     await waitFor(() => {
       const element = getElement(dom, testId);
