b9a10f69e92d17f307948cb1b85fd7445e84cbcd
literat <tomas@litera.me>
2023-02-08 21:01:30 +0100

    Feat(web): Display backdrop for native dialog element

--- packages/web/src/js/Modal.ts
+++ packages/web/src/js/Modal.ts
@@ -1,71 +1,55 @@
 import BaseComponent from './BaseComponent';
 import EventHandler from './dom/EventHandler';
 import SelectorEngine from './dom/SelectorEngine';
-import { getElementFromSelector, reflow } from './utils';
+import { enableToggleTrigger } from './utils/ComponentFunctions';
+
+const NAME = 'modal';
 
 const MODAL_TOGGLE_SELECTOR = '[data-toggle="modal"]';
-const MODAL_DISMISS_ATTRIBUTE = 'data-dismiss';
-const BACKDROP_TAG_NAME = 'div';
-const BACKDROP_CLASSNAME = 'Modal__backdrop';
 
 class Modal extends BaseComponent {
   isShown: boolean;
-  backdrop: HTMLElement;
 
-  constructor(element: HTMLElement) {
+  static get NAME() {
+    return NAME;
+  }
+
+  constructor(element: HTMLElement | string) {
     super(element);
 
     this.isShown = false;
-    this.backdrop = Modal.initBackdrop(element);
-  }
-
-  static initBackdrop(target: HTMLElement) {
-    const backdropEl = document.createElement(BACKDROP_TAG_NAME);
-    backdropEl.classList.add(BACKDROP_CLASSNAME);
-
-    if (!SelectorEngine.findAll(`[class*="${BACKDROP_CLASSNAME}"]`, target).length) {
-      target.appendChild(backdropEl);
-      reflow(backdropEl);
-    }
-
-    return backdropEl;
   }
 
   // Using `unknown` - Object is possibly 'null'.
   // Using `Element | Window` - Property 'hasAttribute' does not exist on type 'EventTarget'.
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   onClick(event: Event & { target: any }) {
-    if (event.target.hasAttribute(MODAL_DISMISS_ATTRIBUTE) || event.target.classList.contains(BACKDROP_CLASSNAME)) {
+    if (event.target === this.element || event.target.dataset.dismiss) {
       event.preventDefault();
       event.stopPropagation();
       this.hide(event);
     }
   }
 
-  onKeyPress(event: KeyboardEvent) {
+  onDialogClose(event: KeyboardEvent) {
     // Do nothing if the event was already processed.
     if (event.defaultPrevented) {
       return;
     }
 
-    if (event.key === 'Esc' || event.key === 'Escape') {
-      this.hide(event);
+    this.hide(event);
 
-      // Cancel the default action to avoid it being handled twice.
-      event.preventDefault();
-    }
-
-    // @TODO: retain focus when using tab
-    // @see: https://github.com/lmc-eu/spirit-design-system/issues/278
+    // Cancel the default action to avoid it being handled twice.
+    event.preventDefault();
   }
 
   addEventListeners() {
-    EventHandler.on(document, 'keydown', (event: KeyboardEvent) => this.onKeyPress(event));
+    EventHandler.on(this.element, 'close', (event: KeyboardEvent) => this.onDialogClose(event));
     EventHandler.on(window, 'click', (event: Event & { target: Window }) => this.onClick(event));
   }
 
   removeEventListeners() {
-    EventHandler.off(document, 'keydown', (event: KeyboardEvent) => this.onKeyPress(event));
+    EventHandler.off(this.element, 'close', (event: KeyboardEvent) => this.onDialogClose(event));
     EventHandler.off(window, 'click', (event: Event & { target: Window }) => this.onClick(event));
   }
 
@@ -88,18 +72,21 @@ class Modal extends BaseComponent {
   hide(event: Event & { target: any }) {
     let target;
     // hiding by resizing
-    if (!event.target.dataset) {
+    if (!event?.target?.dataset) {
       target = this.element;
       // hiding by clicking
     } else if (event.target.dataset.target) {
       target = SelectorEngine.findOne(event.target.dataset.target);
       // hiding by keyboard
     } else {
-      target = event.target.parentNode;
+      target = event.target;
     }
 
     const toggleEl = SelectorEngine.findOne(MODAL_TOGGLE_SELECTOR, this.element);
-    target.close();
+
+    if (typeof target.close === 'function') {
+      target.close();
+    }
     toggleEl?.setAttribute('aria-expanded', 'false');
 
     this.removeEventListeners();
@@ -123,18 +110,6 @@ class Modal extends BaseComponent {
   }
 }
 
-function handleModalClick(event: Event) {
-  const target = getElementFromSelector(this);
-  if (target) {
-    const modal = new Modal(target);
-    modal.toggle(target, event as Event & { target: HTMLOrSVGElement; currentTarget: HTMLOrSVGElement });
-  }
-}
-
-EventHandler.on(window, 'DOMContentLoaded', () => {
-  SelectorEngine.findAll(MODAL_TOGGLE_SELECTOR).forEach((toggleEl) => {
-    EventHandler.on(toggleEl, 'click', handleModalClick);
-  });
-});
+enableToggleTrigger(Modal);
 
 export default Modal;
---
+++ packages/web/src/js/__tests__/Modal.test.ts
@@ -0,0 +1,102 @@
+import Modal from '../Modal';
+import EventHandler from '../dom/EventHandler';
+import { clearFixture, getFixture } from '../../../tests/helpers/fixture';
+
+describe('Modal', () => {
+  let fixtureEl: Element;
+
+  beforeAll(() => {
+    fixtureEl = getFixture();
+  });
+
+  afterEach(() => {
+    clearFixture();
+    document.body.classList.remove('is-open');
+  });
+
+  describe('constructor', () => {
+    it('should take care of element either passed as a CSS selector or DOM element', () => {
+      fixtureEl.innerHTML = '<dialog class="Modal"><div class="Modal__content"></div></dialog>';
+
+      const modalEl = fixtureEl.querySelector('.Modal') as HTMLElement;
+      const modalBySelector = new Modal('.Modal');
+      const modalByElement = new Modal(modalEl);
+
+      expect(modalBySelector.element).toEqual(modalEl);
+      expect(modalByElement.element).toEqual(modalEl);
+    });
+  });
+
+  describe('show', () => {
+    it('should do nothing if a modal is shown', () => {
+      fixtureEl.innerHTML = '<dialog class="Modal"><div class="Modal__content"></div></dialog>';
+
+      const modalEl = fixtureEl.querySelector('.Modal') as HTMLElement;
+      const modal = new Modal(modalEl);
+
+      const spy = jest.spyOn(EventHandler, 'trigger');
+      modal.isShown = true;
+
+      modal.show();
+
+      expect(spy).not.toHaveBeenCalled();
+    });
+  });
+
+  describe('hide', () => {
+    it('should do nothing is the modal is not shown', () => {
+      fixtureEl.innerHTML = '<dialog class="Modal"><div class="Modal__content"></div></dialog>';
+
+      const modalEl = fixtureEl.querySelector('.Modal') as HTMLElement;
+      const modal = new Modal(modalEl);
+      const event = new Event('keydown');
+      modalEl.dispatchEvent(event);
+
+      modal.hide(event);
+
+      expect.anything();
+    });
+  });
+
+  describe('getInstance', () => {
+    it('should return modal instance', () => {
+      fixtureEl.innerHTML = '<dialog class="Modal"><div class="Modal__content"></div></dialog>';
+
+      const dialog = fixtureEl.querySelector('dialog') as HTMLElement;
+      const modal = new Modal(dialog);
+
+      expect(Modal.getInstance(dialog)).toEqual(modal);
+      expect(Modal.getInstance(dialog)).toBeInstanceOf(Modal);
+    });
+
+    it('should return null when there is no modal instance', () => {
+      fixtureEl.innerHTML = '<dialog class="Modal"><div class="Modal__content"></div></dialog>';
+
+      const dialog = fixtureEl.querySelector('dialog') as HTMLElement;
+
+      expect(Modal.getInstance(dialog)).toBeNull();
+    });
+  });
+
+  describe('getOrCreateInstance', () => {
+    it('should return modal instance', () => {
+      fixtureEl.innerHTML = '<dialog></dialog>';
+
+      const dialog = fixtureEl.querySelector('dialog') as HTMLElement;
+      const modal = new Modal(dialog);
+
+      expect(Modal.getOrCreateInstance(dialog)).toEqual(modal);
+      expect(Modal.getInstance(dialog)).toEqual(Modal.getOrCreateInstance(dialog));
+      expect(Modal.getOrCreateInstance(dialog)).toBeInstanceOf(Modal);
+    });
+
+    it('should return new instance when there is no modal instance', () => {
+      fixtureEl.innerHTML = '<dialog></dialog>';
+
+      const dialog = fixtureEl.querySelector('dialog') as HTMLElement;
+
+      expect(Modal.getInstance(dialog)).toBeNull();
+      expect(Modal.getOrCreateInstance(dialog)).toBeInstanceOf(Modal);
+    });
+  });
+});
