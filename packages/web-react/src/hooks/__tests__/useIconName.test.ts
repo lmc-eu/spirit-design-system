be46fc4434bbc4e0395c68aa10f32a3622bcd0ba
literat <tomas@litera.me>
2023-02-15 12:18:22 +0100

    Test(web-react): Add missing test for `useIconName` hook
    refs #DS-619

---
+++ packages/web-react/src/hooks/__tests__/useIconName.test.ts
@@ -0,0 +1,17 @@
+import { renderHook } from '@testing-library/react-hooks';
+import { useIconName } from '../useIconName';
+
+describe('useIconName', () => {
+  it('should return default icon', () => {
+    const { result } = renderHook(() => useIconName('', { default: 'default_icon' }));
+
+    expect(result.current).toBe('default_icon');
+  });
+
+  it('should return specific icon', () => {
+    const icons = { warning: '<path d="ERRW ADSFDSFDS"></path>', default: 'default_icon' };
+    const { result } = renderHook(() => useIconName('warning', icons));
+
+    expect(result.current).toBe('<path d="ERRW ADSFDSFDS"></path>');
+  });
+});
