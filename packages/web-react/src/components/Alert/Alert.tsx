0b6fef1eaa5c718b0f8f59891c4a8a84dbc6a22c
literat <tomas@litera.me>
2023-02-15 12:17:03 +0100

    Fix(web-react): Do not pass `iconName` prop to Alert html element
    refs #DS-619

--- packages/web-react/src/components/Alert/Alert.tsx
+++ packages/web-react/src/components/Alert/Alert.tsx
@@ -12,14 +12,14 @@ const defaultProps = {
 };
 
 export const Alert = <T extends ElementType = 'div', E = void>(props: SpiritAlertProps<T, E>): JSX.Element => {
-  const { elementType: ElementTag = 'div', children, ...restProps } = props;
-  const { classProps, props: modifiedProps } = useAlertStyleProps(restProps);
+  const { elementType: ElementTag = 'div', children, color, iconName, ...restProps } = props;
+  const { classProps, props: modifiedProps } = useAlertStyleProps({ color, ...restProps });
   const { styleProps, props: otherProps } = useStyleProps(modifiedProps);
-  const iconName = useAlertIcon(restProps);
+  const alertIconName = useAlertIcon({ color, iconName, ...otherProps });
 
   return (
     <ElementTag {...otherProps} {...styleProps} className={classNames(classProps, styleProps.className)} role="alert">
-      <Icon name={iconName} />
+      <Icon name={alertIconName} />
       <div>{children}</div>
     </ElementTag>
   );
