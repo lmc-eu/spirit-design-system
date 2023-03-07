4759391eedbfcd59525802edbc3059247dbde352
literat <tomas@litera.me>
2023-03-07 21:47:23 +0100

    Chore(web-twig): Create default controller and template for local web

---
+++ app/Controller/DefaultController.php
@@ -0,0 +1,15 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends AbstractController
+{
+  #[Route('/')]
+    public function index(): Response
+    {
+        return $this->render('default.html.twig');
+    }
+}
