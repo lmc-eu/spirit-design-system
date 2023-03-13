164511a7e07a3eb8c72dcc01e3885d6af9abc609
literat <tomas@litera.me>
2023-03-13 21:46:01 +0100

    Chore(web-twig): Build assets for demo app
    refs #DS-299

--- apps/web-twig-demo/config/bundles.php
+++ apps/web-twig-demo/config/bundles.php
@@ -4,4 +4,5 @@ return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Lmc\SpiritWebTwigBundle\SpiritWebTwigBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
