0cbaf5e446ed92c4b38d36d63d984a86068b184e
literat <tomas@litera.me>
2023-03-13 14:40:36 +0100

    Chore(web-twig): Link bundle to the demo app
    refs #DS-299

--- apps/web-twig-demo/config/bundles.php
+++ apps/web-twig-demo/config/bundles.php
@@ -3,4 +3,5 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Lmc\SpiritWebTwigBundle\SpiritWebTwigBundle::class => ['all' => true],
 ];
