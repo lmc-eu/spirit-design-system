8258c034250570115c8a85d8fb48e62dc4c981f3
literat <tomas@litera.me>
2023-03-13 22:24:25 +0100

    Chore(web-twig): Enable icons for demo app
    refs #DS-299

--- apps/web-twig-demo/config/bundles.php
+++ apps/web-twig-demo/config/bundles.php
@@ -5,4 +5,6 @@ return [
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Lmc\SpiritWebTwigBundle\SpiritWebTwigBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
 ];
