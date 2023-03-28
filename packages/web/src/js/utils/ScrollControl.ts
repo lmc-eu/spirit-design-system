d52f687942a5c24cfa636effa148085b81030833
Tomáš Sychra <tomas.sychra@almamedia.com>
2023-03-28 11:44:25 +0200

    Docs(web): Example for stacked Modal component

--- packages/web/src/js/utils/ScrollControl.ts
+++ packages/web/src/js/utils/ScrollControl.ts
@@ -1,14 +1,17 @@
 import { SelectorEngine } from '../dom';
+import { executeAfterTransition } from './Transitions';
 
 const DIALOG_OPEN_SELECTOR = 'dialog[open]';
 const SCROLLING_DISABLED_CLASSNAME = 'is-scrolling-disabled';
 
 class ScrollControl {
+  target: HTMLElement;
   element: HTMLElement;
   selector: string;
   offsetY: number;
 
-  constructor(element?: HTMLElement, selector?: string) {
+  constructor(target: HTMLElement, element?: HTMLElement, selector?: string) {
+    this.target = target;
     this.element = element || document.body;
     this.selector = selector || DIALOG_OPEN_SELECTOR;
     this.offsetY = 0;
@@ -20,8 +23,10 @@ class ScrollControl {
       return;
     }
 
+    // Selects the current value for calculation
+    const clientOffset = this.element.tagName === 'BODY' ? window.innerWidth : this.element.offsetWidth;
     // Browser scrollbar width, may be 0 on some devices, but solves offset when is not 0.
-    const scrollBarWidth = this.element.offsetWidth - this.element.clientWidth;
+    const scrollBarWidth = clientOffset - this.element.clientWidth;
     // Save Y offset for later purpose
     this.offsetY = window.scrollY;
     // Fixing page offset causing scrollbar on some devices
@@ -32,17 +37,19 @@ class ScrollControl {
     this.element.classList.add(SCROLLING_DISABLED_CLASSNAME);
   }
 
-  activateScroll() {
+  enableScroll() {
     const dialogs = SelectorEngine.findAll(this.selector);
 
-    if (dialogs.length === 0) {
-      // Reset style values
-      this.element.style.paddingRight = '';
-      this.element.style.top = '';
-      // Remove body class
-      this.element.classList.remove(SCROLLING_DISABLED_CLASSNAME);
-      // Scroll after close to saved Y offset position
-      window.scrollTo(0, this.offsetY);
+    if (this.target && dialogs.length === 0) {
+      executeAfterTransition(this.target, () => {
+        // Reset style values
+        this.element.style.paddingRight = '';
+        this.element.style.top = '';
+        // Remove body class
+        this.element.classList.remove(SCROLLING_DISABLED_CLASSNAME);
+        // Scroll after close to saved Y offset position
+        window.scrollTo(0, this.offsetY);
+      });
     }
   }
 }
