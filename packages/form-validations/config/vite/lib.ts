1a6027a4c0760231a47df7eb49691333146b9f54
literat <tomas@litera.me>
2023-04-14 21:08:03 +0200

    Chore(form-validations): Prepare distirbution build
    refs #DS-608, #366

---
+++ packages/form-validations/config/vite/lib.ts
@@ -0,0 +1,23 @@
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  build: {
+    minify: true,
+    lib: {
+      entry: resolve(__dirname, '../../src/index.ts'),
+      name: 'SpiritFormValidations',
+      formats: ['es', 'cjs', 'umd'],
+      fileName: (format) => {
+        return `${format === 'es' ? 'esm' : format}/spirit-form-validations${format === 'umd' ? '.min' : ''}.js`;
+      },
+    },
+    outDir: resolve(__dirname, '../../dist/bundles'),
+    rollupOptions: {
+      output: {
+        // Include sourcemaps for unminified build
+        sourcemap: true,
+      },
+    },
+  },
+});
