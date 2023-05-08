e441a2770424e576c41a3e96b17fd5008099908d
literat <tomas@litera.me>
2023-05-08 21:45:51 +0200

    Feat(form-validation): Validate min/max of input type number
    refs #DS-665

--- packages/form-validations/src/__tests__/FormValidations.test.ts
+++ packages/form-validations/src/__tests__/FormValidations.test.ts
@@ -66,4 +66,64 @@ describe('FormValidations', () => {
       expect(formValidations.validate(input)).toBe(true);
     });
   });
+
+  describe('max/min', () => {
+    beforeEach(() => {
+      const fixture = `
+        <div id="fixture">
+          <form id="form" novalidate method="post">
+            <div class="form-group" data-spirit-validate>
+              <input id="min-input" min="10" type="number" class="form-control" />
+              <input id="max-input" max="100" type="number" class="form-control" />
+              <input id="min-max-input" min="10" max="100" type="number" class="form-control" />
+            </div>
+          </form>
+        </div>`;
+
+      document.body.insertAdjacentHTML('afterbegin', fixture);
+    });
+
+    afterEach(() => {
+      document.body.removeChild(document.getElementById('fixture') as HTMLDivElement);
+    });
+
+    it('should validate min (max) when empty', () => {
+      const form = document.getElementById('fixture') as HTMLDivElement;
+      const formValidations = new FormValidations(form);
+
+      expect(formValidations.validate()).toBe(true);
+    });
+
+    it('should validate min value', () => {
+      const form = document.getElementById('fixture') as HTMLDivElement;
+      const input = document.getElementById('min-input') as HTMLInputElement;
+      const formValidations = new FormValidations(form);
+
+      input.value = '9';
+      expect(formValidations.validate()).toBe(false);
+      expect(formValidations.getErrors()[0].errors).toHaveLength(1);
+
+      input.value = '10';
+      expect(formValidations.validate()).toBe(true);
+
+      input.value = '11';
+      expect(formValidations.validate()).toBe(true);
+    });
+
+    it('should validate the max value', () => {
+      const form = document.getElementById('fixture') as HTMLDivElement;
+      const input = document.getElementById('max-input') as FormValidationsHTMLElement;
+      const formValidations = new FormValidations(form);
+
+      for (const item of [
+        [99, true],
+        [100, true],
+        [101, false],
+      ]) {
+        input.value = item[0].toString();
+
+        expect(formValidations.validate(input, true)).toBe(item[1]);
+      }
+    });
+  });
 });
