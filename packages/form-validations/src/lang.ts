109306223651a8176ba5c79a0fef2ebc44376e96
literat <tomas@litera.me>
2023-05-11 22:17:41 +0200

    Feat(form-validations): Add missing default validation texts
    refs #DS-665

--- packages/form-validations/src/lang.ts
+++ packages/form-validations/src/lang.ts
@@ -4,5 +4,16 @@ export const lang: Languages = {
   en: {
     required: 'This field is required',
     default: 'Please enter a correct value',
+    email: 'This field requires a valid e-mail address',
+    number: 'This field requires a number',
+    integer: 'This field requires an integer value',
+    url: 'This field requires a valid website URL',
+    tel: 'This field requires a valid telephone number',
+    maxlength: `This fields length must be < ${1}`,
+    minlength: `This fields length must be > ${1}`,
+    min: `Minimum value for this field is ${1}`,
+    max: `Maximum value for this field is ${1}`,
+    pattern: 'Please match the requested format',
+    equals: 'The two fields do not match',
   },
 };
