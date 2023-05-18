6724d7aa7e38ae8c9d25e0beacc7c4095f012bcd
Jan Kryšpín <crishpeen@gmail.com>
2023-05-18 17:04:47 +0200

    Feat(web,web-react,web-twig): Allow multiline validationText in FileUploaderInput #DS-737

--- packages/web-react/tests/providerTests/validationTextPropsTest.tsx
+++ packages/web-react/tests/providerTests/validationTextPropsTest.tsx
@@ -9,7 +9,17 @@ export const validationTextPropsTest = (
   type: TextFieldType | null = null,
 ) => {
   it('should have message', async () => {
-    const dom = render(<Component id="component" label="Label" type={type as TextFieldType} message="text" />);
+    const dom = render(
+      <Component
+        id="component"
+        label="Label"
+        type={type as TextFieldType}
+        /** @deprecated Will be removed in the next major version. */
+        message="text"
+        validationState="danger"
+        validationText="text"
+      />,
+    );
 
     await waitFor(() => {
       const element = dom.container.querySelector(className) as HTMLElement;
@@ -19,7 +29,15 @@ export const validationTextPropsTest = (
 
   it('should have multiline message as a list', async () => {
     const dom = render(
-      <Component id="component" label="Label" type={type as TextFieldType} message={['foo', 'bar']} />,
+      <Component
+        id="component"
+        label="Label"
+        type={type as TextFieldType}
+        /** @deprecated Will be removed in the next major version. */
+        message={['foo', 'bar']}
+        validationState="danger"
+        validationText={['foo', 'bar']}
+      />,
     );
 
     await waitFor(() => {
