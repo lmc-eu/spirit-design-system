7f9982076ac6ffef8bfcdfcb569bcec65c89bb27
Jan Kryšpín <crishpeen@gmail.com>
2023-06-09 11:04:49 +0200

    BREAKING CHANGE(web-react): Rename ScrollView `indicators` prop to `overflowDecorators` #DS-825
     ## Migration Guide
    
    Rename `indicators` prop to `overflowDecorators` on all ScrollView component usages.
    
    - `<ScrollView indicators="shadows" …>` → `<ScrollView overflowDecorators="shadows" …>`
    - `<ScrollView indicators="borders" …>` → `<ScrollView overflowDecorators="borders" …>`
    - `<ScrollView indicators="both" …>` → `<ScrollView overflowDecorators="both" …>`
    
    Please refer back to these instructions or reach out to our team
    if you encounter any issues during migration.

--- packages/web-react/src/components/ScrollView/useScrollViewStyleProps.ts
+++ packages/web-react/src/components/ScrollView/useScrollViewStyleProps.ts
@@ -1,13 +1,13 @@
 import classNames from 'classnames';
 import { useClassNamePrefix } from '../../hooks';
-import { ScrollViewDirectionType, ScrollViewEdgeIndicatorType } from '../../types';
+import { ScrollViewDirectionType, ScrollViewOverflowDecoratorsType } from '../../types';
 
 export interface UseScrollViewStyleProps {
   direction: ScrollViewDirectionType;
-  edgeIndicators: ScrollViewEdgeIndicatorType;
   isScrollbarDisabled?: boolean;
   isScrolledAtEnd: boolean;
   isScrolledAtStart: boolean;
+  overflowDecorators: ScrollViewOverflowDecoratorsType;
 }
 
 export interface UseScrollViewStyleReturn {
@@ -16,29 +16,29 @@ export interface UseScrollViewStyleReturn {
     root: string;
     viewport: string;
     content: string;
-    indicators: string;
+    overflowDecorators: string;
   };
 }
 
 export const useScrollViewStyleProps = ({
   direction,
-  edgeIndicators,
   isScrollbarDisabled,
   isScrolledAtEnd,
   isScrolledAtStart,
+  overflowDecorators,
 }: UseScrollViewStyleProps): UseScrollViewStyleReturn => {
   const scrollViewRootClass = useClassNamePrefix('ScrollView');
   const scrollViewRootDirectionClass = `${scrollViewRootClass}--${direction}`;
   const scrollViewRootScrollbarDisabledClass = `${scrollViewRootClass}--scrollbarDisabled`;
   const scrollViewViewportClass = `${scrollViewRootClass}__viewport`;
   const scrollViewContentClass = `${scrollViewRootClass}__content`;
-  const scrollViewIndicatorsClass = `${scrollViewRootClass}__indicators`;
-  const scrollViewRootEdgeIndicatorClasses = {
-    shadows: `${scrollViewIndicatorsClass}--shadows`,
-    borders: `${scrollViewIndicatorsClass}--borders`,
-    both: classNames(`${scrollViewIndicatorsClass}--shadows`, `${scrollViewIndicatorsClass}--borders`),
+  const scrollViewOverflowDecoratorsClass = `${scrollViewRootClass}__overflowDecorators`;
+  const scrollViewRootOverflowDecoratorsClasses = {
+    shadows: `${scrollViewOverflowDecoratorsClass}--shadows`,
+    borders: `${scrollViewOverflowDecoratorsClass}--borders`,
+    both: classNames(`${scrollViewOverflowDecoratorsClass}--shadows`, `${scrollViewOverflowDecoratorsClass}--borders`),
   };
-  const scrollViewRootEdgeIndicatorClass = scrollViewRootEdgeIndicatorClasses[edgeIndicators];
+  const scrollViewRootOverflowDecoratorsClass = scrollViewRootOverflowDecoratorsClasses[overflowDecorators];
   const scrollViewAtStartClass = 'is-scrolled-at-start';
   const scrollViewAtEndClass = 'is-scrolled-at-end';
 
@@ -51,7 +51,7 @@ export const useScrollViewStyleProps = ({
       }),
       viewport: scrollViewViewportClass,
       content: scrollViewContentClass,
-      indicators: classNames(scrollViewIndicatorsClass, scrollViewRootEdgeIndicatorClass),
+      overflowDecorators: classNames(scrollViewOverflowDecoratorsClass, scrollViewRootOverflowDecoratorsClass),
     },
   };
 };
