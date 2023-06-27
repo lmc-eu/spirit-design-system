0e081837ce411bf3e4547cae88f421970988d25d
Tomáš Sychra <tomas.sychra@almamedia.com>
2023-06-27 10:00:35 +0200

    BREAKING CHANGE(web-react): Remove Header deprecations #DS-651
     ## Migration Guide
    
    Instead of `HeaderModern`, now use `Header` and its structure.
    
    The most notable change is that the mobile and desktop navigation
    are now separate components that cannot be nested:
    
    - `Header`
    - `HeaderDialog`
    
    Formerly, the desktop dialog (holding the site navigation) was
    a responsive CSS modification of a shared HTML code. With the
    new `Header` and `HeaderDialog`, each component has its own
    instance of the navigation, i.e. the navigation code is duplicated.
    This is because `HeaderDialog` will become an independent
    component in the future.
    
    You may need to take additional measures so your SEO is not
    affected by the duplicate HTML code of the navigation.
    
    The old components in `Header` can be roughly mapped onto the
    new subcomponents like this:
    
    - `NavbarToggler` → `HeaderMobileOnlyActions`
    - `Navbar` → removed without a replacement
    - `Navbar variant="dialog"` → `HeaderDialog`, stands outside `Header`
    - `NavbarClose` → removed without a replacement
    - `HeaderActions` → `HeaderDesktopOnlyActions`
    - `HeaderBackdrop` → removed without a replacement
    - `Nav` → `HeaderNav`
    - `NavItem` → `HeaderNavItem`
    - `NavLink` → `HeaderLink`
    - `Header__text` → removed without a replacement
    
    Please refer back to these instructions or reach out to our team
    if you encounter any issues during migration.

--- packages/web-react/src/components/Header/deprecated/stories/NavLinkProps.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import NavLink from '../NavLink';
-import Props from '../../../../../docs/stories/Props';
-
-const Story = () => <Props component={NavLink} />;
-
-export default Story;
