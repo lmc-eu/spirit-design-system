b5638cee56aacda0bb854f42fe692b111d0276cd
literat <tomas@litera.me>
2023-07-19 16:05:22 +0200

    BREAKING CHANGE(web-react): Remove align prop in ModalFooter
      ## Migration guide
    
    Use `alignmentX` instead of `align` prop on ModalFooter component.
    
    - `<ModalFooter align="right" />` → `<ModalFooter alignmentX="right" />`
    
    Please refer back to these instructions or reach out to our team
    if you encounter any issues during migration.

--- packages/web-react/src/components/Modal/ModalFooter.tsx
+++ packages/web-react/src/components/Modal/ModalFooter.tsx
@@ -1,24 +1,14 @@
 import React from 'react';
 import classNames from 'classnames';
-import { useStyleProps, useDeprecationMessage } from '../../hooks';
+import { useStyleProps } from '../../hooks';
 import { ModalFooterProps } from '../../types';
 import { useModalStyleProps } from './useModalStyleProps';
 
 const ModalFooter = (props: ModalFooterProps) => {
-  const { children, alignmentX = 'right', align = 'right', description, ...restProps } = props;
+  const { children, alignmentX = 'right', description, ...restProps } = props;
 
-  const { classProps } = useModalStyleProps({ footerAlignment: alignmentX || align });
+  const { classProps } = useModalStyleProps({ footerAlignment: alignmentX });
   const { styleProps, props: otherProps } = useStyleProps(restProps);
-
-  useDeprecationMessage({
-    method: 'property',
-    trigger: !!align,
-    componentName: 'ModalFooter',
-    propertyProps: {
-      deprecatedName: 'align',
-      newName: 'alignmentX',
-    },
-  });
 
   return (
     <footer {...otherProps} {...styleProps} className={classNames(classProps.footer.root, styleProps.className)}>
