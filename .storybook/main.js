e58eae8ed018757ee35114489165c394ee0c08ca
literat <tomas@litera.me>
2023-08-16 17:31:08 +0200

    Chore(web-react): Migrate Storybook to Vite builder
      * more performant
      * less dependencies

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,5 @@
-const path = require('path');
+const { resolve } = require('path');
+const { mergeConfig } = require('vite');
 
 module.exports = {
   stories: [
@@ -9,62 +10,28 @@ module.exports = {
     '@storybook/addon-links',
     '@storybook/addon-docs',
     '@storybook/addon-essentials',
-    '@whitespace/storybook-addon-html',
   ],
   features: {
     storyStoreV7: true,
   },
   core: {
-    builder: {
-      name: 'webpack5',
-      options: {
-        lazyCompilation: true,
-        fsCache: true,
-      }
-    },
+    builder: '@storybook/builder-vite'
   },
-  webpackFinal: async (config) => {
-    config.module.rules.push({
-      test: /\.scss$/,
-      use: [
-        'style-loader',
-        'css-loader',
-        {
-          loader: 'sass-loader',
-          options: {
-            sassOptions: {
-              includePaths: [
-                path.resolve(__dirname, '../node_modules'),
-                path.resolve(__dirname, '../packages/design-tokens/src/scss'),
-              ],
-            },
+
+  async viteFinal(config) {
+    // Merge custom configuration into the default config
+    return mergeConfig(config, {
+      css: {
+        postcss: resolve(__dirname, 'config'),
+        preprocessorOptions: {
+          scss: {
+            includePaths: [
+              resolve(__dirname, '../node_modules'),
+              resolve(__dirname, '../node_modules/@lmc-eu/spirit-design-tokens/src/scss'),
+            ],
           },
         },
-      ],
-    });
-
-    config.module.rules.push({
-      test: /\.m?js/,
-      resolve: {
-        fullySpecified: false,
       },
     });
-
-    /**
-     * Enable import of SVG files like React Components
-     * eg.: import Icon from 'icon.svg'
-     * and use as <Icon />
-     */
-    // Default rule for images /\.(svg|ico|jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|cur|ani|pdf)(\?.*)?$/
-    const fileLoaderRule = config.module.rules.find(rule => rule.test && rule.test.test('.svg'));
-    fileLoaderRule.exclude = /\.svg$/;
-
-    config.module.rules.unshift({
-      test: /\.svg$/,
-      enforce: 'pre',
-      use: ['@svgr/webpack'],
-    });
-
-    return config;
   },
 };
