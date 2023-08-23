a6dc3af32f63351859ac8f4fc143e43e473e3204
literat <tomas@litera.me>
2023-08-23 22:41:05 +0200

    Test(web-react): Find `goBackTitle` in Breadcrumbs refs #DS-834

--- packages/web-react/src/components/Breadcrumbs/__tests__/Breadcrumbs.test.tsx
+++ packages/web-react/src/components/Breadcrumbs/__tests__/Breadcrumbs.test.tsx
@@ -1,4 +1,6 @@
 import '@testing-library/jest-dom';
+import { render } from '@testing-library/react';
+import React from 'react';
 import { classNamePrefixProviderTest } from '../../../../tests/providerTests/classNamePrefixProviderTest';
 import { stylePropsTest } from '../../../../tests/providerTests/stylePropsTest';
 import { restPropsTest } from '../../../../tests/providerTests/restPropsTest';
@@ -10,4 +12,20 @@ describe('Breadcrumbs', () => {
   stylePropsTest(Breadcrumbs);
 
   restPropsTest(Breadcrumbs, 'nav');
+
+  it('should render breadcrumbs with go back title', () => {
+    const dom = render(
+      <Breadcrumbs
+        items={[
+          { title: 'test', url: '/test' },
+          { title: 'test_2', url: '/test_2' },
+        ]}
+        goBackTitle="test_title"
+      />,
+    );
+
+    const element = dom.container.querySelectorAll('ol > li > a')[0] as HTMLElement;
+
+    expect(element).toHaveTextContent('test_title');
+  });
 });
--- packages/web-react/src/components/Breadcrumbs/demo/index.tsx
+++ packages/web-react/src/components/Breadcrumbs/demo/index.tsx
@@ -1,14 +1,41 @@
+// Because there is no `dist` directory during the CI run
+/* eslint-disable import/no-extraneous-dependencies, import/extensions, import/no-unresolved */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment, import/extensions, import/no-unresolved
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore: No declaration file
+import icons from '@lmc-eu/spirit-icons/dist/icons';
 import DocsSection from '../../../../docs/DocsSections';
-import Breadcrumbs from './Breadcrumbs';
+import Breadcrumbs from '../Breadcrumbs';
+
+import { IconsProvider } from '../../../context';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <DocsSection title="Default">
-      <Breadcrumbs {...Breadcrumbs.args} goBackTitle="Back" />
+    <DocsSection title="Breadcrumbs">
+      <IconsProvider value={icons}>
+        <Breadcrumbs
+          items={[
+            {
+              title: 'Root',
+              url: '#rootUrl',
+            },
+            {
+              title: 'Category',
+              url: '#categoryUrl',
+            },
+            {
+              title: 'Subcategory',
+              url: '#subcategoryUrl',
+            },
+            {
+              title: 'Current page',
+              url: '#currentUrl',
+            },
+          ]}
+          goBackTitle="Back"
+        />
+      </IconsProvider>
     </DocsSection>
   </React.StrictMode>,
 );
