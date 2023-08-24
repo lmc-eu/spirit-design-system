0733ec009a0b292c1b1165707340b88884192859
literat <tomas@litera.me>
2023-08-24 11:34:34 +0200

    Style(web-react): Declare missing `prettier` module for types
      * installing @types/prettier or prettier itself did not help
      * second options is to use `skipLibCheck: true`

--- packages/web-react/src/global.d.ts
+++ packages/web-react/src/global.d.ts
@@ -6,3 +6,15 @@ declare module '*.md?raw' {
 interface Window {
   console: Console;
 }
+
+/**
+ * Declaring `prettier` module to fix following problem
+ *
+ * ../../node_modules/@storybook/components/dist/index.d.ts:5:35
+ * error TS7016: Could not find a declaration file for module 'prettier'. '/spirit-design-system/node_modules/prettier/index.js' implicitly has an 'any' type.
+ *  Try `npm i --save-dev @types/prettier` if it exists or add a new declaration (.d.ts) file containing `declare module 'prettier';`
+ * 5 import { BuiltInParserName } from 'prettier';
+ */
+declare module 'prettier' {
+  export type BuiltInParserName = unknown;
+}
