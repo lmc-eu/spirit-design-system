da8efe0016715f4e16228c16e38037199c604124
Pavel Klibani <pavel@techloop.io>
2023-08-30 17:56:59 +0200

    Docs(web-react): Add Accordion demo #DS-890
    Accordion demo in web-react is now same as demo in web and web-twig

---
+++ packages/web-react/src/components/Accordion/demo/AccordionBasic.tsx
@@ -0,0 +1,92 @@
+import React, { useState } from 'react';
+import { AccordionOpenStateType } from '../../../types';
+import { Link } from '../../Link';
+import { Pill } from '../../Pill';
+import Accordion from '../Accordion';
+import AccordionContent from '../AccordionContent';
+import AccordionHeader from '../AccordionHeader';
+import AccordionItem from '../AccordionItem';
+import toggleValueByType from './toggleValueByType';
+
+const AccordionBasic = () => {
+  const [openState, setOpenState] = useState<AccordionOpenStateType>('AccordionItemExample0');
+
+  const toggle = (id: string) => {
+    setOpenState(toggleValueByType(id, openState));
+  };
+
+  return (
+    <Accordion open={openState} toggle={toggle}>
+      <AccordionItem id="AccordionItemExample0">
+        <AccordionHeader id="AccordionItemExample0Header" for="AccordionItemExample0Content">
+          Accordion Item no. 1
+        </AccordionHeader>
+        <AccordionContent id="AccordionItemExample0Content" labelledById="AccordionItemExample0Header">
+          Sit amet interdum, accumsan dolor sit amet posuere vel arcu mauris placerat non mauris, non sed vitae
+          curabitur odio leo. Dignissim tristique, consequat vel arcu et nisi odio leo pretium accumsan condimentum at
+          sem, mauris aenean aliquet enim. Neque sapien, volutpat erat id nunc facilisis eget ipsum phasellus, tellus
+          ultricies sollicitudin ligula. Sem proin, nibh maximus donec nec commodo molestie nulla sapien nec commodo,
+          commodo et fermentum et. Mauris posuere, mi orci et nisi et iaculis lorem fringilla sed mauris auctor, lorem
+          tempus a pulvinar felis scelerisque. Suscipit vivamus, elit vel arcu lorem fringilla finibus quis sit amet
+          ligula convallis, consectetur potenti aenean efficitur.
+        </AccordionContent>
+      </AccordionItem>
+      <AccordionItem id="AccordionItemExample1">
+        <AccordionHeader
+          id="AccordionItemExample1Header"
+          for="AccordionItemExample1Content"
+          slot={
+            <>
+              <Link href="/">Link</Link>
+              <Pill color="selected">3</Pill>
+            </>
+          }
+        >
+          Accordion Item no. 2
+        </AccordionHeader>
+        <AccordionContent id="AccordionItemExample1Content" labelledById="AccordionItemExample1Header">
+          Non suspendisse, maximus suscipit tortor non mauris bibendum felis scelerisque bibendum, nam augue scelerisque
+          non nulla. Erat nec, integer nec egestas integer consequat cursus sed porttitor, dolor sit amet lorem ipsum
+          consectetur porta. Condimentum urna, suspendisse mauris ligula duis id vivamus quis odio eget, integer ornare
+          fermentum et vehicula. Consequat bibendum, dui fusce gravida iaculis urna integer vitae id, ante purus nullam
+          et nisl. Accumsan arcu, nunc nulla faucibus purus vivamus facilisis augue, volutpat convallis eget suscipit.
+          Tellus nunc ut enim et, urna fusce pulvinar fusce et mauris donec, vitae odio morbi risus aliquet. et.
+        </AccordionContent>
+      </AccordionItem>
+      <AccordionItem id="AccordionItemExample2">
+        <AccordionHeader id="AccordionItemExample2Header" for="AccordionItemExample2Content">
+          Accordion Item no. 3
+        </AccordionHeader>
+        <AccordionContent id="AccordionItemExample2Content" labelledById="AccordionItemExample2Header">
+          Sit amet interdum, accumsan dolor sit amet posuere vel arcu mauris placerat non mauris, non sed vitae
+          curabitur odio leo. Dignissim tristique, consequat vel arcu et nisi odio leo pretium accumsan condimentum at
+          sem, mauris aenean aliquet enim. Neque sapien, volutpat erat id nunc facilisis eget ipsum phasellus, tellus
+          ultricies sollicitudin ligula. Sem proin, nibh maximus donec nec commodo molestie nulla sapien nec commodo,
+          commodo et fermentum et. Mauris posuere, mi orci et nisi et iaculis lorem fringilla sed mauris auctor, lorem
+          tempus a pulvinar felis scelerisque. Suscipit vivamus, elit vel arcu lorem fringilla finibus quis sit amet
+          ligula convallis, consectetur potenti aenean efficitur.
+        </AccordionContent>
+      </AccordionItem>
+      <AccordionItem id="AccordionItemExample3">
+        <AccordionHeader
+          id="AccordionItemExample3Header"
+          for="AccordionItemExample3Content"
+          slot={<Pill color="selected">3</Pill>}
+        >
+          Augue iaculis, quis ante sapien aliquam aliquam non cursus, vestibulum nunc ipsum maximus. Libero sed non
+          nulla, condimentum lorem ipsum molestie integer curabitur rutrum curabitur, tellus pulvinar libero tempus
+        </AccordionHeader>
+        <AccordionContent id="AccordionItemExample3Content" labelledById="AccordionItemExample3Header">
+          Non suspendisse, maximus suscipit tortor non mauris bibendum felis scelerisque bibendum, nam augue scelerisque
+          non nulla. Erat nec, integer nec egestas integer consequat cursus sed porttitor, dolor sit amet lorem ipsum
+          consectetur porta. Condimentum urna, suspendisse mauris ligula duis id vivamus quis odio eget, integer ornare
+          fermentum et vehicula. Consequat bibendum, dui fusce gravida iaculis urna integer vitae id, ante purus nullam
+          et nisl. Accumsan arcu, nunc nulla faucibus purus vivamus facilisis augue, volutpat convallis eget suscipit.
+          Tellus nunc ut enim et, urna fusce pulvinar fusce et mauris donec, vitae odio morbi risus aliquet. et.
+        </AccordionContent>
+      </AccordionItem>
+    </Accordion>
+  );
+};
+
+export default AccordionBasic;
--- packages/web-react/src/components/Accordion/demo/index.tsx
+++ packages/web-react/src/components/Accordion/demo/index.tsx
@@ -5,27 +5,19 @@ import ReactDOM from 'react-dom/client';
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment, import/extensions, import/no-unresolved
 // @ts-ignore: No declaration file
 import icons from '@lmc-eu/spirit-icons/dist/icons';
-import { IconsProvider } from '../../../context';
 import DocsSection from '../../../../docs/DocsSections';
-import Accordion from './Accordion';
-import AccordionStayOpen from './AccordionStayOpen';
-import AccordionUncontrolled from './AccordionUncontrolled';
-import AccordionUncontrolledStayOpen from './AccordionUncontrolledStayOpen';
+import { IconsProvider } from '../../../context';
+import AccordionBasic from './AccordionBasic';
+import AccordionOneItemOpenAtATime from './AccordionOneItemOpenAtATime';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <IconsProvider value={icons}>
-      <DocsSection title="Default">
-        <Accordion />
+      <DocsSection title="Basic usage">
+        <AccordionBasic />
       </DocsSection>
-      <DocsSection title="Stay Open">
-        <AccordionStayOpen />
-      </DocsSection>
-      <DocsSection title="Uncontrolled">
-        <AccordionUncontrolled />
-      </DocsSection>
-      <DocsSection title="Uncontrolled Stay Open">
-        <AccordionUncontrolledStayOpen />
+      <DocsSection title="One item open at a time">
+        <AccordionOneItemOpenAtATime />
       </DocsSection>
     </IconsProvider>
   </React.StrictMode>,
