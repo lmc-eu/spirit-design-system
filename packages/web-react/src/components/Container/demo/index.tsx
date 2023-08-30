2feb7821d3225a4b8680499860d58c0245895a07
Pavel Klibani <pavel@techloop.io>
2023-08-30 21:45:52 +0200

    Docs(web-react): Add Container demo #DS-894
    Container demo in web-react is now same as demo in web and web-twig

--- packages/web-react/src/components/Container/demo/index.tsx
+++ packages/web-react/src/components/Container/demo/index.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import DocsSection from '../../../../docs/DocsSections';
-import { LayoutContainer } from '../Container.stories';
+import ContainerDefault from './ContainerDefault';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <DocsSection title="Default">
-      <LayoutContainer {...LayoutContainer.args} />
-    </DocsSection>
+    <ContainerDefault />
   </React.StrictMode>,
 );
