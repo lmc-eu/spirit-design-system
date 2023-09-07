b978ee4b41ef6a5663a97293eb78fc5ca9bd5880
Pavel Klibani <pavel@techloop.io>
2023-09-07 16:17:29 +0200

    Feat(web-react): Introduce HelperText component #DS-886

--- packages/web-react/src/components/Field/index.ts
+++ packages/web-react/src/components/Field/index.ts
@@ -1,3 +1,5 @@
 export * from './ValidationText';
+export * from './HelperText';
 export * from './useValidationText';
 export { default as ValidationText } from './ValidationText';
+export { default as HelperText } from './HelperText';
