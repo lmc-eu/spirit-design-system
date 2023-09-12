340613db655c35a344654619305b113c108e147d
Pavel Klibani <pavel@techloop.io>
2023-09-12 11:32:20 +0200

    Docs(web-react): Add hasStack and stackAlignment to DocsSection comp
    Add same props and functionality as in the web-twig component.

--- packages/web-react/docs/DocsSections.tsx
+++ packages/web-react/docs/DocsSections.tsx
@@ -3,11 +3,13 @@ import { Tag } from '../src/components';
 
 interface DocsSectionProps {
   children: ReactNode;
+  hasStack?: boolean;
+  stackAlignment?: 'start' | 'center' | 'end';
   tag?: string;
   title: string;
 }
 
-const DocsSection = ({ children, title, tag }: DocsSectionProps) => (
+const DocsSection = ({ children, hasStack = true, stackAlignment = 'start', title, tag }: DocsSectionProps) => (
   <section className="docs-Section">
     <h2 className="docs-Heading">
       {title}
@@ -17,11 +19,13 @@ const DocsSection = ({ children, title, tag }: DocsSectionProps) => (
         </Tag>
       )}
     </h2>
-    {children}
+    {hasStack ? <div className={`docs-Stack docs-Stack--${stackAlignment}`}>{children}</div> : children}
   </section>
 );
 
 DocsSection.defaultProps = {
+  hasStack: true,
+  stackAlignment: 'start',
   tag: '',
 };
 
