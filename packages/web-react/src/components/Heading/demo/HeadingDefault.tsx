41dac122145a595712f25cf51a26c8873d7a77a7
Pavel Klibani <pavel@techloop.io>
2023-09-13 14:20:39 +0200

    Docs(web-react): Add Heading demo #DS-899
    Heading demo in web-react is now same as demo in web and web-twig

---
+++ packages/web-react/src/components/Heading/demo/HeadingDefault.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Heading from '../Heading';
+
+const HeadingDefault = () => (
+  <>
+    <Heading>Heading</Heading>
+    <Heading elementType="h2">Heading H2</Heading>
+  </>
+);
+
+export default HeadingDefault;
