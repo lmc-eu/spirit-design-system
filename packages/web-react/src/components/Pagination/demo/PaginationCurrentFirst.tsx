32c33c1d04725c159a3b746781b06029d01e79a6
Pavel Klibani <pavel@techloop.io>
2023-09-16 19:52:51 +0200

    Docs(web-react): Add Pagination demo DS-903
    - Pagination demo in web-react is now same as demo in web and web-twig

--- packages/web-react/src/components/Pagination/demo/PaginationCurrentFirst.tsx
+++ packages/web-react/src/components/Pagination/demo/PaginationCurrentFirst.tsx
@@ -1,36 +1,30 @@
 import React from 'react';
-import { ComponentStory } from '@storybook/react';
-import { SpiritPaginationProps } from '../../../types';
 import Pagination from '../Pagination';
 import PaginationItem from '../PaginationItem';
 import PaginationLink from '../PaginationLink';
 import PaginationLinkNext from '../PaginationLinkNext';
 
-const Story: ComponentStory<typeof Pagination> = (args: SpiritPaginationProps) => <Pagination {...args} />;
+const PaginationCurrentFirst = () => (
+  <Pagination>
+    <PaginationItem>
+      <PaginationLink href="#" isCurrent accessibilityLabel="Current Page, Page 1" pageNumber={1} />
+    </PaginationItem>
+    <PaginationItem>
+      <PaginationLink href="#" accessibilityLabel="Go to Page 2" pageNumber={2} />
+    </PaginationItem>
+    <PaginationItem>
+      <PaginationLink href="#" accessibilityLabel="Go to Page 3" pageNumber={3} />
+    </PaginationItem>
+    <PaginationItem>
+      <PaginationLink href="#" accessibilityLabel="Go to Page 4" pageNumber={4} />
+    </PaginationItem>
+    <PaginationItem>
+      <PaginationLink href="#" accessibilityLabel="Go to Page 5" pageNumber={5} />
+    </PaginationItem>
+    <PaginationItem>
+      <PaginationLinkNext href="#" />
+    </PaginationItem>
+  </Pagination>
+);
 
-Story.args = {
-  children: (
-    <>
-      <PaginationItem>
-        <PaginationLink href="#" isCurrent accessibilityLabel="Current Page, Page 1" pageNumber={1} />
-      </PaginationItem>
-      <PaginationItem>
-        <PaginationLink href="#" accessibilityLabel="Go to Page 2" pageNumber={2} />
-      </PaginationItem>
-      <PaginationItem>
-        <PaginationLink href="#" accessibilityLabel="Go to Page 3" pageNumber={3} />
-      </PaginationItem>
-      <PaginationItem>
-        <PaginationLink href="#" accessibilityLabel="Go to Page 4" pageNumber={4} />
-      </PaginationItem>
-      <PaginationItem>
-        <PaginationLink href="#" accessibilityLabel="Go to Page 5" pageNumber={5} />
-      </PaginationItem>
-      <PaginationItem>
-        <PaginationLinkNext href="#" />
-      </PaginationItem>
-    </>
-  ),
-};
-
-export default Story;
+export default PaginationCurrentFirst;
