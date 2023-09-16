54aef510be5ac0901b8f744e07751566a3875400
Pavel Klibani <pavel@techloop.io>
2023-09-16 20:41:29 +0200

    Docs(web-react): Add Spinner demo #DS-907
    - Spinner demo in web-react is now same as demo in web and web-twig

---
+++ packages/web-react/src/components/Spinner/demo/SpinnerIconProps.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Spinner from '../Spinner';
+
+const SpinnerIconProps = () => (
+  <>
+    <Spinner boxSize="36" />
+
+    <Spinner title="Spinner title" />
+  </>
+);
+
+export default SpinnerIconProps;
