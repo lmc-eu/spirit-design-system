6e8fdb44bbc59ee2072a1b3892c46e2cd3f5678f
Pavel Klibani <pavel@techloop.io>
2023-09-16 20:07:40 +0200

    Docs(web-react): Add Pill demo #DS-904
    - Pill demo in web-react is now same as demo in web and web-twig

--- packages/web-react/src/components/Pill/demo/index.tsx
+++ packages/web-react/src/components/Pill/demo/index.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import DocsSection from '../../../../docs/DocsSections';
-import Pill from './Pill';
 import PillColors from './PillColors';
+import PillLongText from './PillLongText';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <DocsSection title="Default">
-      <Pill {...Pill.args} />
-    </DocsSection>
     <DocsSection title="Colors">
       <PillColors />
+    </DocsSection>
+    <DocsSection title="Long Text">
+      <PillLongText />
     </DocsSection>
   </React.StrictMode>,
 );
