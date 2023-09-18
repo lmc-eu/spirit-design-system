998c252bba671ebdbbc0cad0f0d6cf34cb257667
Pavel Klibani <pavel@techloop.io>
2023-09-18 14:29:02 +0200

    Docs(web): Add Tooltip demo #DS-914
    - Tooltip demo in web is now same as demo in web-twig and web-react

---
+++ packages/web/src/scss/components/Tooltip/dismissible-tooltip.mjs
@@ -0,0 +1,18 @@
+import Tooltip from '../../../js/Tooltip';
+
+const myTooltipEl = document.getElementById('my-dismissible-tooltip2');
+const myTooltip = new Tooltip(myTooltipEl);
+
+if (!window.localStorage.getItem('my-tooltip')) {
+  myTooltip.show();
+}
+
+document.getElementById('my-dismissible-button').addEventListener('click', () => {
+  myTooltip.show();
+  window.localStorage.removeItem('my-tooltip');
+});
+
+document.getElementById('my-dismissible-tooltip2-button').addEventListener('click', () => {
+  myTooltip.hide();
+  window.localStorage.setItem('my-tooltip', true);
+});
--- packages/web/src/scss/components/Tooltip/floating-ui-example.mjs
+++ packages/web/src/scss/components/Tooltip/floating-ui-example.mjs
@@ -1,7 +1,7 @@
 // To fully understand Floating UI and its options, please consult Floating UI docs:
 // @see https://floating-ui.com
 
-import { autoUpdate, computePosition, flip } from 'https://cdn.skypack.dev/@floating-ui/dom@0.5.4';
+import { autoUpdate, computePosition, flip } from 'https://cdn.skypack.dev/@floating-ui/dom@1.5.3';
 
 const button = document.getElementById('my-button');
 const tooltip = document.getElementById('my-advanced-tooltip');
@@ -15,7 +15,8 @@ const cleanup = autoUpdate(button, tooltip, () => {
       top: `${y}px`,
       left: `${x}px`,
     });
-    tooltip.dataset.placement = placement;
+
+    tooltip.dataset.spiritPlacement = placement;
   });
 });
 
