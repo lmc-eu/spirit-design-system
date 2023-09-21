bdc2ee7eb63d8b1e870241eb78158582e23fc600
Adam Kudrna <adam.kudrna@lmc.eu>
2023-09-21 11:39:33 +0200

    Docs(web, web-twig, web-react): Introduce header with navigation to development preview

--- apps/web-twig-demo/src/Controller/ComponentsController.php
+++ apps/web-twig-demo/src/Controller/ComponentsController.php
@@ -10,6 +10,31 @@ class ComponentsController extends AbstractController
     #[Route('/components/{component}', name: 'component_view')]
     public function show(string $component): Response
     {
-        return $this->render(sprintf('@components/%s/%s.stories.twig', $component, $component));
+        return $this->render(sprintf('@components/%s/%s.stories.twig', $component, $component),  [ 'components' => $this->getWebTwigComponents() ]);
+    }
+
+    // TODO Refactor to abstract controller
+    /**
+     * @return Array<string>
+     */
+    private function getWebTwigComponents(): array
+    {
+        $spiritWebTwigBundleComponentsPath = 'spirit-web-twig-bundle/src/Resources/components';
+
+        $directories = new \DirectoryIterator('../../' . $spiritWebTwigBundleComponentsPath);
+        $components = [];
+
+        /** @var SplFileInfo $file */
+        foreach ($directories as $fileinfo) {
+            // display only directories where stories exists
+            if ($fileinfo->isDir() && !$fileinfo->isDot() && in_array('stories', scandir($fileinfo->getPathname()))) {
+                $components[] = $fileinfo->getBasename();
+            }
+        }
+
+        // sort them alphabetically
+        sort($components);
+
+        return $components;
     }
 }
