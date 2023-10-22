d8368d1e37dd4cedb49a57722cd1e68cb8599f1b
literat <tomas@litera.me>
2023-10-22 22:01:44 +0200

    Feat(web-react): Introduce UncontrolledPagination component

---
+++ packages/web-react/src/components/Pagination/__tests__/UncontrolledPagination.test.tsx
@@ -0,0 +1,53 @@
+import { fireEvent, render, screen } from '@testing-library/react';
+import React from 'react';
+import UncontrolledPagination from '../UncontrolledPagination';
+
+describe('UncontrolledPagination', () => {
+  const onPageChange = jest.fn();
+
+  beforeEach(() => {
+    onPageChange.mockClear();
+  });
+
+  it('renders pagination items with test page selected', () => {
+    render(
+      <UncontrolledPagination accessibilityLabel="test page" totalPages={10} defaultPage={5} onChange={onPageChange} />,
+    );
+
+    const items = screen.getAllByRole('button');
+    expect(items).toHaveLength(2);
+
+    const selectedPageItem = screen.getByText('test page 5').parentElement;
+    expect(selectedPageItem).toHaveClass('Pagination__link Pagination__link--current');
+  });
+
+  it('renders disabled items for the first and last page', () => {
+    const { container } = render(
+      <UncontrolledPagination accessibilityLabel="test page" totalPages={10} defaultPage={1} onChange={onPageChange} />,
+    );
+
+    const items = screen.getAllByRole('button');
+    expect(items).toHaveLength(1);
+
+    const firstPageItem = screen.getByText('test page 1').parentElement;
+    const lastItemPage = container.querySelector('.Button--square');
+
+    expect(firstPageItem).toHaveClass('Pagination__link Pagination__link--current');
+    expect(lastItemPage).toHaveClass('Button Button--secondary Button--medium Button--square');
+  });
+
+  it('calls the onPageChange function when an item is clicked', () => {
+    render(
+      <UncontrolledPagination accessibilityLabel="test page" totalPages={10} defaultPage={5} onChange={onPageChange} />,
+    );
+
+    const items = screen.getAllByRole('button');
+    expect(items).toHaveLength(2);
+
+    const nextPageItem = screen.getByText('test page 6');
+
+    fireEvent.click(nextPageItem);
+
+    expect(onPageChange).toHaveBeenCalledWith(6);
+  });
+});
--- packages/web-react/src/components/Pagination/index.ts
+++ packages/web-react/src/components/Pagination/index.ts
@@ -1,8 +1,10 @@
 export { default as Pagination } from './Pagination';
+export { default as PaginationButtonLink } from './PaginationButtonLink';
 export { default as PaginationItem } from './PaginationItem';
 export { default as PaginationLink } from './PaginationLink';
-export { default as PaginationButtonLink } from './PaginationButtonLink';
-export { default as PaginationLinkPrevious } from './PaginationLinkPrevious';
 export { default as PaginationLinkNext } from './PaginationLinkNext';
-export * from './usePaginationStyleProps';
+export { default as PaginationLinkPrevious } from './PaginationLinkPrevious';
+export * from './UncontrolledPagination';
+export { default as UncontrolledPagination } from './UncontrolledPagination';
 export * from './constants';
+export * from './usePaginationStyleProps';
