44e9a59a5bef733cfa2e2353f967070170e5ad56
literat <tomas@litera.me>
2023-10-23 15:27:38 +0200

    Style(repo): Disable stylelint rule `declaration-block-no-redundant-longhand-properties`
      * it can lead to less readable code

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -2,5 +2,10 @@ module.exports = {
   extends: [
     '@lmc-eu/stylelint-config',
     'stylelint-config-prettier'
-  ]
+  ],
+  rules: {
+    // This rule can lead to less readable code
+    // @see: https://github.com/stylelint/stylelint-config-recommended/issues/14
+    'declaration-block-no-redundant-longhand-properties': null,
+  }
 }
