a7f3c1d0517748b74f58fb33951f4ebb53136d44
literat <tomas@litera.me>
2023-11-05 08:52:15 +0100

    Feat(web-react): Introduce `useIsomorphicLayoutEffect` hook
      * avoid using `useLayoutEffect` hook on server side
      * @see https://gist.github.com/gaearon/e7d97cdf38a2907924ea12e4ebdf3c85
      * @see https://usehooks-ts.com/react-hook/use-isomorphic-layout-effect
      * @see https://medium.com/@alexandereardon/uselayouteffect-and-ssr-192986cdcf7a

---
+++ packages/web-react/src/hooks/useIsomorphicLayoutEffect.ts
@@ -0,0 +1,9 @@
+/**
+ * This file fixes the problem of using useLayoutEffect hook on the server side.
+ * @see https://gist.github.com/gaearon/e7d97cdf38a2907924ea12e4ebdf3c85
+ * @see https://medium.com/@alexandereardon/uselayouteffect-and-ssr-192986cdcf7a
+ */
+/* eslint-disable no-restricted-imports */
+import { useEffect, useLayoutEffect } from 'react';
+
+export const useIsomorphicLayoutEffect = typeof window !== 'undefined' ? useLayoutEffect : useEffect;
