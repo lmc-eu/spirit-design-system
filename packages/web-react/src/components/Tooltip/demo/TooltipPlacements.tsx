81d9952be09599acb2c8780b8e6227ab5dede53d
Adam Kudrna <adam.kudrna@lmc.eu>
2023-11-14 11:43:15 +0100

    Docs(web,web-twig,web-react): Make use of new `GridItem` component in `Tooltip` placement examples

--- packages/web-react/src/components/Tooltip/demo/TooltipPlacements.tsx
+++ packages/web-react/src/components/Tooltip/demo/TooltipPlacements.tsx
@@ -1,7 +1,7 @@
 import React, { ChangeEvent, useState } from 'react';
 import DocsBox from '../../../../docs/DocsBox';
 import { PlacementDictionaryType } from '../../../types';
-import { Grid } from '../../Grid';
+import { Grid, GridItem } from '../../Grid';
 import { Radio } from '../../Radio';
 import Tooltip from '../Tooltip';
 import TooltipWrapper from '../TooltipWrapper';
@@ -20,7 +20,7 @@ const TooltipPlacements = () => {
         UNSAFE_className="mx-auto"
         UNSAFE_style={{ alignItems: 'center', justifyItems: 'center', maxWidth: '30rem' }}
       >
-        <div style={{ gridRow: 1, gridColumn: 2 }}>
+        <GridItem columnStart={2} rowStart={1}>
           <Radio
             name="placement"
             isChecked={placement === 'top-left'}
@@ -48,8 +48,8 @@ const TooltipPlacements = () => {
             label="top-right"
             value="top-right"
           />
-        </div>
-        <div style={{ gridRow: 3, gridColumn: 2 }}>
+        </GridItem>
+        <GridItem columnStart={2} rowStart={3}>
           <Radio
             name="placement"
             isChecked={placement === 'bottom-left'}
@@ -77,8 +77,12 @@ const TooltipPlacements = () => {
             label="bottom-right"
             value="bottom-right"
           />
-        </div>
-        <div style={{ display: 'flex', flexDirection: 'column', gridRow: 2, gridColumn: 1, justifySelf: 'start' }}>
+        </GridItem>
+        <GridItem
+          columnStart={1}
+          rowStart={2}
+          UNSAFE_style={{ display: 'flex', flexDirection: 'column', justifySelf: 'start' }}
+        >
           <Radio
             name="placement"
             isChecked={placement === 'left-top'}
@@ -106,8 +110,12 @@ const TooltipPlacements = () => {
             label="left-bottom"
             value="left-bottom"
           />
-        </div>
-        <div style={{ display: 'flex', flexDirection: 'column', gridRow: 2, gridColumn: 3, justifySelf: 'end' }}>
+        </GridItem>
+        <GridItem
+          columnStart={3}
+          rowStart={2}
+          UNSAFE_style={{ display: 'flex', flexDirection: 'column', justifySelf: 'end' }}
+        >
           <Radio
             name="placement"
             isChecked={placement === 'right-top'}
@@ -135,8 +143,8 @@ const TooltipPlacements = () => {
             label="right-bottom"
             value="right-bottom"
           />
-        </div>
-        <div style={{ gridRow: 2, gridColumn: 2 }}>
+        </GridItem>
+        <GridItem columnStart={2} rowStart={2}>
           <TooltipWrapper UNSAFE_style={{ margin: '5rem auto' }}>
             <DocsBox UNSAFE_className="px-900 py-900">
               Click
@@ -144,7 +152,7 @@ const TooltipPlacements = () => {
             </DocsBox>
             <Tooltip placement={placement as PlacementDictionaryType}>{placement}</Tooltip>
           </TooltipWrapper>
-        </div>
+        </GridItem>
       </Grid>
     </form>
   );
