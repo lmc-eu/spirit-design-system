8572077e05e8957ca84802b50b33bd488fa553cd
literat <tomas@litera.me>
2023-11-15 13:16:31 +0100

    Test(web-twig): Use httpS instead of http in SVG namespace

--- packages/web-twig/src/DependencyInjection/SpiritWebTwigExtension.php
+++ packages/web-twig/src/DependencyInjection/SpiritWebTwigExtension.php
@@ -12,7 +12,7 @@ use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 /**
  * This is the class that loads and manages your bundle configuration
  *
- * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
+ * To learn more see {@link https://symfony.com/doc/current/cookbook/bundles/extension.html}
  */
 class SpiritWebTwigExtension extends Extension
 {
