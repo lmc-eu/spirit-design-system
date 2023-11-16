4ccaf169a815d7da6cc5d9814d55ad259e088b1e
Pavel Klibani <pavel@techloop.io>
2023-11-16 14:01:46 +0100

    Test(web-react): Tabs tests for styleProps #DS-977

--- packages/web-react/src/components/Tabs/__tests__/TabItem.test.tsx
+++ packages/web-react/src/components/Tabs/__tests__/TabItem.test.tsx
@@ -2,11 +2,14 @@ import '@testing-library/jest-dom';
 import { fireEvent, render, waitFor } from '@testing-library/react';
 import React from 'react';
 import { classNamePrefixProviderTest } from '../../../../tests/providerTests/classNamePrefixProviderTest';
+import { stylePropsTest } from '../../../../tests/providerTests/stylePropsTest';
 import { restPropsTest } from '../../../../tests/providerTests/restPropsTest';
 import { withTabsContext } from '../../../../tests/testUtils/withTabsContext';
 import TabItem from '../TabItem';
 
 describe('TabItem', () => {
+  stylePropsTest((props) => <TabItem forTab={1} data-testid="TabItemTestId" {...props} />, 'TabItemTestId');
+
   classNamePrefixProviderTest(() => <TabItem forTab={0} />, 'Tabs__item');
 
   restPropsTest((props) => <TabItem forTab={0} {...props} />, 'button');
