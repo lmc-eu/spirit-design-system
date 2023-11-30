f20cf85e009b0805fb0f6c6bfa9dcfb8adf50aaf
literat <tomas@litera.me>
2023-11-30 21:57:04 +0100

    Chore(repo): Introduce Omlet analysis config file

---
+++ .omletrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  "include": [
+    "packages/web-react/src/**/*.{jsx,tsx,js,ts}"
+  ],
+  "ignore": [
+    "**/demo/**/*",
+    "**/build/**/*",
+  ],
+  "workspaces": {
+    "@lmc-eu/spirit-web-react": {
+      "exports": {
+        ".": "src/index.ts",
+        "./components": "src/components/index.ts",
+        "./components/*": "src/components/*",
+      },
+      "tsconfigPath": "tsconfig.json",
+    }
+  }
+}
