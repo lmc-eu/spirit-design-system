343ab51c1fe802eff8ccdef09fc8543d282a817f
Jan Kryšpín <crishpeen@gmail.com>
2023-12-07 16:11:32 +0100

    Fix(web-react): Mock window.scrollTo  in test of useScrollControl hook

--- packages/web-react/src/hooks/__tests__/useScrollControl.test.ts
+++ packages/web-react/src/hooks/__tests__/useScrollControl.test.ts
@@ -3,10 +3,21 @@ import { MutableRefObject } from 'react';
 import { useScrollControl } from '../useScrollControl';
 
 describe('useScrollControl', () => {
+  let originalScrollTo: typeof window.scrollTo;
+
   const createMockRef = (isOpen: boolean) => ({
     current: {
       open: isOpen,
     },
+  });
+
+  beforeEach(() => {
+    originalScrollTo = window.scrollTo;
+    window.scrollTo = jest.fn();
+  });
+
+  afterEach(() => {
+    window.scrollTo = originalScrollTo;
   });
 
   it('should disable scroll when isOpen is true', () => {
@@ -24,6 +35,8 @@ describe('useScrollControl', () => {
   it('should enable scroll when isOpen is false', () => {
     const mockRef = createMockRef(false) as MutableRefObject<HTMLDialogElement | null>;
 
+    const scrollToSpy = jest.spyOn(window, 'scrollTo');
+
     act(() => {
       renderHook(() => useScrollControl(mockRef, true));
     });
@@ -32,6 +45,7 @@ describe('useScrollControl', () => {
       renderHook(() => useScrollControl(mockRef, false));
     });
 
+    expect(scrollToSpy).toHaveBeenCalledWith(0, 0);
     expect(document.body.classList.contains('is-scrolling-disabled')).toBe(false);
     expect(document.body.style.top).toBe('');
   });
