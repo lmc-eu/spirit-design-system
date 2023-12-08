75b479a156309ee801baed6eb5ecaba243076875
literat <tomas@litera.me>
2023-12-08 16:07:58 +0100

    Feat(analytics): Add a timestamp to the scanner result files

--- packages/analytics/src/constants.ts
+++ packages/analytics/src/constants.ts
@@ -1,7 +1,7 @@
 export const OUTPUT_FILENAME_PREFIX = 'spirit-analytics';
 export const OUTPUT_DIR = '.scanner';
 export const ROOT_PATH = '.';
-export const REACT_OUTPUT_FILE = 'adoption-data-react.json';
-export const TWIG_OUTPUT_FILE = './.scanner/adoption-data-twig.json';
+export const REACT_OUTPUT_FILE_NAME = 'adoption-data-react';
+export const TWIG_OUTPUT_FILE_NAME = './.scanner/adoption-data-twig';
 export const TWIG_CORE_COMPONENTS_PATH = './vendor/lmc/spirit-web-twig-bundle/src/Resources/twig-components';
 export const TWIG_CONFIG_FILE = './config/spirit-web-twig.yml';
