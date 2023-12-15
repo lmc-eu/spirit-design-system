6616257d1a03705eb82636bc2f9db43aec181f86
literat <tomas@litera.me>
2023-12-15 16:28:01 +0100

    Feat(common): Introduce `info` and `warning` utilities
    refs #DS-767

--- packages/common/constants/__tests__/environments.test.ts
+++ packages/common/constants/__tests__/environments.test.ts
@@ -1,4 +1,4 @@
-import { isTesting } from '../environments';
+import { isDevelopment, isProduction, isTesting } from '../environments';
 
 describe('environments', () => {
   const OLD_ENV = process.env;
@@ -15,6 +15,20 @@ describe('environments', () => {
     process.env = OLD_ENV;
   });
 
+  describe('isDevelopment', () => {
+    it('should return true when NODE_ENV is set to `development`', () => {
+      process.env.NODE_ENV = 'development';
+
+      expect(isDevelopment()).toBeTruthy();
+    });
+
+    it('should return false when NODE_ENV is not set to `development`', () => {
+      process.env.NODE_ENV = 'production';
+
+      expect(isDevelopment()).toBeFalsy();
+    });
+  });
+
   describe('isTesting', () => {
     it('should return true when NODE_ENV is set to `testing`', () => {
       process.env.NODE_ENV = 'testing';
@@ -26,6 +40,20 @@ describe('environments', () => {
       process.env.NODE_ENV = 'production';
 
       expect(isTesting()).toBeFalsy();
+    });
+  });
+
+  describe('isProduction', () => {
+    it('should return true when NODE_ENV is set to `production`', () => {
+      process.env.NODE_ENV = 'production';
+
+      expect(isProduction()).toBeTruthy();
+    });
+
+    it('should return false when NODE_ENV is not set to `production`', () => {
+      process.env.NODE_ENV = 'development';
+
+      expect(isProduction()).toBeFalsy();
     });
   });
 });
