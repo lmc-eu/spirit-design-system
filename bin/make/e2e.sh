8f94f2415839904da57a56a3ad1cf21dc96e6efb
literat <tomas@litera.me>
2023-12-18 11:26:30 +0100

    Deps: Update all non-major dependencies

--- bin/make/e2e.sh
+++ bin/make/e2e.sh
@@ -2,7 +2,7 @@
 
 set -o errexit
 
-PLAYWRIGHT_VERSION=1.39.0
+PLAYWRIGHT_VERSION=1.40.1
 UBUNTU_VERSION=jammy
 
 docker run --rm --network host -v $(pwd):/work/ -w /work/ -it mcr.microsoft.com/playwright:v$PLAYWRIGHT_VERSION-$UBUNTU_VERSION yarn test:e2e
