7b87c15e8fa979456181ed66a1d2e630d334f528
Jan Kryšpín <crishpeen@gmail.com>
2024-01-02 13:25:08 +0100

    Feat(demo): Add option to hide symfony toolbar using URL param

---
+++ apps/web-twig-demo/README.md
@@ -0,0 +1,12 @@
+# Web Twig Demo App
+
+This is a demo app for the [Web Twig][web-twig-pkg] package.
+You can use it to develop and test the package.
+
+## Hide Symfony Toolbar
+
+To hide the Symfony toolbar, add `HIDE_TOOLBAR` parameter to the URL.
+For example: `https://localhost:4443/?HIDE_TOOLBAR`.
+Remember that the parameter is case sensitive and is removed if you navigate to another page.
+
+[web-twig-pkg]: https://github.com/lmc-eu/spirit-design-system/tree/main/packages/web-twig
--- apps/web-twig-demo/public/index.php
+++ apps/web-twig-demo/public/index.php
@@ -5,5 +5,14 @@ use App\Kernel;
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return function (array $context) {
-    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+    $kernel = new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+    $kernel->boot();
+
+    if (isset($_GET['HIDE_TOOLBAR'])) {
+        if ($kernel->getContainer()->has('profiler')) {
+            $kernel->getContainer()->get('profiler')->disable();
+        }
+    }
+
+    return $kernel;
 };
