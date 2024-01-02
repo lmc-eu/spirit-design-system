dcb7e09f027e0926deb2fd38a3228221ae0298d1
literat <tomas@litera.me>
2024-01-02 14:10:16 +0100

    Chore(repo): Map localhost in docker to host

--- bin/make/e2e.sh
+++ bin/make/e2e.sh
@@ -5,4 +5,4 @@ set -o errexit
 PLAYWRIGHT_VERSION=1.40.1
 UBUNTU_VERSION=jammy
 
-docker run --rm --network host -v $(pwd):/work/ -w /work/ -it mcr.microsoft.com/playwright:v$PLAYWRIGHT_VERSION-$UBUNTU_VERSION yarn test:e2e
+docker run --rm --network=host --ipc=host -v $(pwd):/work/ -w /work/ -it mcr.microsoft.com/playwright:v$PLAYWRIGHT_VERSION-$UBUNTU_VERSION yarn test:e2e
