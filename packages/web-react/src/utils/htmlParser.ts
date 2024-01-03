696ab8bb22c89843aa7116927433732e04e6e6e1
literat <tomas@litera.me>
2024-01-03 21:28:56 +0100

    Refactor(web-react): Dynamically check for html parser loading

--- packages/web-react/src/utils/htmlParser.ts
+++ packages/web-react/src/utils/htmlParser.ts
@@ -1,3 +1,14 @@
-import parse from 'html-react-parser';
+let parse = null;
 
-export const htmlParser = typeof window === 'undefined' ? parse : null;
+import('html-react-parser')
+  .then((htmlReactParser) => {
+    parse = htmlReactParser;
+  })
+  .catch(() =>
+    // eslint-disable-next-line no-console
+    console.warn(
+      '`html-react-parser` is not installed and will be required in the next major version. Please install, missing peer dependency.',
+    ),
+  );
+
+export const htmlParser = typeof parse === 'function' ? parse : null;
