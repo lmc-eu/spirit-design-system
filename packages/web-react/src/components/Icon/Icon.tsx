d4fc36e4530a40edc8c3cdc760126f725e79b5ae
literat <tomas@litera.me>
2024-01-03 21:27:02 +0100

    Refactor(web-react): Check for loaded html parser otherwise use NoSsr

--- packages/web-react/src/components/Icon/Icon.tsx
+++ packages/web-react/src/components/Icon/Icon.tsx
@@ -3,16 +3,18 @@ import React from 'react';
 import { useIcon, useStyleProps } from '../../hooks';
 import { IconProps } from '../../types';
 import { htmlParser } from '../../utils/htmlParser';
+import { NoSsr } from '../NoSsr';
 
 const defaultProps = {
   ariaHidden: true,
   boxSize: 24,
 };
 
-export const Icon = (props: IconProps): JSX.Element => {
+export const Icon = (props: IconProps) => {
   const { boxSize, name, title, ariaHidden, ...restProps } = props;
   let icon = useIcon(name);
   const { styleProps, props: otherProps } = useStyleProps(restProps);
+  const isHtmlParserLoaded = typeof htmlParser === 'function';
 
   if (title) {
     icon = `<title>${title}</title>${icon}`;
@@ -26,23 +28,44 @@ export const Icon = (props: IconProps): JSX.Element => {
     );
   }
 
+  if (isHtmlParserLoaded) {
+    return (
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore: Incompatible HTMLElement and SVGSVGElement
+      <svg
+        viewBox="0 0 24 24"
+        fill="none"
+        width={boxSize}
+        height={boxSize}
+        aria-hidden={ariaHidden}
+        {...otherProps}
+        {...styleProps}
+        className={styleProps.className}
+      >
+        {/* @ts-expect-error TS2349: This expression is not callable. Type 'never' has no call signatures. */}
+        {htmlParser(icon)}
+      </svg>
+    );
+  }
+
   return (
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore: Incompatible HTMLElement and SVGSVGElement
-    <svg
-      viewBox="0 0 24 24"
-      fill="none"
-      width={boxSize}
-      height={boxSize}
-      aria-hidden={ariaHidden}
-      // @deprecated Usage of dangerouslySetInnerHTML is discouraged due to support of SSR and will be removed in the next major version.
-      {...(typeof htmlParser !== 'function' ? { dangerouslySetInnerHTML: { __html: icon } } : {})}
-      {...otherProps}
-      {...styleProps}
-      className={styleProps.className}
-    >
-      {typeof htmlParser === 'function' ? htmlParser(icon) : null}
-    </svg>
+    <NoSsr>
+      {/* eslint-disable-next-line @typescript-eslint/ban-ts-comment */}
+      {/* @ts-ignore: Incompatible HTMLElement and SVGSVGElement */}
+      <svg
+        viewBox="0 0 24 24"
+        fill="none"
+        width={boxSize}
+        height={boxSize}
+        aria-hidden={ariaHidden}
+        // @deprecated Usage of dangerouslySetInnerHTML is discouraged due to support of SSR and will be removed in the next major version.
+        // eslint-disable-next-line react/no-danger
+        dangerouslySetInnerHTML={{ __html: icon }}
+        {...otherProps}
+        {...styleProps}
+        className={styleProps.className}
+      />
+    </NoSsr>
   );
 };
 
