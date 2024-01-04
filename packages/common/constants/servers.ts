c7824b353993e6b52286e1ca08422c1af5b9e760
Jan Kryšpín <crishpeen@gmail.com>
2024-01-04 12:53:16 +0100

    Feat(common): Configure https in server constants

--- packages/common/constants/servers.ts
+++ packages/common/constants/servers.ts
@@ -12,22 +12,26 @@ const SERVERS = {
     // @see: https://vitejs.dev/config/server-options.html
     web: {
       host: 'localhost',
+      https: false,
       port: 5172,
       strictPort: true,
     },
     // @see: https://vitejs.dev/config/server-options.html
     'web-react': {
       host: 'localhost',
+      https: false,
       port: 5173,
       strictPort: true,
     },
     'web-twig': {
       host: 'localhost',
+      https: true,
       port: 4443,
     },
     // @see: https://vitejs.dev/config/server-options.html
     'form-validations': {
       host: 'localhost',
+      https: false,
       port: 5174,
       strictPort: true,
     },
@@ -39,9 +43,10 @@ const SERVERS = {
   },
 };
 
-const getDevelopmentEndpointUri = (packageName, { isDocker } = { isDocker: false }) =>
-  `http://${isDocker ? 'host.docker.internal' : SERVERS.DEVELOPMENT[packageName].host}:${
-    SERVERS.DEVELOPMENT[packageName].port
-  }`;
+const getDevelopmentEndpointUri = (packageName, { isDocker } = { isDocker: false }) => {
+  const { https, host, port } = SERVERS.DEVELOPMENT[packageName];
+
+  return `http${https ? 's' : ''}://${isDocker ? 'host.docker.internal' : host}:${port}`;
+};
 
 module.exports = { SERVERS, getDevelopmentEndpointUri };
