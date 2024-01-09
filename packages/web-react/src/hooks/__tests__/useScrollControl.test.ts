0c7a00b6c518d73389b827006a87a57adfbd3002
Jan Kryšpín <crishpeen@gmail.com>
2024-01-09 16:53:54 +0100

    Fix(web-react): Simplify useScrollControl to not scroll or set top offset #DS-1124

--- packages/web-react/src/hooks/__tests__/useScrollControl.test.ts
+++ packages/web-react/src/hooks/__tests__/useScrollControl.test.ts
@@ -3,21 +3,10 @@ import { MutableRefObject } from 'react';
 import { useScrollControl } from '../useScrollControl';
 
 describe('useScrollControl', () => {
-  let originalScrollTo: typeof window.scrollTo;
-
   const createMockRef = (isOpen: boolean) => ({
     current: {
       open: isOpen,
     },
-  });
-
-  beforeEach(() => {
-    originalScrollTo = window.scrollTo;
-    window.scrollTo = jest.fn();
-  });
-
-  afterEach(() => {
-    window.scrollTo = originalScrollTo;
   });
 
   it('should disable scroll when isOpen is true', () => {
@@ -28,14 +17,11 @@ describe('useScrollControl', () => {
     });
 
     expect(document.body.classList.contains('is-scrolling-disabled')).toBe(true);
-    expect(document.body.style.top).toMatch(/-\d+px/);
     expect(document.body.style.paddingRight).toMatch(/\d+px/);
   });
 
   it('should enable scroll when isOpen is false', () => {
     const mockRef = createMockRef(false) as MutableRefObject<HTMLDialogElement | null>;
-
-    const scrollToSpy = jest.spyOn(window, 'scrollTo');
 
     act(() => {
       renderHook(() => useScrollControl(mockRef, true));
@@ -45,8 +31,6 @@ describe('useScrollControl', () => {
       renderHook(() => useScrollControl(mockRef, false));
     });
 
-    expect(scrollToSpy).toHaveBeenCalledWith(0, 0);
     expect(document.body.classList.contains('is-scrolling-disabled')).toBe(false);
-    expect(document.body.style.top).toBe('');
   });
 });
