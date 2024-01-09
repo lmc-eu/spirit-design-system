0df3c47244f263ec41d133eb6818f1d4667d7921
Jan Kryšpín <crishpeen@gmail.com>
2024-01-09 16:53:54 +0100

    Fix(web-react): Simplify useScrollControl to not scroll or set top offset #DS-1124

--- packages/web-react/src/hooks/useScrollControl.ts
+++ packages/web-react/src/hooks/useScrollControl.ts
@@ -5,19 +5,15 @@ const CLASSNAME_SCROLLING_DISABLED = 'is-scrolling-disabled';
 const disableScroll = () => {
   const { body } = document;
   const scrollBarWidth = window.innerWidth - body.clientWidth;
-  const offsetY = window.scrollY;
 
   body.style.paddingRight = `${scrollBarWidth}px`;
-  body.style.top = `-${offsetY}px`;
   body.classList.add(CLASSNAME_SCROLLING_DISABLED);
 };
 
-const enableScroll = (offsetY: number) => {
+const enableScroll = () => {
   const { body } = document;
   body.style.paddingRight = '';
-  body.style.top = '';
   body.classList.remove(CLASSNAME_SCROLLING_DISABLED);
-  window.scrollTo(0, -offsetY);
 };
 
 export const useScrollControl = (ref: MutableRefObject<HTMLDialogElement | null>, isOpen: boolean) => {
@@ -25,8 +21,7 @@ export const useScrollControl = (ref: MutableRefObject<HTMLDialogElement | null>
     if (isOpen) {
       disableScroll();
     } else if (ref.current && !ref.current.open) {
-      const offsetY = parseFloat(document.body.style.top || '0');
-      enableScroll(offsetY);
+      enableScroll();
     }
   }, [isOpen, ref]);
 };
