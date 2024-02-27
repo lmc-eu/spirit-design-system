0ddc4fee101e1d523e173baffed857f3e8324a3a
Pavel Klibani <pavel@techloop.io>
2024-02-27 10:15:51 +0100

    Feat(web-react): Add spirit-codemod package
    - Added codemod-spirit package with first
    transform for buttonLabel->buttonText
    
    - Added test to this transform

---
+++ packages/codemods/src/transforms/v2/web-react/__testfixtures__/button-text.output.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+// @ts-ignore: No declaration -- The library is not installed; we don't need to install it for fixtures.
+import { Button } from '@lmc-eu/spirit-web-react';
+
+export const MyComponent = () => <Button buttonText="Click me" />;
