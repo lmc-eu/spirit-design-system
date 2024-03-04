d6d61f37b460637501023700dd7b416d8f4b5938
crishpeen <crishpeen@gmail.com>
2024-03-04 15:42:11 +0000

    Deploying to gh-pages from @ lmc-eu/spirit-design-system@ed4df63273f82ee5bee1ae57277ac8006102169a 🚀

--- docs/icons/react/index.ts
+++ docs/icons/react/index.ts
@@ -7,6 +7,7 @@ export {default as ChevronLeftIcon} from './ChevronLeftIcon';
 export {default as ChevronRightIcon} from './ChevronRightIcon';
 export {default as ChevronUpIcon} from './ChevronUpIcon';
 export {default as CloseIcon} from './CloseIcon';
+export {default as DangerIcon} from './DangerIcon';
 export {default as DownloadIcon} from './DownloadIcon';
 export {default as EditIcon} from './EditIcon';
 export {default as FileIcon} from './FileIcon';
