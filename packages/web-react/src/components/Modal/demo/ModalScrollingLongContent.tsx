b51354750063110c52ad04dc36d0a818556a3b3b
Adam Kudrna <adam.kudrna@lmc.eu>
2024-04-09 22:43:54 +0200

    Fix(web-react): `Dropdown` can now be placed inside a non-scrollable `Modal` #DS-939

--- packages/web-react/src/components/Modal/demo/ModalScrollingLongContent.tsx
+++ packages/web-react/src/components/Modal/demo/ModalScrollingLongContent.tsx
@@ -1,18 +1,28 @@
-import React, { useState } from 'react';
-import { Button, Modal, ModalBody, ModalDialog, ModalFooter, ModalHeader, ScrollView } from '../..';
+import React, { Ref, useState } from 'react';
+import { Button, Dropdown, Item, Modal, ModalBody, ModalDialog, ModalFooter, ModalHeader, ScrollView } from '../..';
+import { DropdownRenderProps } from '../../../types';
 
 const ModalScrollingLongContent = () => {
   const [isFirstOpen, setFirstOpen] = useState(false);
   const [isSecondOpen, setSecondOpen] = useState(false);
   const [isThirdOpen, setThirdOpen] = useState(false);
+  const [isModalWithDropdownOpen, setModalWithDropdownOpen] = useState(false);
 
   const toggleFirstModal = () => setFirstOpen(!isFirstOpen);
   const toggleSecondModal = () => setSecondOpen(!isSecondOpen);
   const toggleThirdModal = () => setThirdOpen(!isThirdOpen);
+  const toggleModalWithDropdown = () => setModalWithDropdownOpen(!isModalWithDropdownOpen);
 
   const handleFirstClose = () => setFirstOpen(false);
   const handleSecondClose = () => setSecondOpen(false);
   const handleThirdClose = () => setThirdOpen(false);
+  const handleModalWithDropdownClose = () => setModalWithDropdownOpen(false);
+
+  const dropdownTrigger = ({ trigger: { className, ref, ...restOf } }: DropdownRenderProps) => (
+    <Button color="secondary" UNSAFE_className={className} ref={ref as Ref<HTMLButtonElement>} {...restOf}>
+      Dropdown
+    </Button>
+  );
 
   return (
     <>
@@ -168,6 +178,32 @@ const ModalScrollingLongContent = () => {
           <ModalFooter>
             <Button onClick={handleThirdClose}>Primary action</Button>
             <Button color="secondary" onClick={handleThirdClose}>
+              Secondary action
+            </Button>
+          </ModalFooter>
+        </ModalDialog>
+      </Modal>
+
+      <Button onClick={toggleModalWithDropdown}>Open Non-Scrolling Modal with Dropdown</Button>
+
+      <Modal id="example-dropdown" isOpen={isModalWithDropdownOpen} onClose={handleModalWithDropdownClose}>
+        <ModalDialog isScrollable={false}>
+          <ModalHeader>Modal Title</ModalHeader>
+          <ModalBody>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam at excepturi laudantium magnam mollitia
+              perferendis reprehenderit, voluptate. Cum delectus dicta ducimus eligendi excepturi natus perferendis
+              provident unde. Eveniet, iste, molestiae?
+            </p>
+            <Dropdown id="dropdown-in-modal" renderTrigger={dropdownTrigger}>
+              <Item elementType="a" href="#" label="Action" />
+              <Item elementType="a" href="#" label="Another action" />
+              <Item elementType="a" href="#" label="Something else here" />
+            </Dropdown>
+          </ModalBody>
+          <ModalFooter description="Optional description">
+            <Button onClick={handleModalWithDropdownClose}>Primary action</Button>
+            <Button color="secondary" onClick={handleModalWithDropdownClose}>
               Secondary action
             </Button>
           </ModalFooter>
