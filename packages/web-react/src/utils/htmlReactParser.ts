7a3515497d659c38a7c417d519bd529535f26200
literat <tomas@litera.me>
2024-04-19 14:47:44 +0200

    Fix(web-react): Add backward compatibility for CJS when building to it
      * there is a problem in both packages of how they expose default export and
        how the default export is transpiled by the bundler
      * so better to check it if the function is defined on the `default` or not
    
    refs #DS-1227

--- packages/web-react/src/utils/htmlReactParser.ts
+++ packages/web-react/src/utils/htmlReactParser.ts
@@ -4,10 +4,10 @@
  *
  * @see { @link https://github.com/remarkablemark/html-react-parser/issues/1329 }
  */
-import htmlToDOM from 'html-dom-parser';
-import domToReact from 'html-react-parser/lib/dom-to-react';
+import htmlDomParser from 'html-dom-parser';
+import domToReactLib from 'html-react-parser/lib/dom-to-react';
 
-export const htmlReactParser = (html: string): ReturnType<typeof domToReact> => {
+export const htmlReactParser = (html: string): ReturnType<typeof domToReactLib> => {
   if (typeof html !== 'string') {
     throw new TypeError('First argument must be a string');
   }
@@ -15,6 +15,12 @@ export const htmlReactParser = (html: string): ReturnType<typeof domToReact> => 
   if (!html) {
     return [];
   }
+
+  // support backwards compatibility for ES Module
+  // @ts-expect-error Property 'default' does not exists on type -- exactly, we need to check it first
+  const htmlToDOM = typeof htmlDomParser.default === 'function' ? htmlDomParser.default : htmlDomParser;
+  // @ts-expect-error Property 'default' does not exists on type -- exactly, we need to check it first
+  const domToReact = typeof domToReactLib.default === 'function' ? domToReactLib.default : domToReactLib;
 
   return domToReact(htmlToDOM(html));
 };
