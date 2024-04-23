0813dcb0d5b476e5c6cbe277258e90d2f968e5c3
Pavel Klibani <pavel@techloop.io>
2024-04-23 14:25:19 +0200

    BREAKING CHANGE(web-react): Remove alert danger icon deprecation
    solves #DS-1194

--- packages/web-react/src/hooks/useIcon.ts
+++ packages/web-react/src/hooks/useIcon.ts
@@ -1,33 +1,8 @@
 import { useContext } from 'react';
-import warning from '../common/utilities/warning';
 import IconsContext from '../context/IconsContext';
 
 export const useIcon = (name: string) => {
   const icons = useContext(IconsContext);
 
-  /**
-   * @todo Remove this block when DS-1177 is resolved and return the previous code
-   *
-   * @see { @link https://jira.almacareer.tech/browse/DS-1177 }
-   *
-   * Previous code: return icons != null ? icons[name] : '';
-   */
-  const iconFallbacks: { [key: string]: string } = {
-    danger: 'warning',
-  };
-
-  if (icons != null && icons[name] != null) {
-    return icons[name];
-  }
-
-  if (icons != null && icons[name] == null) {
-    warning(
-      name !== 'danger' || iconFallbacks[name] !== 'warning',
-      'The "danger" icon is missing from your assets. It will be required in the next major version. Using "warning" as a fallback.',
-    );
-
-    return icons[iconFallbacks[name]];
-  }
-
-  return '';
+  return icons != null ? icons[name] : '';
 };
