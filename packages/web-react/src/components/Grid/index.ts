c07b296ce46a3cb28993ae077db0f28842255a0a
Jan Kryšpín <crishpeen@gmail.com>
2024-04-28 15:19:54 +0200

    BREAKING CHANGE(web-react): Remove GridSpan component #DS-1244
    See the Grid: GridSpan Component section in the web-react package Migration Guide to version 2.

--- packages/web-react/src/components/Grid/index.ts
+++ packages/web-react/src/components/Grid/index.ts
@@ -1,7 +1,5 @@
 export * from './Grid';
 export * from './GridItem';
-export * from './GridSpan';
 export * from './useGridStyleProps';
 export { default as Grid } from './Grid';
 export { default as GridItem } from './GridItem';
-export { default as GridSpan } from './GridSpan';
