863de188b2e9fdbe121aa75277afd8ddbcb70ab6
Jakub Čurda <jakub.curda@almacareer.com>
2024-05-03 12:45:24 +0200

    BREAKING CHANGE(web-react): Refactor types in Tabs #DS-935
    See the Tabs: TabItem and TabPane Props section
    in the web-react package Migration Guide to version 2.

--- packages/web-react/src/components/Tabs/Tabs.tsx
+++ packages/web-react/src/components/Tabs/Tabs.tsx
@@ -1,13 +1,6 @@
 import React from 'react';
-import { ChildrenProps, TabId, TransferProps } from '../../types';
-import { TabsProvider, TabsToggler } from './TabContext';
-
-export interface TabsProps extends ChildrenProps, TransferProps {
-  selectedTab: TabId;
-  toggle: TabsToggler;
-  // eslint-disable-next-line react/require-default-props
-  onSelectionChange?: (tabId: TabId) => void;
-}
+import { TabsProvider } from './TabContext';
+import { TabsProps } from '../../types';
 
 const Tabs = ({ children, selectedTab, toggle: selectTab, onSelectionChange }: TabsProps): JSX.Element => (
   <TabsProvider value={{ selectedTabId: selectedTab, selectTab, onSelectionChange }}>{children}</TabsProvider>
