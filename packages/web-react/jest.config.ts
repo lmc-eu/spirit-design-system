55bb122e351c805590a0ec87c9f029d075534e2e
literat <tomas@litera.me>
2024-05-15 16:51:38 +0200

    Test(web-react): Use jest-config-spirit common preset

---
+++ packages/web-react/jest.config.ts
@@ -0,0 +1,14 @@
+const config = {
+  preset: 'jest-config-spirit/jsdom',
+
+  /**
+   * @todo Move this configuration to the `jest-config-spirit` preset
+   *
+   * @see { @link https://github.com/lmc-eu/spirit-design-system/issues/1413 }
+   */
+  // A list of paths to modules that run some code to configure or set up the testing framework before each test file in the suite is executed.
+  // https://jestjs.io/docs/configuration#setupfilesafterenv-array
+  setupFilesAfterEnv: ['<rootDir>/config/jest/setupTestingLibrary.ts'],
+};
+
+export default config;
