b9a7c2d0c4e05c601081fa8166d77e793ce2e3fd
Jan Kryšpín <crishpeen@gmail.com>
2024-05-21 13:48:34 +0200

    BREAKING CHANGE(web-react): Turn off scrolling inside ModalDialog by default #DS-1184

--- packages/web-react/src/components/Modal/__tests__/ModalDialog.test.tsx
+++ packages/web-react/src/components/Modal/__tests__/ModalDialog.test.tsx
@@ -1,6 +1,6 @@
 import '@testing-library/jest-dom';
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { classNamePrefixProviderTest } from '../../../../tests/providerTests/classNamePrefixProviderTest';
 import { stylePropsTest } from '../../../../tests/providerTests/stylePropsTest';
 import { restPropsTest } from '../../../../tests/providerTests/restPropsTest';
@@ -14,52 +14,52 @@ describe('ModalDialog', () => {
   restPropsTest(ModalDialog, 'article');
 
   it('should render children', () => {
-    const dom = render(
+    render(
       <ModalDialog>
         <div>Test</div>
       </ModalDialog>,
     );
 
-    expect(dom.container).toHaveTextContent('Test');
+    expect(screen.getByRole('article')).toHaveTextContent('Test');
   });
 
   it('should render with custom element type', () => {
-    const dom = render(
-      <ModalDialog elementType="section">
+    render(
+      <ModalDialog elementType="main">
         <div>Test</div>
       </ModalDialog>,
     );
 
-    expect(dom.container.querySelector('section')).toBeInTheDocument();
+    expect(screen.getByRole('main')).toBeInTheDocument();
   });
 
   it('should render docked on mobile', () => {
-    const dom = render(
+    render(
       <ModalDialog isDockedOnMobile>
         <div>Test</div>
       </ModalDialog>,
     );
 
-    expect(dom.container.querySelector('.ModalDialog')).toHaveClass('ModalDialog--dockOnMobile');
+    expect(screen.getByRole('article')).toHaveClass('ModalDialog--dockOnMobile');
   });
 
   it('should render expanded on mobile', () => {
-    const dom = render(
+    render(
       <ModalDialog isExpandedOnMobile>
         <div>Test</div>
       </ModalDialog>,
     );
 
-    expect(dom.container.querySelector('.ModalDialog')).toHaveClass('ModalDialog--expandOnMobile');
+    expect(screen.getByRole('article')).toHaveClass('ModalDialog--expandOnMobile');
   });
 
-  it('should render non scrollable', () => {
-    const dom = render(
-      <ModalDialog isScrollable={false}>
+  it('should render scrollable', () => {
+    render(
+      <ModalDialog isScrollable>
         <div>Test</div>
       </ModalDialog>,
     );
 
-    expect(dom.container.querySelector('.ModalDialog')).toHaveClass('ModalDialog--nonScrollable');
+    expect(screen.getByRole('article')).toHaveClass('ModalDialog--scrollable');
   });
 });
