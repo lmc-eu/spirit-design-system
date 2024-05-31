d8527e3b4eaf2c9a673d9ec27bf930cf57f8ad98
Pavel Klibani <pavel@techloop.io>
2024-05-31 14:18:58 +0200

    Refactor(web-react): Tooltip className changes #DS-1267
    - changed internal classnames for Tooltip

--- packages/web-react/src/components/Tooltip/__tests__/Tooltip.test.tsx
+++ packages/web-react/src/components/Tooltip/__tests__/Tooltip.test.tsx
@@ -1,6 +1,6 @@
 import '@testing-library/jest-dom';
 import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
+import { render, fireEvent, screen } from '@testing-library/react';
 import { stylePropsTest } from '../../../../tests/providerTests/stylePropsTest';
 import { restPropsTest } from '../../../../tests/providerTests/restPropsTest';
 import { Button } from '../../Button';
@@ -19,46 +19,41 @@ describe('Tooltip', () => {
     const onToggle = () => null;
     const open = true;
 
-    const dom = render(
+    render(
       <Tooltip id={id} isOpen={open} onToggle={onToggle}>
         <TooltipTrigger>{triggerText}</TooltipTrigger>
         <TooltipPopover>{popoverText}</TooltipPopover>
       </Tooltip>,
     );
 
-    const triggerElement = dom.container.querySelector(`#${id}`) as HTMLElement;
-    const popoverElement = dom.container.querySelector('[data-spirit-element="tooltip"]') as HTMLElement;
-
-    expect(triggerElement.textContent).toBe(triggerText);
-    expect(popoverElement.textContent).toBe(popoverText);
+    expect(screen.getByRole('button')).toHaveTextContent(triggerText);
+    expect(screen.getByRole('tooltip')).toHaveTextContent(popoverText);
   });
 
   it('should be opened', () => {
     const onToggle = jest.fn();
 
-    const dom = render(
+    render(
       <Tooltip id={id} isOpen onToggle={onToggle}>
         <TooltipTrigger elementType={Button}>trigger</TooltipTrigger>
         <TooltipPopover>{popoverText}</TooltipPopover>
       </Tooltip>,
     );
-    const element = dom.container.querySelector('.Tooltip') as HTMLElement;
 
-    expect(element).not.toHaveClass('is-hidden');
+    expect(screen.getByRole('tooltip')).not.toHaveClass('is-hidden');
   });
 
   it('should call toggle function', () => {
     const onToggle = jest.fn();
 
-    const dom = render(
+    render(
       <Tooltip id={id} isOpen={false} onToggle={onToggle}>
         <TooltipTrigger elementType={Button}>trigger</TooltipTrigger>
         <TooltipPopover>Hello World</TooltipPopover>
       </Tooltip>,
     );
-    const trigger = dom.container.querySelector(`button#${id}`) as HTMLElement;
 
-    fireEvent.click(trigger);
+    fireEvent.click(screen.getByRole('button'));
 
     expect(onToggle).toHaveBeenCalled();
   });
