4ddc8c0c7355fa423c5bb9270f60b11669feff9c
Pavel Klibani <pavel@techloop.io>
2024-05-31 14:18:58 +0200

    Refactor(web-react): Tooltip className changes #DS-1267
    - changed internal classnames for Tooltip

--- packages/web-react/src/components/Tooltip/useTooltipStyleProps.ts
+++ packages/web-react/src/components/Tooltip/useTooltipStyleProps.ts
@@ -10,7 +10,7 @@ export interface UseTooltipStyleProps extends TooltipProps {}
 export interface UseTooltipStylePropsReturn {
   classProps: {
     rootClassName: string;
-    wrapperClassName: string;
+    popoverClassName: string;
     arrowClassName: string;
     closeButtonClassName: string;
   };
@@ -20,27 +20,25 @@ export interface UseTooltipStylePropsReturn {
 export const useTooltipStyleProps = (props: Omit<UseTooltipStyleProps, omittedProps>): UseTooltipStylePropsReturn => {
   const { isDismissible, isOpen, ...modifiedProps } = props;
   const tooltipClass = useClassNamePrefix('Tooltip');
-  const tooltipWrapperClass = `${tooltipClass}Wrapper`;
-  const arrowClass = `${tooltipClass}__arrow`;
-  const closeButtonClass = `${tooltipClass}__close`;
-  const rootDismissibleClass = `${tooltipClass}--dismissible`;
+  const tooltipPopoverClass = `${tooltipClass}Popover`;
+  const arrowClass = `${tooltipPopoverClass}__arrow`;
+  const closeButtonClass = `${tooltipPopoverClass}__close`;
+  const rootDismissibleClass = `${tooltipPopoverClass}--dismissible`;
   const rootHiddenClass = 'is-hidden';
 
   const isHiddenClass = useMemo(() => isOpen === false, [isOpen]);
 
-  const tooltipClassName = classNames(tooltipClass, {
+  const tooltipPopoverClassName = classNames(tooltipPopoverClass, {
     [rootDismissibleClass]: isDismissible,
     [rootHiddenClass]: isHiddenClass,
   });
-  const arrowClassName = arrowClass;
-  const closeButtonClassName = closeButtonClass;
 
   return {
     classProps: {
-      rootClassName: tooltipClassName,
-      wrapperClassName: tooltipWrapperClass,
-      arrowClassName,
-      closeButtonClassName,
+      rootClassName: tooltipClass,
+      popoverClassName: tooltipPopoverClassName,
+      arrowClassName: arrowClass,
+      closeButtonClassName: closeButtonClass,
     },
     props: modifiedProps,
   };
