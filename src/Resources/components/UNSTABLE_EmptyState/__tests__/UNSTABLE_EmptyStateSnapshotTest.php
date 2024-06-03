5d22b4b513bf97659759ad076d0096b9feddb02a
Jakub Čurda <jakub.curda@almacareer.com>
2024-06-03 10:20:25 +0200

    Feat(web-twig): Introduce unstable_EmptyState component #DS-1308

---
+++ src/Resources/components/UNSTABLE_EmptyState/__tests__/UNSTABLE_EmptyStateSnapshotTest.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Lmc\SpiritWebTwigBundle\Resources\components\UNSTABLE_EmptyState;
+
+use Lmc\SpiritWebTwigBundle\AbstractComponentSnapshotTest;
+
+class UNSTABLE_EmptyStateSnapshotTest extends AbstractComponentSnapshotTest
+{
+}
