ca291d1717f109fb4b1bb066d2249ca839ef2296
literat <tomas@litera.me>
2024-06-03 13:53:12 +0200

    Refactor(web-react): Modal with Dropdown composition use new Dropdown structure

--- packages/web-react/docs/stories/examples/ModalWithDropdown.stories.tsx
+++ packages/web-react/docs/stories/examples/ModalWithDropdown.stories.tsx
@@ -1,48 +1,54 @@
-import React, { useState, Ref } from 'react';
-import { Button, Modal, ModalDialog, ModalHeader, ModalBody, Text, Dropdown } from '../../../src/components';
-import { DropdownRenderProps } from '../../../src/types';
+import React from 'react';
+import {
+  Button,
+  Dropdown,
+  DropdownPopover,
+  DropdownTrigger,
+  Modal,
+  ModalBody,
+  ModalDialog,
+  ModalHeader,
+  Text,
+} from '../../../src/components';
+import { useToggle } from '../../../src/hooks';
 
 export default {
   title: 'Examples/Compositions/Modals',
 };
 
-const dropdownTrigger = ({ isOpen, trigger: { className, ref, ...restOf } }: DropdownRenderProps) => (
-  <Button UNSAFE_className={className} ref={ref as Ref<HTMLButtonElement>} {...restOf}>
-    Dropdown is {isOpen ? 'open' : 'closed'}
-  </Button>
-);
-
 export const ModalWithDropdown = () => {
-  const [isOpen, setOpen] = useState(false);
-
-  const toggleModal = () => setOpen(!isOpen);
+  const [isModalOpen, toggleModal] = useToggle(false);
+  const [isDropdownOpen, toggleDropdown] = useToggle(false);
 
   const handleClose = () => {
-    setOpen(false);
+    toggleModal();
   };
 
   return (
     <>
-      <Button onClick={toggleModal} aria-expanded={isOpen} aria-controls="ModalExample">
-        {isOpen ? 'Close' : 'Open'} Modal
+      <Button onClick={toggleModal} aria-expanded={isModalOpen} aria-controls="ModalExample">
+        {isModalOpen ? 'Close' : 'Open'} Modal
       </Button>
-      <Modal id="ModalExample" isOpen={isOpen} onClose={handleClose}>
+      <Modal id="ModalExample" isOpen={isModalOpen} onClose={handleClose}>
         <ModalDialog>
           <ModalHeader />
           <ModalBody>
-            <Dropdown renderTrigger={dropdownTrigger}>
-              <a href="#info" className="d-flex mb-400">
-                <Text UNSAFE_className="ml-300">Information</Text>
-              </a>
-              <a href="#link" className="d-flex mb-400">
-                <Text UNSAFE_className="ml-300">More links</Text>
-              </a>
-              <a href="#profile" className="d-flex mb-400">
-                <Text UNSAFE_className="ml-300">Profile</Text>
-              </a>
-              <a href="#help" className="d-flex">
-                <Text UNSAFE_className="ml-300">Help</Text>
-              </a>
+            <Dropdown id="modal-with-dropdown" isOpen={isDropdownOpen} onToggle={toggleDropdown}>
+              <DropdownTrigger elementType="button">Dropdown is {isDropdownOpen ? 'open' : 'closed'}</DropdownTrigger>
+              <DropdownPopover>
+                <a href="#info" className="d-flex mb-400">
+                  <Text UNSAFE_className="ml-300">Information</Text>
+                </a>
+                <a href="#link" className="d-flex mb-400">
+                  <Text UNSAFE_className="ml-300">More links</Text>
+                </a>
+                <a href="#profile" className="d-flex mb-400">
+                  <Text UNSAFE_className="ml-300">Profile</Text>
+                </a>
+                <a href="#help" className="d-flex">
+                  <Text UNSAFE_className="ml-300">Help</Text>
+                </a>
+              </DropdownPopover>
             </Dropdown>
           </ModalBody>
         </ModalDialog>
