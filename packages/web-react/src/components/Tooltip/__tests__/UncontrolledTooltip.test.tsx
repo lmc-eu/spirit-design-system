caae0f2900e7b0832742467546f819c96e821027
Pavel Klibani <pavel@techloop.io>
2024-06-03 14:13:29 +0200

    Test(web-react): Update test for tooltip
    - updated to new structure using 'screen' function

--- packages/web-react/src/components/Tooltip/__tests__/UncontrolledTooltip.test.tsx
+++ packages/web-react/src/components/Tooltip/__tests__/UncontrolledTooltip.test.tsx
@@ -1,6 +1,6 @@
 import '@testing-library/jest-dom';
-import React from 'react';
 import { render, screen } from '@testing-library/react';
+import React from 'react';
 import { stylePropsTest } from '../../../../tests/providerTests/stylePropsTest';
 import { restPropsTest } from '../../../../tests/providerTests/restPropsTest';
 import UncontrolledTooltip from '../UncontrolledTooltip';
