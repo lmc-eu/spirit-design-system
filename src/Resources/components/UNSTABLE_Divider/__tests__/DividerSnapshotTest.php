732816121c54f1d3ee02226ba9d1973b5324eb90
Jan Kryšpín <crishpeen@gmail.com>
2024-06-11 10:24:24 +0200

    Feat(web-twig): Add Snapshot tests to Divider #DS-1304

---
+++ src/Resources/components/UNSTABLE_Divider/__tests__/DividerSnapshotTest.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Lmc\SpiritWebTwigBundle\Resources\components\UNSTABLE_Divider;
+
+use Lmc\SpiritWebTwigBundle\AbstractComponentSnapshotTest;
+
+class DividerSnapshotTest extends AbstractComponentSnapshotTest
+{
+}
