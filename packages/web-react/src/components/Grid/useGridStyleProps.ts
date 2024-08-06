e6fe8de2dbf0b300738a7a22baee80e740a06d5d
Jan Kryšpín <crishpeen@gmail.com>
2024-08-06 16:07:35 +0200

    Refactor(web-react): Use Spacing hook in Grid component #DS-1425

--- packages/web-react/src/components/Grid/useGridStyleProps.ts
+++ packages/web-react/src/components/Grid/useGridStyleProps.ts
@@ -1,28 +1,12 @@
 import classNames from 'classnames';
 import { CSSProperties, ElementType } from 'react';
-import { useClassNamePrefix } from '../../hooks';
+import { DirectionAxis } from '../../constants';
+import { useClassNamePrefix, useSpacingStyle } from '../../hooks';
 import { SpiritGridProps } from '../../types';
 
 interface GridCSSProperties extends CSSProperties {
   [key: string]: string | undefined | number;
 }
-
-const setStyleProperty = (styleObject: GridCSSProperties, propertyName: string, value: string | undefined) => {
-  (styleObject as Record<string, string | undefined>)[propertyName] = `var(--spirit-${value})`;
-};
-
-const setGridStyle = (styleObject: GridCSSProperties, baseVarName: string, propValue: GridCSSProperties) => {
-  if (typeof propValue === 'object' && propValue !== null) {
-    Object.keys(propValue).forEach((key) => {
-      const suffix = key === 'mobile' ? '' : `-${key}`;
-      const propName = `--${baseVarName}${suffix}`;
-      setStyleProperty(styleObject, propName, propValue[key as keyof GridCSSProperties]?.toString());
-    });
-  } else {
-    const propName = `--${baseVarName}`;
-    setStyleProperty(styleObject, propName, propValue);
-  }
-};
 
 export interface GridStyles<T> {
   /** className props */
@@ -34,44 +18,18 @@ export interface GridStyles<T> {
 }
 
 export function useGridStyleProps(props: SpiritGridProps<ElementType>): GridStyles<SpiritGridProps<ElementType>> {
-  const { cols, ...restProps } = props;
+  const { cols, spacing, spacingX, spacingY, ...restProps } = props;
 
   const gridClass = useClassNamePrefix('Grid');
 
-  const gridStyle: GridCSSProperties = {};
-
-  const typePropNames = Object.keys(props).filter((propName) => propName.startsWith('spacing'));
-
-  typePropNames.forEach((propName) => {
-    let type: string;
-
-    if (props[propName]) {
-      if (propName.startsWith('spacingX')) {
-        type = 'spacing-x';
-      } else if (propName.startsWith('spacingY')) {
-        type = 'spacing-y';
-      } else {
-        type = propName;
-      }
-
-      if (type === 'spacing') {
-        setGridStyle(
-          gridStyle,
-          `grid-spacing-x${propName.replace(type, '').toLowerCase()}`,
-          props[propName] as GridCSSProperties,
-        );
-        setGridStyle(
-          gridStyle,
-          `grid-spacing-y${propName.replace(type, '').toLowerCase()}`,
-          props[propName] as GridCSSProperties,
-        );
-      } else {
-        setGridStyle(gridStyle, `grid-${type}`, props[propName] as GridCSSProperties);
-      }
-    }
-
-    delete props[propName];
-  });
+  const gridStyle: GridCSSProperties = {
+    ...{
+      ...useSpacingStyle(spacing, 'grid', DirectionAxis.X),
+      ...useSpacingStyle(spacing, 'grid', DirectionAxis.Y),
+    },
+    ...useSpacingStyle(spacingX, 'grid', DirectionAxis.X),
+    ...useSpacingStyle(spacingY, 'grid', DirectionAxis.Y),
+  };
 
   let classes: string;
   let gridColsClass: string;
