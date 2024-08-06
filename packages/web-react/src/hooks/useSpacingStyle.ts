1ccc85f61abfb69ca3a854b1eeb07df1cf43de4b
Jan Kryšpín <crishpeen@gmail.com>
2024-08-06 16:07:35 +0200

    Refactor(web-react): Use Spacing hook in Grid component #DS-1425

--- packages/web-react/src/hooks/useSpacingStyle.ts
+++ packages/web-react/src/hooks/useSpacingStyle.ts
@@ -1,17 +1,22 @@
+import { DirectionAxis } from '../constants';
 import { SpacingProp, SpacingCSSProperties } from '../types';
 
-export function useSpacingStyle(spacing: SpacingProp | undefined, prefix: string): SpacingCSSProperties {
+export function useSpacingStyle(
+  spacing: SpacingProp | undefined,
+  prefix: string,
+  direction: undefined | (typeof DirectionAxis)[keyof typeof DirectionAxis] = undefined,
+): SpacingCSSProperties {
   const style: SpacingCSSProperties = {};
+  const directionSuffix = direction ? `-${direction}` : '';
 
   if (typeof spacing === 'object' && spacing !== null) {
     Object.keys(spacing).forEach((key) => {
-      const suffix = key === 'mobile' ? '' : `-${key}`;
-      (style as Record<string, string | undefined>)[`--${prefix}-spacing${suffix}`] = `var(--spirit-${spacing[
-        key as keyof typeof spacing
-      ]?.toString()})`;
+      const breakpointSuffix = key === 'mobile' ? '' : `-${key}`;
+      (style as Record<string, string | undefined>)[`--${prefix}-spacing${directionSuffix}${breakpointSuffix}`] =
+        `var(--spirit-${spacing[key as keyof typeof spacing]?.toString()})`;
     });
   } else if (spacing) {
-    (style as Record<string, string | undefined>)[`--${prefix}-spacing`] = `var(--spirit-${spacing})`;
+    (style as Record<string, string | undefined>)[`--${prefix}-spacing${directionSuffix}`] = `var(--spirit-${spacing})`;
   }
 
   return style;
