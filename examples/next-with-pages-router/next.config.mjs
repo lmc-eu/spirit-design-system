70b4dc097c302122e05260bb25db5fe449b4b117
Jakub Čurda <jakub.curda@almacareer.com>
2024-08-12 12:22:22 +0200

    Feat: Example NextJS app with pages router #DS-1393

---
+++ examples/next-with-pages-router/next.config.mjs
@@ -0,0 +1,20 @@
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const pathDir = dirname(fileURLToPath(import.meta.url));
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  transpilePackages: ['@lmc-eu/spirit-web-react'],
+  reactStrictMode: true,
+  sassOptions: {
+    fiber: false,
+    includePaths: [
+      path.join(pathDir, 'node_modules'),
+      path.join(pathDir, 'node_modules/@lmc-eu/spirit-design-tokens/scss'),
+      path.join(pathDir, 'src/styles'),
+    ],
+  },
+};
+
+export default nextConfig;
---
+++ examples/next-with-pages-router/src/pages/index.tsx
@@ -0,0 +1,7 @@
+import { Heading } from '@lmc-eu/spirit-web-react/components';
+
+const Home = () => {
+  return <Heading size="large">Spirit Pages App</Heading>;
+};
+
+export default Home;
