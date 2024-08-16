6c31d5e82d5a892b7f01d1efaec3b0df166f708a
Jan Kryšpín <crishpeen@gmail.com>
2024-08-16 16:42:06 +0200

    BREAKING CHANGE(design-tokens): Introduce new design tokens structure #DS-1430

---
+++ packages/design-tokens/src/js/themes/index.ts
@@ -0,0 +1,12 @@
+import * as themeLightDefault from './theme-light-default';
+import * as themeLightOnBrand from './theme-light-on-brand';
+
+// The first theme is the default theme, as the left column in the Figma table.
+export const themes = {
+  themeLightDefault: {
+    colors: themeLightDefault.colors,
+  },
+  themeLightOnBrand: {
+    colors: themeLightOnBrand.colors,
+  },
+};
