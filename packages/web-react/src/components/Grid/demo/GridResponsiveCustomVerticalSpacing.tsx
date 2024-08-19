32fbd1c5b84b6fdadcdd2c7c9a712cb304c1f76b
Jan Kryšpín <crishpeen@gmail.com>
2024-08-19 09:07:41 +0200

    Docs(web,web-react,web-twig): Switch Grid spacing tokens to v3 structure

--- packages/web-react/src/components/Grid/demo/GridResponsiveCustomVerticalSpacing.tsx
+++ packages/web-react/src/components/Grid/demo/GridResponsiveCustomVerticalSpacing.tsx
@@ -7,8 +7,8 @@ const GridResponsiveCustomVerticalSpacing = () => (
     cols={{ mobile: 2, tablet: 3, desktop: 4 }}
     spacingY={{
       mobile: 'space-100',
-      tablet: 'space-400',
-      desktop: 'space-900',
+      tablet: 'space-500',
+      desktop: 'space-1100',
     }}
   >
     <GridItemFactory items={8} label="1/2, 1/3, 1/4" />
