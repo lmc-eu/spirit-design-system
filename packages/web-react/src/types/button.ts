fa690b3cde02e17f9165c335c69f2b5c24ad8630
Jan Kryšpín <crishpeen@gmail.com>
2024-08-27 22:09:13 +0200

    BREAKING CHANGE(web-react): Remove inverted variant from Button and ButtonLink #DS-1445

--- packages/web-react/src/types/button.ts
+++ packages/web-react/src/types/button.ts
@@ -1,6 +1,6 @@
 import { ElementType } from 'react';
 import {
-  ActionColorsDictionaryType,
+  ActionButtonColorsDictionaryType,
   AriaLabelingProps,
   ChildrenProps,
   ClickEvents,
@@ -10,7 +10,7 @@ import {
   StyleProps,
 } from './shared';
 
-export type ButtonColor<C> = ActionColorsDictionaryType | EmotionColorsDictionaryType | C;
+export type ButtonColor<C> = ActionButtonColorsDictionaryType | EmotionColorsDictionaryType | C;
 export type ButtonSize<S> = SizesDictionaryType | S;
 export type ButtonType = 'button' | 'submit' | 'reset';
 
