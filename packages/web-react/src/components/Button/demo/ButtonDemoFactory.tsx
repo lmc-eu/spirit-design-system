16ade37b01f7d4bbbdba0bed53b429877876ee50
Jan Kryšpín <crishpeen@gmail.com>
2024-08-27 22:09:13 +0200

    BREAKING CHANGE(web-react): Remove inverted variant from Button and ButtonLink #DS-1445

--- packages/web-react/src/components/Button/demo/ButtonDemoFactory.tsx
+++ packages/web-react/src/components/Button/demo/ButtonDemoFactory.tsx
@@ -1,34 +1,32 @@
 import React from 'react';
 import DocsSection from '../../../../docs/DocsSections';
-import { ActionColors, EmotionColors, Sizes } from '../../../constants';
+import { ActionButtonColors, EmotionColors, Sizes } from '../../../constants';
 import { ButtonBaseProps } from '../../../types';
 import { Icon } from '../../Icon';
 import { Button } from '../Button';
 
 const ButtonDemoFactory = ({ ...props }: ButtonBaseProps) => {
   const sizes = Object.values(Sizes);
-  const actionColors = Object.values(ActionColors);
+  const actionButtonColors = Object.values(ActionButtonColors);
   const emotionColors = Object.values(EmotionColors);
-  const colors = [...actionColors, ...emotionColors];
+  const colors = [...actionButtonColors, ...emotionColors];
 
   return (
     <>
       {sizes.map((size) => (
         <DocsSection key={size} title={`Size ${size}`}>
           {colors.map((color) => (
-            <div key={color} className={`${color === 'inverted' ? 'docs-Box ' : ''}`}>
-              <div>
-                <Button size={size} color={color} elementType="a" {...props}>
-                  {`Button ${color}`}
-                </Button>{' '}
-                <Button size={size} color={color} {...props}>
-                  <Icon name="hamburger" marginRight="space-400" />
-                  Menu
-                </Button>{' '}
-                <Button size={size} color={color} isSquare {...props}>
-                  <Icon name="hamburger" />
-                </Button>
-              </div>
+            <div key={color}>
+              <Button size={size} color={color} elementType="a" {...props}>
+                {`Button ${color}`}
+              </Button>{' '}
+              <Button size={size} color={color} {...props}>
+                <Icon name="hamburger" marginRight="space-400" />
+                Menu
+              </Button>{' '}
+              <Button size={size} color={color} isSquare {...props}>
+                <Icon name="hamburger" />
+              </Button>
             </div>
           ))}
         </DocsSection>
--- packages/web-react/src/components/ButtonLink/demo/ButtonLinkDemoFactory.tsx
+++ packages/web-react/src/components/ButtonLink/demo/ButtonLinkDemoFactory.tsx
@@ -1,34 +1,32 @@
 import React from 'react';
 import DocsSection from '../../../../docs/DocsSections';
-import { ActionColors, EmotionColors, Sizes } from '../../../constants';
+import { ActionButtonColors, EmotionColors, Sizes } from '../../../constants';
 import { ButtonBaseProps } from '../../../types';
 import { Icon } from '../../Icon';
 import { ButtonLink } from '../ButtonLink';
 
 const ButtonLinkDemoFactory = ({ ...props }: ButtonBaseProps) => {
   const sizes = Object.values(Sizes);
-  const actionColors = Object.values(ActionColors);
+  const actionButtonColors = Object.values(ActionButtonColors);
   const emotionColors = Object.values(EmotionColors);
-  const colors = [...actionColors, ...emotionColors];
+  const colors = [...actionButtonColors, ...emotionColors];
 
   return (
     <>
       {sizes.map((size) => (
         <DocsSection key={size} title={`Size ${size}`}>
           {colors.map((color) => (
-            <div key={color} className={`${color === 'inverted' ? 'docs-Box ' : ''}`}>
-              <div>
-                <ButtonLink size={size} color={color} elementType="a" {...props}>
-                  {`Button ${color}`}
-                </ButtonLink>{' '}
-                <ButtonLink size={size} color={color} {...props}>
-                  <Icon name="link" marginRight="space-400" />
-                  Menu
-                </ButtonLink>{' '}
-                <ButtonLink size={size} color={color} isSquare {...props}>
-                  <Icon name="link" />
-                </ButtonLink>
-              </div>
+            <div key={color}>
+              <ButtonLink size={size} color={color} elementType="a" {...props}>
+                {`Button ${color}`}
+              </ButtonLink>{' '}
+              <ButtonLink size={size} color={color} {...props}>
+                <Icon name="link" marginRight="space-400" />
+                Menu
+              </ButtonLink>{' '}
+              <ButtonLink size={size} color={color} isSquare {...props}>
+                <Icon name="link" />
+              </ButtonLink>
             </div>
           ))}
         </DocsSection>
