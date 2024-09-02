1002d3a0b77e7ca655b1a4ecd737cdfcf7eacd6e
Jan Kryšpín <crishpeen@gmail.com>
2024-09-02 17:18:19 +0200

    Docs(web-react,web-twig): Use new spacing tokens values in ScrollView #DS-1451

--- packages/web-react/src/components/ScrollView/demo/ScrollViewHorizontalBreakout.tsx
+++ packages/web-react/src/components/ScrollView/demo/ScrollViewHorizontalBreakout.tsx
@@ -6,7 +6,7 @@ import ScrollView from '../ScrollView';
 const ScrollViewHorizontalBreakout = () => (
   <div className="breakout-container">
     <ScrollView direction="horizontal">
-      <Grid cols={4} marginBottom="space-700" UNSAFE_style={{ paddingInline: 'var(--container-padding-inline)' }}>
+      <Grid cols={4} marginBottom="space-900" UNSAFE_style={{ paddingInline: 'var(--container-padding-inline)' }}>
         {[1, 2, 3, 4].map((i) => (
           <DocsBox key={i} size="small" UNSAFE_style={{ width: '20rem', aspectRatio: '2/1' }}>
             1/4
