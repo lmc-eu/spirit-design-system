d0a4e6fea1f7eabb81c8e9d3c1b6bac7c1887d77
Jan Kryšpín <crishpeen@gmail.com>
2024-09-05 11:18:09 +0200

    Docs(web-react): Use correct typography helper and spacings in Flex #DS-1444

--- packages/web-react/src/components/Flex/demo/FlexCustomSpacing.tsx
+++ packages/web-react/src/components/Flex/demo/FlexCustomSpacing.tsx
@@ -3,7 +3,7 @@ import DocsBox from '../../../../docs/DocsBox';
 import Flex from '../Flex';
 
 const FlexCustomSpacing = () => (
-  <Flex spacing="space-1200">
+  <Flex spacing="space-1600">
     <DocsBox size="small">Item 1</DocsBox>
     <DocsBox size="small">Item 2</DocsBox>
     <DocsBox size="small">Item 3</DocsBox>
