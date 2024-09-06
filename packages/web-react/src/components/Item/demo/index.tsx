2ae9e26dcd10e944cdbf6ba260044d0478baa694
literat <tomas@litera.me>
2024-09-06 17:53:04 +0200

    Deps(repo): Add missing required dependencies by other packages

--- packages/web-react/src/components/Item/demo/index.tsx
+++ packages/web-react/src/components/Item/demo/index.tsx
@@ -1,3 +1,5 @@
+// Because there is no `dist` directory during the CI run
+/* eslint-disable import/no-extraneous-dependencies, import/extensions, import/no-unresolved */
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore: No declaration file -- @see https://jira.almacareer.tech/browse/DS-561
 import icons from '@lmc-eu/spirit-icons/icons';
