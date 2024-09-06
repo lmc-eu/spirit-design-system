731f66b25e71fa8020b2fcab38be6431d972772f
Pavel Klibani <pavel@techloop.io>
2024-09-06 12:25:36 +0200

    Feat(web-react): Prop `hasSafeAreaDisabled` changed to `hasSafeArea` in `PartnerLogo` component
    - Default value of `hasSafeArea` changed to `true`
    - Preparation for stabilization of the PartnerLogo component
    - Solves #DS-1373

--- packages/web-react/src/components/UNSTABLE_PartnerLogo/demo/PartnerLogoSafeAreaDisabled.tsx
+++ packages/web-react/src/components/UNSTABLE_PartnerLogo/demo/PartnerLogoSafeAreaDisabled.tsx
@@ -3,7 +3,7 @@ import PartnerLogoDemoFactory from './PartnerLogoDemoFactory';
 
 const PartnerLogoSafeAreaDisabled = () => (
   <div style={{ display: 'flex', flexWrap: 'wrap', gap: '24px' }}>
-    <PartnerLogoDemoFactory hasSafeAreaDisabled />
+    <PartnerLogoDemoFactory hasSafeArea={false} />
   </div>
 );
 
