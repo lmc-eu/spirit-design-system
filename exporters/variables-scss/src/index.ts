179aabcd0af8a3bc19f4fd2497c2c82bf1803467
Jakub Čurda <jakub.curda@almacareer.com>
2024-09-09 13:09:12 +0200

    Feat(variables-scss): Support for themes #DS-1436

--- exporters/variables-scss/src/index.ts
+++ exporters/variables-scss/src/index.ts
@@ -7,7 +7,7 @@ import {
   OutputTextFile,
 } from '@supernovaio/sdk-exporters';
 import { ExporterConfiguration } from '../config';
-import { generateFiles } from './generators/fileGenerator';
+import { generateOutputFilesByThemes } from './generators/fileGenerator';
 
 // https://github.com/Supernova-Studio/exporters/issues/4
 // @ts-ignore-next-line
@@ -28,20 +28,9 @@ Pulsar.export(async (sdk: Supernova, context: PulsarContext): Promise<Array<AnyO
     tokenGroups = tokenGroups.filter((tokenGroup) => tokenGroup.brandId === context.brandId);
   }
 
-  // Apply theme, if specified by the VSCode extension or pipeline configuration
-  if (context.themeId) {
-    const themes = await sdk.tokens.getTokenThemes(remoteVersionIdentifier);
-    const currentTheme = themes.find((theme) => theme.id === context.themeId);
-    if (currentTheme) {
-      tokens = await sdk.tokens.computeTokensByApplyingThemes(tokens, [currentTheme]);
-    } else {
-      // Don't allow applying theme which doesn't exist in the system
-      throw new Error('Unable to apply theme which does not exist in the system.');
-    }
-  }
-
   // Convert all color tokens to CSS variables
   const mappedTokens = new Map(tokens.map((token) => [token.id, token]));
+  const themes = await sdk.tokens.getTokenThemes(remoteVersionIdentifier);
 
   const createTextFile = (relativePath: string, fileName: string, content: string): OutputTextFile => {
     return FileHelper.createTextFile({
@@ -51,7 +40,11 @@ Pulsar.export(async (sdk: Supernova, context: PulsarContext): Promise<Array<AnyO
     });
   };
 
-  const files = generateFiles(tokens, mappedTokens, tokenGroups);
+  let textFiles: Array<OutputTextFile> = [];
+  const outputFilesData = await generateOutputFilesByThemes(tokens, mappedTokens, tokenGroups, themes, sdk);
+  textFiles = outputFilesData.map((file) => {
+    return createTextFile(file.path, file.fileName, file.content);
+  });
 
   // TODO: Only for debugging purposes, remove for production!
   const safeStringify = (obj: object) => {
@@ -75,15 +68,13 @@ Pulsar.export(async (sdk: Supernova, context: PulsarContext): Promise<Array<AnyO
     return str;
   };
 
-  return [
-    ...files.map((file) => {
-      return createTextFile('./global/', file.fileName, file.content);
-    }),
-    // TODO: Only for debugging purposes - remove for production!
+  // TODO: Only for debugging purposes - remove for production!
+  textFiles.push(
     createTextFile('./original-data/', '_original-tokens.json', safeStringify(tokens)),
-    // TODO: Only for debugging purposes - remove for production!
     createTextFile('./original-data/', '_original-groups.json', JSON.stringify(tokenGroups, null, 2)),
-  ];
+  );
+
+  return textFiles;
 });
 
 export const exportConfiguration = Pulsar.exportConfig<ExporterConfiguration>();
