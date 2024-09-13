2647c6e66cc729a23e5a1e2da7c610d2a5333b96
Jakub Čurda <jakub.curda@almacareer.com>
2024-09-13 10:50:29 +0200

    Feat(web-react): Stabilization of PartnerLogo component #DS-1443

---
+++ packages/web-react/src/components/PartnerLogo/__tests__/PartnerLogo.test.tsx
@@ -0,0 +1,40 @@
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { classNamePrefixProviderTest } from '../../../../tests/providerTests/classNamePrefixProviderTest';
+import { restPropsTest } from '../../../../tests/providerTests/restPropsTest';
+import { stylePropsTest } from '../../../../tests/providerTests/stylePropsTest';
+import { Sizes } from '../../../constants';
+import PartnerLogo from '../PartnerLogo';
+
+describe('PartnerLogo', () => {
+  classNamePrefixProviderTest(PartnerLogo, 'PartnerLogo');
+
+  stylePropsTest(PartnerLogo);
+
+  restPropsTest(PartnerLogo, 'div');
+
+  it('should render children', () => {
+    render(<PartnerLogo>Content</PartnerLogo>);
+
+    expect(screen.getByText('Content')).toBeInTheDocument();
+  });
+
+  it('should have correct className', () => {
+    render(<PartnerLogo>Content</PartnerLogo>);
+
+    expect(screen.getByText('Content')).toHaveClass('PartnerLogo PartnerLogo--medium PartnerLogo--safeArea');
+  });
+
+  it.each(Object.values(Sizes))('should return %s size', (size) => {
+    render(<PartnerLogo size={size}>Content</PartnerLogo>);
+
+    expect(screen.getByText('Content')).toHaveClass(`PartnerLogo--${size}`);
+  });
+
+  it('should render without safe area', () => {
+    render(<PartnerLogo hasSafeArea={false}>Content</PartnerLogo>);
+
+    expect(screen.getByText('Content')).not.toHaveClass('PartnerLogo--safeArea');
+  });
+});
