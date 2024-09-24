a18768df1062db2c1cf9e50ca637dcff2fbc98b6
Jan Kryšpín <crishpeen@gmail.com>
2024-09-24 16:33:21 +0200

    BREAKING CHANGE(web-react): Rename `inverted` variant of `Toast` to `neutral` #DS-1446
    Refactor ToastBarLink and ToastCloseButton.

--- packages/web-react/src/components/Toast/ToastCloseButton.tsx
+++ packages/web-react/src/components/Toast/ToastCloseButton.tsx
@@ -1,26 +1,32 @@
 'use client';
 
+import classNames from 'classnames';
 import React from 'react';
+import { useStyleProps } from '../../hooks';
 import { ToastCloseButtonProps } from '../../types';
-import { Button } from '../Button';
 import { Icon } from '../Icon';
 import { VisuallyHidden } from '../VisuallyHidden';
+import { useToastBarStyleProps } from './useToastBarStyleProps';
 
-const ToastCloseButton = ({ color, onClose, isOpen, id, closeLabel, isDismissible }: ToastCloseButtonProps) => {
+const ToastCloseButton = (props: ToastCloseButtonProps) => {
+  const { onClose, isOpen, id, closeLabel, isDismissible, ...restProps } = props;
+  const { classProps, props: modifiedProps } = useToastBarStyleProps({ ...restProps });
+  const { styleProps, props: otherProps } = useStyleProps(modifiedProps);
+
   if (isDismissible && onClose) {
     return (
-      <Button
+      <button
+        {...otherProps}
+        {...styleProps}
         type="button"
-        color={color}
+        className={classNames(classProps.close, styleProps.className)}
         onClick={onClose}
-        size="small"
-        isSquare
         aria-expanded={isOpen}
         aria-controls={id}
       >
         <Icon name="close" />
         <VisuallyHidden>{closeLabel}</VisuallyHidden>
-      </Button>
+      </button>
     );
   }
 
