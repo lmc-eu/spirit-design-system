5516d611f0a32e3bdb1a5fdf738594e979b25130
Jakub Čurda <jakub.curda@almacareer.com>
2024-09-30 13:15:50 +0200

    Fix(web-react): FileUploader document object not found when ssr #DS-1470

--- examples/next-with-pages-router/src/pages/index.tsx
+++ examples/next-with-pages-router/src/pages/index.tsx
@@ -1,5 +1,6 @@
 import { Heading } from '@lmc-eu/spirit-web-react';
+import { NextPage } from 'next';
 
-const Home = () => <Heading size="large">Spirit Pages App</Heading>;
+const Home: NextPage = () => <Heading size="large">Spirit Pages App</Heading>;
 
 export default Home;
