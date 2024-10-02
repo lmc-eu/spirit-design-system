23245ff8e27abe257a5f597ef64d720417160a05
literat <tomas@litera.me>
2024-10-02 14:04:51 +0200

    fixup! Chore(ci): Iontroduce semantic release for automated release management

--- .releaserc.mjs
+++ .releaserc.mjs
@@ -14,7 +14,7 @@ export default {
 
   plugins: [
     '@semantic-release/commit-analyzer',
-    ['semantic-release-lerna', { generateNotes: true, prepare: false, publish: false }],
+    ['semantic-release-lerna', { generateNotes: true, prepare: true, publish: true }],
     '@semantic-release/changelog',
     [
       '@semantic-release/git',
