cfcfd2ccb7d0e312b95a9a4c45d178a5d770d0b1
Jakub Čurda <jakub.curda@almacareer.com>
2024-10-03 11:29:46 +0200

    Feat(codemods): Button and ButtonLink have isSymmetrical prop instead of isSquare #DS-1484

---
+++ packages/codemods/src/transforms/v3/web-react/buttonLink-isSquare-prop-name.ts
@@ -0,0 +1,50 @@
+import { API, FileInfo } from 'jscodeshift';
+
+const transform = (fileInfo: FileInfo, api: API) => {
+  const j = api.jscodeshift;
+  const root = j(fileInfo.source);
+
+  // Find import statements for the specific module and ButtonLink specifier
+  const importStatements = root.find(j.ImportDeclaration, {
+    source: {
+      value: (value: string) => /^@lmc-eu\/spirit-web-react(\/.*)?$/.test(value),
+    },
+  });
+
+  // Check if the module is imported
+  if (importStatements.length > 0) {
+    const componentSpecifier = importStatements.find(j.ImportSpecifier, {
+      imported: {
+        type: 'Identifier',
+        name: 'ButtonLink',
+      },
+    });
+
+    // Check if ButtonLink specifier is present
+    if (componentSpecifier.length > 0) {
+      // Find ButtonLink components in the module
+      const components = root.find(j.JSXOpeningElement, {
+        name: {
+          type: 'JSXIdentifier',
+          name: 'ButtonLink',
+        },
+      });
+
+      // Rename 'isSquare' attribute to 'isSymmetrical'
+      components
+        .find(j.JSXAttribute, {
+          name: {
+            type: 'JSXIdentifier',
+            name: 'isSquare',
+          },
+        })
+        .forEach((attributePath) => {
+          attributePath.node.name.name = 'isSymmetrical';
+        });
+    }
+  }
+
+  return root.toSource();
+};
+
+export default transform;
