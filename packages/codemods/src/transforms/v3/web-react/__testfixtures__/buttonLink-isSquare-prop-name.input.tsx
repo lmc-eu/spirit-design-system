1b459c786c0acd0bb8c20244c98c4d19d7818603
Jakub Čurda <jakub.curda@almacareer.com>
2024-10-03 11:29:46 +0200

    Feat(codemods): Button and ButtonLink have isSymmetrical prop instead of isSquare #DS-1484

---
+++ packages/codemods/src/transforms/v3/web-react/__testfixtures__/buttonLink-isSquare-prop-name.input.tsx
@@ -0,0 +1,23 @@
+// @ts-ignore: No declaration -- The library is not installed; we don't need to install it for fixtures.
+import { ButtonLink } from '@lmc-eu/spirit-web-react';
+
+export const MyComponent = () => (
+  <>
+    <ButtonLink href="" isSquare>
+      Click me
+    </ButtonLink>
+    <ButtonLink elementType="div" isSquare>
+      Click me
+    </ButtonLink>
+    <ButtonLink isDisabled isSquare>
+      Click me
+    </ButtonLink>
+    <ButtonLink color="primary">Click me</ButtonLink>
+    <ButtonLink isDisabled isSquare={false}>
+      Click me
+    </ButtonLink>
+    <ButtonLink isDisabled isSquare={true}>
+      Click me
+    </ButtonLink>
+  </>
+);
