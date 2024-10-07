d6b6aa880e242f6bccaed3cf3e15241281b3b2e2
Jakub Čurda <jakub.curda@almacareer.com>
2024-10-07 15:58:16 +0200

    Fix(web-react): FileUploaderInput className  #DS-1508

--- packages/web-react/src/components/FileUploader/__tests__/FileUploaderInput.test.tsx
+++ packages/web-react/src/components/FileUploader/__tests__/FileUploaderInput.test.tsx
@@ -1,7 +1,11 @@
+import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { renderToString } from 'react-dom/server';
 import { classNamePrefixProviderTest } from '../../../../tests/providerTests/classNamePrefixProviderTest';
 import { restPropsTest } from '../../../../tests/providerTests/restPropsTest';
 import { validationTextPropsTest } from '../../../../tests/providerTests/validationTextPropsTest';
 import FileUploaderInput from '../FileUploaderInput';
+import '@testing-library/jest-dom';
 
 describe('FileUploaderInput', () => {
   classNamePrefixProviderTest(FileUploaderInput, 'FileUploaderInput');
@@ -9,4 +13,20 @@ describe('FileUploaderInput', () => {
   restPropsTest(FileUploaderInput, 'div');
 
   validationTextPropsTest(FileUploaderInput, '.FileUploaderInput__validationText');
+
+  it('should have drag-and-drop class in Client component', () => {
+    render(<FileUploaderInput id="test-uploader" name="test-uploader" label="upload" data-testid="test" />);
+
+    const dropZone = screen.getAllByTestId('test')[0];
+
+    expect(dropZone).toHaveClass('has-drag-and-drop');
+  });
+
+  it('should not have drag-and-drop class in Server component', () => {
+    const container = renderToString(
+      <FileUploaderInput id="test-uploader" name="test-uploader" label="upload" data-testid="test" />,
+    );
+
+    expect(container).not.toContain('has-drag-and-drop');
+  });
 });
