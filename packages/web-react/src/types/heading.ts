2726e469551cc22b3bbab7248874c04f5c927522
Pavel Klibani <pavel@techloop.io>
2024-10-08 15:04:40 +0200

    BREAKING CHANGE(web-react): Remove default elementType value for Heading component

--- packages/web-react/src/types/heading.ts
+++ packages/web-react/src/types/heading.ts
@@ -9,22 +9,20 @@ import {
   TransferProps,
 } from './shared';
 
-export interface HeadingElementTypeProps<T extends ElementType = 'div'> {
+export interface HeadingElementTypeProps<T extends ElementType> {
   /**
-   * The HTML element or React element used to render the Heading, e.g. 'div'.
-   *
-   * @default 'div'
+   * The HTML element or React element used to render the Heading, e.g. 'h2'.
    */
-  elementType?: T | JSXElementConstructor<unknown>;
+  elementType: T | JSXElementConstructor<unknown>;
 }
 
-export interface HeadingProps<T extends ElementType = 'div'>
+export interface HeadingProps<T extends ElementType>
   extends HeadingElementTypeProps<T>,
     ChildrenProps,
     StyleProps,
     TransferProps {}
 
-export interface SpiritHeadingProps<T extends ElementType = 'div', S = void, E = void>
+export interface SpiritHeadingProps<T extends ElementType, S = void, E = void>
   extends HeadingProps<T>,
     SizeProps<SizeExtendedDictionaryType<S>>,
     EmphasisProps<EmphasisDictionaryType<E>> {}
