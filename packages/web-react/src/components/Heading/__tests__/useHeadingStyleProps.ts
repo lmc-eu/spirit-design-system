64d9175c2150df83dd46e50cc769564d27dc9613
Pavel Klibani <pavel@techloop.io>
2024-10-08 15:04:40 +0200

    BREAKING-CHANGE(web-react): Remove default elementType value for Heading component

--- packages/web-react/src/components/Heading/__tests__/useHeadingStyleProps.ts
+++ packages/web-react/src/components/Heading/__tests__/useHeadingStyleProps.ts
@@ -1,11 +1,12 @@
 import { renderHook } from '@testing-library/react';
+import { ElementType } from 'react';
 import { SpiritHeadingProps } from '../../../types';
 import { useHeadingStyleProps } from '../useHeadingStyleProps';
 import headingSizeDataProvider from './headingSizeDataProvider';
 
 describe('useHeadingStyleProps', () => {
   it.each(headingSizeDataProvider)('should return typography heading class', (size, emphasis, expectedClassName) => {
-    const props = { size, emphasis } as SpiritHeadingProps;
+    const props = { size, emphasis } as SpiritHeadingProps<ElementType, void, void>;
     const { result } = renderHook(() => useHeadingStyleProps(props));
 
     expect(result.current.classProps).toBe(expectedClassName);
