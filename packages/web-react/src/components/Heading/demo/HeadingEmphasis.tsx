ffef619852daa1096a81668c8253aab3a6de0f4d
Pavel Klibani <pavel@techloop.io>
2024-10-08 15:04:40 +0200

    BREAKING-CHANGE(web-react): Remove default elementType value for Heading component

--- packages/web-react/src/components/Heading/demo/HeadingEmphasis.tsx
+++ packages/web-react/src/components/Heading/demo/HeadingEmphasis.tsx
@@ -3,10 +3,18 @@ import Heading from '../Heading';
 
 const HeadingEmphasis = () => (
   <>
-    <Heading emphasis="regular">Heading regular</Heading>
-    <Heading emphasis="semibold">Heading semibold</Heading>
-    <Heading emphasis="bold">Heading bold</Heading>
-    <Heading emphasis="italic">Heading italic</Heading>
+    <Heading elementType="h2" emphasis="regular">
+      Heading regular
+    </Heading>
+    <Heading elementType="h2" emphasis="semibold">
+      Heading semibold
+    </Heading>
+    <Heading elementType="h2" emphasis="bold">
+      Heading bold
+    </Heading>
+    <Heading elementType="h2" emphasis="italic">
+      Heading italic
+    </Heading>
   </>
 );
 
