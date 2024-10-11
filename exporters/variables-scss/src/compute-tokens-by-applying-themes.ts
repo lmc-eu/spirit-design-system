82eba3e6dee42d525a902b75f1738f7b07636cba
literat <tomas@litera.me>
2024-10-11 16:41:36 +0200

    Fix: It returns some data now

--- exporters/variables-scss/src/compute-tokens-by-applying-themes.ts
+++ exporters/variables-scss/src/compute-tokens-by-applying-themes.ts
@@ -1,16 +1,10 @@
 import SupernovaSDK from '@supernovaio/sdk';
-import { themesData } from './data/themes-data';
+import { themesDataBckp } from './data/themes-data-bckp';
 import { tokensDataBckp } from './data/tokens-data-bckp';
 
 const apiKey = '';
 const sdk = new SupernovaSDK.Supernova(apiKey);
 
-const allThemes = await Promise.all(
-  themesData.map(async (theme) => {
-    const themedTokens = await sdk.tokens.computeTokensByApplyingThemes(tokensDataBckp, [theme]);
+const themedTokens = await sdk.tokens.computeTokensByApplyingThemes([], tokensDataBckp, themesDataBckp);
 
-    return { themedTokens, theme };
-  }),
-);
-
-console.log(allThemes);
+console.log(themedTokens);
