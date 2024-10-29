c574570421d023b31231bf38ce67245ff763802d
Jakub Čurda <jakub.curda@almacareer.com>
2024-10-29 15:41:17 +0100

    Feat(codemods): Add the option to allow visited state in Link #DS-1501

---
+++ packages/codemods/src/transforms/v3/web-react/__testfixtures__/link-hasVisitedStyleAllowed-prop.input.tsx
@@ -0,0 +1,30 @@
+// @ts-ignore: No declaration -- The library is not installed; we don't need to install it for fixtures.
+import { Heading, Link, Text } from '@lmc-eu/spirit-web-react';
+
+export const MyComponent = () => (
+  <>
+    <Heading elementType="h1">
+      <Link>Test Link</Link>
+    </Heading>
+    <Heading elementType="h1">
+      <Link>Test Link 1</Link>
+      <Link>Test Link 2</Link>
+    </Heading>
+    <Heading elementType="h1">
+      <Link hasVisitedStyleAllowed>Test Link 1</Link>
+      <Link>Test Link 2</Link>
+    </Heading>
+    <Text>
+      <Link>Test Link</Link>
+    </Text>
+    <Heading elementType="h1">
+      <div>
+        <Link>Test Link 1</Link>
+        <Link>Test Link 2</Link>
+      </div>
+    </Heading>
+    <Heading elementType="h1">
+      <Link hasVisitedStyleAllowed={false}>Test Link</Link>
+    </Heading>
+  </>
+);
