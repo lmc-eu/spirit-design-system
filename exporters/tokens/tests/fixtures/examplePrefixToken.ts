a4d33bd9eec3e2058e8fa9d2326e66972d9ac76b
Jan Kryšpín <crishpeen@gmail.com>
2024-11-07 09:25:03 +0100

    Fix(exporter-tokens): Use css-variable-prefix token instead of token-prefix

--- exporters/tokens/tests/fixtures/examplePrefixToken.ts
+++ exporters/tokens/tests/fixtures/examplePrefixToken.ts
@@ -3,7 +3,7 @@ import { StringToken, Token, TokenType } from '@supernovaio/sdk-exporters';
 export const examplePrefixToken = new Map<string, Token>();
 examplePrefixToken.set('stringRef', {
   id: 'stringRef',
-  name: 'token-prefix',
+  name: 'css-variable-prefix',
   tokenType: TokenType.string,
   parentGroupId: '1',
   origin: null,
