55d120e6dd5bfe0e4662d170883b6dbfab1e5859
Jakub Čurda <jakub.curda@almacareer.com>
2024-11-19 13:38:31 +0100

    Fix(examples): Add mandatory elementType to examples

--- examples/next-with-app-router/src/app/page.tsx
+++ examples/next-with-app-router/src/app/page.tsx
@@ -1,6 +1,10 @@
 import { Heading } from '@lmc-eu/spirit-web-react';
 import { NextPage } from 'next';
 
-const Home: NextPage = () => <Heading size="large">Spirit App Router</Heading>;
+const Home: NextPage = () => (
+  <Heading elementType="h2" size="large">
+    Spirit App Router
+  </Heading>
+);
 
 export default Home;
