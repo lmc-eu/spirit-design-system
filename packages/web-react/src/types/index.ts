80f04eef416af5b3e7481b4ab88115b074234848
Jakub Čurda <jakub.curda@almacareer.com>
2024-12-02 11:27:20 +0100

    Feat(web-react): Introduce UNSTABLE_Header #DS-1524

--- packages/web-react/src/types/index.ts
+++ packages/web-react/src/types/index.ts
@@ -38,4 +38,5 @@ export * from './textFieldBase';
 export * from './toast';
 export * from './toggle';
 export * from './tooltip';
+export * from './unstableHeader';
 export * from './visuallyHidden';
