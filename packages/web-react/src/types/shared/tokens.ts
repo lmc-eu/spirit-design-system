4e595db5a4661390665fb67fcb36e771ed07b3ad
literat <tomas@litera.me>
2024-12-04 14:05:24 +0100

    Feat(web-react): Introduce expandable space and breakpoint token types
    refs #DS-1557

--- packages/web-react/src/types/shared/tokens.ts
+++ packages/web-react/src/types/shared/tokens.ts
@@ -3,4 +3,15 @@ import { breakpoints, spaces } from '@lmc-eu/spirit-design-tokens';
 export const BREAKPOINT_MOBILE = 'mobile';
 
 export type BreakpointToken = keyof typeof breakpoints | string;
-export type SpaceToken = `${'space-'}${Extract<keyof typeof spaces, string>}` | `${'space-'}${number}`;
+export type SpaceToken = `${'space-'}${Extract<keyof typeof spaces, string>}`;
+
+// Extendable token types
+declare global {
+  interface ExpandableTokens {
+    spaces?: Record<string, string | number>;
+    breakpoints?: Record<string, string>;
+  }
+}
+
+export type ExpandableSpaceToken = SpaceToken | `${'space-'}${Extract<keyof ExpandableTokens['spaces'], string>}`;
+export type ExpandableBreakpointToken = BreakpointToken | keyof ExpandableTokens['breakpoints'] | string;
