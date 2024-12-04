46213dceaf8605b5ddb1c0fd7b3200548bb2c746
literat <tomas@litera.me>
2024-12-04 14:06:27 +0100

    Test(examples): Try to test expandable tokens

---
+++ examples/next-with-app-router/custom-tokens.d.ts
@@ -0,0 +1,12 @@
+// custom-tokens.d.ts
+declare global {
+  interface ExpandableTokens {
+    spaces: {
+      150: '10px';
+      123: '5px';
+    };
+    breakpoints: {
+      tablet: '768px';
+    };
+  }
+}
--- examples/next-with-app-router/src/app/page.tsx
+++ examples/next-with-app-router/src/app/page.tsx
@@ -1,10 +1,15 @@
-import { Heading } from '@lmc-eu/spirit-web-react';
+import { Button, Heading } from '@lmc-eu/spirit-web-react';
 import { NextPage } from 'next';
 
 const Home: NextPage = () => (
-  <Heading elementType="h2" size="large">
-    Spirit App Router
-  </Heading>
+  <>
+    <Heading elementType="h2" size="large">
+      Spirit App Router
+    </Heading>
+    <Button color="primary" size="large" marginBottom="space-123">
+      Hello, World!
+    </Button>
+  </>
 );
 
 export default Home;
