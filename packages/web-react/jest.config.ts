e88ca5d5f9f1f5f9606895fa15acb6e99f824318
literat <tomas@litera.me>
2024-12-09 17:50:02 +0100

    Test(web-react): Set Jest to automatically clear all mocks

--- packages/web-react/jest.config.ts
+++ packages/web-react/jest.config.ts
@@ -9,6 +9,10 @@ const config = {
   // A list of paths to modules that run some code to configure or set up the testing framework before each test file in the suite is executed.
   // https://jestjs.io/docs/configuration#setupfilesafterenv-array
   setupFilesAfterEnv: ['<rootDir>/config/jest/setupTestingLibrary.ts'],
+
+  // Automatically clear mock calls, instances, contexts and results before every test.
+  // https://jestjs.io/docs/configuration#clearmocks-boolean
+  clearMocks: true,
 };
 
 export default config;
