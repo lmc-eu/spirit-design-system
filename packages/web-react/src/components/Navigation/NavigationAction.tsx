73737b6730c25c6178d966d2339d28c7be51b590
Jan Kryšpín <crishpeen@gmail.com>
2024-12-17 16:16:39 +0100

    Fix(web-react): Rename `NavigationLink` component to `NavigationAction`

--- packages/web-react/src/components/Navigation/NavigationAction.tsx
+++ packages/web-react/src/components/Navigation/NavigationAction.tsx
@@ -3,33 +3,33 @@
 import classNames from 'classnames';
 import React, { ElementType, forwardRef } from 'react';
 import { useStyleProps } from '../../hooks';
-import { PolymorphicRef, SpiritNavigationLinkProps } from '../../types';
-import { useNavigationLinkProps } from './useNavigationLinkProps';
-import { useNavigationLinkStyleProps } from './useNavigationLinkStyleProps';
+import { PolymorphicRef, SpiritNavigationActionProps } from '../../types';
+import { useNavigationActionProps } from './useNavigationActionProps';
+import { useNavigationActionStyleProps } from './useNavigationActionStyleProps';
 
-const defaultProps: Partial<SpiritNavigationLinkProps> = {
+const defaultProps: Partial<SpiritNavigationActionProps> = {
   elementType: 'a',
 };
 
 /* We need an exception for components exported with forwardRef */
-/* eslint no-underscore-dangle: ['error', { allow: ['_NavigationLink'] }] */
-const _NavigationLink = <E extends ElementType = 'a'>(
-  props: SpiritNavigationLinkProps<E>,
+/* eslint no-underscore-dangle: ['error', { allow: ['_NavigationAction'] }] */
+const _NavigationAction = <E extends ElementType = 'a'>(
+  props: SpiritNavigationActionProps<E>,
   ref: PolymorphicRef<E>,
 ): JSX.Element => {
   const propsWithDefaults = { ...defaultProps, ...props };
   const { elementType = defaultProps.elementType as ElementType, children, ...restProps } = propsWithDefaults;
   const ElementTag = propsWithDefaults.isDisabled ? 'span' : elementType;
 
-  const { navigationLinkProps } = useNavigationLinkProps(propsWithDefaults);
-  const { classProps, props: modifiedProps } = useNavigationLinkStyleProps(restProps);
+  const { navigationActionProps } = useNavigationActionProps(propsWithDefaults);
+  const { classProps, props: modifiedProps } = useNavigationActionStyleProps(restProps);
   const { styleProps, props: otherProps } = useStyleProps(modifiedProps);
 
   return (
     <ElementTag
       {...otherProps}
       {...styleProps}
-      {...navigationLinkProps}
+      {...navigationActionProps}
       className={classNames(classProps, styleProps.className)}
       ref={ref}
     >
@@ -38,6 +38,6 @@ const _NavigationLink = <E extends ElementType = 'a'>(
   );
 };
 
-const NavigationLink = forwardRef<HTMLElement, SpiritNavigationLinkProps<ElementType>>(_NavigationLink);
+const NavigationAction = forwardRef<HTMLElement, SpiritNavigationActionProps<ElementType>>(_NavigationAction);
 
-export default NavigationLink;
+export default NavigationAction;
