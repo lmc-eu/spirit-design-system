e2fdb90077949ed7c4cc09f78afd2ff5c954dbde
Jan Kryšpín <crishpeen@gmail.com>
2025-01-08 14:50:39 +0100

    Feat(web-react): Introduce `NavigationItem` subcomponent #DS-1411

--- packages/web-react/src/components/Navigation/__tests__/NavigationItem.test.tsx
+++ packages/web-react/src/components/Navigation/__tests__/NavigationItem.test.tsx
@@ -1,14 +1,27 @@
 import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
 import React from 'react';
-import { restPropsTest } from '../../../../tests/providerTests/restPropsTest';
-import { stylePropsTest } from '../../../../tests/providerTests/stylePropsTest';
+import { classNamePrefixProviderTest, restPropsTest, stylePropsTest } from '@local/tests';
 import NavigationItem from '../NavigationItem';
 
 describe('NavigationItem', () => {
+  classNamePrefixProviderTest(NavigationItem, 'NavigationItem');
+
   stylePropsTest(NavigationItem);
 
   restPropsTest(NavigationItem, 'li');
+
+  it('should have default classname', () => {
+    render(<NavigationItem>Content</NavigationItem>);
+
+    expect(screen.getByRole('listitem')).toHaveClass('NavigationItem NavigationItem--alignmentYCenter');
+  });
+
+  it('should have stretch alignment classname', () => {
+    render(<NavigationItem alignmentY="stretch">Content</NavigationItem>);
+
+    expect(screen.getByRole('listitem')).toHaveClass('NavigationItem--alignmentYStretch');
+  });
 
   it('should have correct role', () => {
     render(<NavigationItem>Content</NavigationItem>);
