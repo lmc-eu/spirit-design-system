88f2fe5e5159d0e4455b35dfdcc1d83b6a3e8511
Jan Kryšpín <crishpeen@gmail.com>
2025-01-08 14:50:39 +0100

    Feat(web-react): Introduce `NavigationItem` subcomponent #DS-1411

--- packages/web-react/src/components/Navigation/NavigationItem.tsx
+++ packages/web-react/src/components/Navigation/NavigationItem.tsx
@@ -1,16 +1,25 @@
 'use client';
 
+import classNames from 'classnames';
 import React from 'react';
+import { AlignmentYExtended } from '../../constants';
 import { useStyleProps } from '../../hooks';
 import { SpiritNavigationItemProps } from '../../types';
+import { useNavigationStyleProps } from './useNavigationStyleProps';
+
+const defaultProps: Partial<SpiritNavigationItemProps> = {
+  alignmentY: AlignmentYExtended.CENTER,
+};
 
 const NavigationItem = (props: SpiritNavigationItemProps): JSX.Element => {
-  const { children, ...restProps } = props;
+  const propsWithDefaults = { ...defaultProps, ...props };
+  const { children, ...restProps } = propsWithDefaults;
 
-  const { styleProps, props: otherProps } = useStyleProps(restProps);
+  const { classProps, props: modifiedProps } = useNavigationStyleProps(restProps);
+  const { styleProps, props: otherProps } = useStyleProps(modifiedProps);
 
   return (
-    <li {...otherProps} className={styleProps.className} style={styleProps.style}>
+    <li {...otherProps} className={classNames(classProps.item, styleProps.className)} style={styleProps.style}>
       {children}
     </li>
   );
