fadde50dd2687d3aedaf99243d4de579dbc0dc94
Jakub Čurda <jakub.curda@almacareer.com>
2025-01-31 11:20:18 +0100

    Feat(web-react): Add Pill Navigation to UNSTABLE_Header demo #DS-1672

--- packages/web-react/src/components/UNSTABLE_Header/demo/HeaderWithNavigation/MainNavigation.tsx
+++ packages/web-react/src/components/UNSTABLE_Header/demo/HeaderWithNavigation/MainNavigation.tsx
@@ -1,21 +1,36 @@
 import React from 'react';
 import { Direction } from '../../../../constants';
-import { SpiritNavigationProps } from '../../../../types';
+import { NavigationActionVariantsType, SpiritNavigationProps } from '../../../../types';
 import { Navigation, NavigationAction, NavigationItem } from '../../../Navigation';
+import { NavigationActionVariants } from '../../../Navigation/constants';
 
-export const MainNavigation = ({ direction = Direction.HORIZONTAL }: Partial<SpiritNavigationProps>) => (
+interface MainNavigationProps extends Partial<SpiritNavigationProps> {
+  variant?: NavigationActionVariantsType;
+}
+
+export const MainNavigation = ({
+  direction = Direction.HORIZONTAL,
+  variant = NavigationActionVariants.BOX,
+}: Partial<MainNavigationProps>) => (
   <Navigation
     aria-label="Main Navigation"
     direction={direction}
     UNSAFE_className={direction === Direction.HORIZONTAL ? 'd-none d-desktop-flex' : undefined}
   >
     <NavigationItem>
-      <NavigationAction href="#" isSelected>
+      <NavigationAction href="#" variant={variant} isSelected>
         Job Offers
       </NavigationAction>
     </NavigationItem>
     <NavigationItem>
-      <NavigationAction href="#">Magazine</NavigationAction>
+      <NavigationAction href="#" variant={variant}>
+        Magazine
+      </NavigationAction>
+    </NavigationItem>
+    <NavigationItem>
+      <NavigationAction href="#" variant={variant}>
+        Contact
+      </NavigationAction>
     </NavigationItem>
   </Navigation>
 );
