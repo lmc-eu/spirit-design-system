1dc9de25620bc7027a250ad7734d52f4b91bb321
Jakub Čurda <jakub.curda@almacareer.com>
2025-02-11 15:46:54 +0100

    Feat(codemods): Stabilization of Avatar #DS-1677

---
+++ packages/codemods/src/transforms/v4/web-react/__testfixtures__/unstable-avatar-component-name.output.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+// @ts-ignore: No declaration -- The library is not installed; we don't need to install it for fixtures.
+import { Avatar } from '@lmc-eu/spirit-web-react';
+
+export const MyComponent = () => (
+  <>
+    <Avatar elementType="a" href="#" size="small" aria-label="Profile of Jiří Bárta">
+      <span aria-hidden="true">JB</span>
+    </Avatar>
+  </>
+);
