f73be258c31925eb90dd16cffae50b5acf306e1e
Jakub Čurda <jakub.curda@almacareer.com>
2025-02-14 13:10:07 +0100

    Feat(web-react): Deprecate Header component #DS-1598

--- packages/web-react/src/components/Header/Header.tsx
+++ packages/web-react/src/components/Header/Header.tsx
@@ -2,7 +2,7 @@
 
 import classNames from 'classnames';
 import React from 'react';
-import { useStyleProps } from '../../hooks';
+import { useDeprecationMessage, useStyleProps } from '../../hooks';
 import { HeaderProps } from '../../types';
 import { HEADER_COLOR_DEFAULT } from './constants';
 import { useHeaderStyleProps } from './useHeaderStyleProps';
@@ -12,6 +12,14 @@ const Header = (props: HeaderProps) => {
 
   const { classProps } = useHeaderStyleProps({ color, isSimple });
   const { styleProps, props: otherProps } = useStyleProps(restProps);
+
+  useDeprecationMessage({
+    method: 'custom',
+    trigger: true,
+    componentName: 'Header',
+    customText: `The component will be removed in the next major version. Component is replaced by the current implementation of UNSTABLE_Header.
+    Please see the composition of the UNSTABLE_Header, Drawer and Navigation components.`,
+  });
 
   return (
     <header {...otherProps} className={classNames(classProps.root, styleProps.className)} style={styleProps.style}>
