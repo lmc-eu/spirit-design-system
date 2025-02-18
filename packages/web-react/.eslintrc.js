332f3ecb8eebefae33c2e2f244fd576da76529de
literat <tomas@litera.me>
2025-02-18 16:58:08 +0100

    Style(web-react): Use sharable ESlint config

--- packages/web-react/.eslintrc.js
+++ packages/web-react/.eslintrc.js
@@ -1,12 +1,10 @@
 module.exports = {
   extends: [
-    '../../.eslintrc',
+    'eslint-config-spirit',
     '@lmc-eu/eslint-config-react',
     '@lmc-eu/eslint-config-typescript',
     '@lmc-eu/eslint-config-typescript/react',
     'prettier',
-    'plugin:prettier/recommended',
-    '@lmc-eu/eslint-config-jest',
   ],
 
   parserOptions: {
@@ -14,37 +12,18 @@ module.exports = {
     project: './config/tsconfig.eslint.json',
   },
 
-  settings: {
-    'import/resolver': {
-      typescript: {},
-      node: {
-        extensions: ['.js', '.jsx', '.ts', '.tsx', '.md'],
-      },
-    },
-  },
-
-  plugins: ['promise', 'react', '@typescript-eslint', 'prettier', 'react-refresh'],
   rules: {
-    // Turn off unresolved imports because Typescript is already handling this
-    // @see: https://github.com/import-js/eslint-plugin-import/blob/main/docs/rules/no-unresolved.md#importno-unresolved
-    'import/no-unresolved': 'off',
+    // We are using default values
+    // @see { @link https://react.dev/learn/passing-props-to-a-component#specifying-a-default-value-for-a-prop React default values }
+    // @see { @link https://github.com/jsx-eslint/eslint-plugin-react/blob/master/docs/rules/require-default-props.md }
+    'react/require-default-props': 'off',
 
-    'import/extensions': [
-      'error',
-      'ignorePackages',
-      {
-        js: 'never',
-        jsx: 'never',
-        ts: 'never',
-        tsx: 'never',
-      },
-    ],
     // @see: https://github.com/ArnaudBarre/eslint-plugin-react-refresh
     'react-refresh/only-export-components': 'warn',
-    // @TODO: add to typescript config
-    'react/jsx-filename-extension': ['error', { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
+
     // we like to use props spreading for additional props in this case
     'react/jsx-props-no-spreading': 'off', // Used inside HOC, that is fine.
+
     // prefer arrow function over function expression
     'react/function-component-definition': [
       'warn',
@@ -54,7 +33,12 @@ module.exports = {
     // it is good for debugging
     // @see: https://github.com/jsx-eslint/eslint-plugin-react/blob/master/docs/rules/display-name.md
     'react/display-name': 'off',
-    // ignore UNSTABLE_ prefix in component names
+
+    /* start: allow UNSTABLE_ and UNSAFE_ prefix in component and prop names */
+    // @see: https://github.com/lmc-eu/spirit-design-system/blob/main/docs/decisions/006-experimental-code.md
+
+    // enforce PascalCase for user-defined JSX components
+    // @see: https://github.com/jsx-eslint/eslint-plugin-react/blob/master/docs/rules/jsx-pascal-case.md
     'react/jsx-pascal-case': [
       'error',
       {
@@ -62,7 +46,9 @@ module.exports = {
         ignore: ['UNSTABLE_*'],
       },
     ],
-    // ignore UNSAFE and UNSTABLE_ prefixes
+
+    // enforce camelcase naming conventions
+    // @see: https://eslint.org/docs/latest/rules/camelcase
     camelcase: [
       'error',
       {
@@ -71,10 +57,8 @@ module.exports = {
         allow: ['^UNSAFE_', '^UNSTABLE_'],
       },
     ],
-    // allow ++ in for loops
-    'no-plusplus': ['error', { allowForLoopAfterthoughts: true }],
-    // allow reassign in properties
-    'no-param-reassign': ['warn', { props: false }],
+    /* end: allow UNSTABLE_ and UNSAFE_ prefix in component and prop names */
+
     // support monorepos
     'import/no-extraneous-dependencies': [
       'error',
@@ -83,32 +67,7 @@ module.exports = {
         peerDependencies: true,
       },
     ],
-    'import/order': [
-      'error',
-      {
-        groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
-        pathGroups: [
-          {
-            pattern: '@local/**',
-            group: 'parent',
-            position: 'before',
-          },
-          {
-            pattern: '..',
-            group: 'parent',
-            position: 'after',
-          },
-        ],
-        pathGroupsExcludedImportTypes: ['builtin'],
-        alphabetize: {
-          order: 'asc',
-          caseInsensitive: true,
-        },
-        'newlines-between': 'never',
-      },
-    ],
-    // disable double quotes
-    quotes: ['warn', 'single'],
+
     // use useIsomorphicLayoutEffect instead of useLayoutEffect
     // @see: https://medium.com/@alexandereardon/uselayouteffect-and-ssr-192986cdcf7a
     'no-restricted-imports': [
@@ -120,17 +79,30 @@ module.exports = {
         message: '`useLayoutEffect` causes a warning in SSR. Use `useIsomorphicLayoutEffect`',
       },
     ],
+
     // allow empty interfaces with single extends (e.g. interface Foo extends SpiritBar {})
     // interface which extends some other interface is not considered as meaningful interface
     // we need this for meeting our component API conventions
     // @see: https://typescript-eslint.io/rules/no-empty-interface/
     '@typescript-eslint/no-empty-interface': ['error', { allowSingleExtends: true }],
   },
+
   overrides: [
     {
+      // Stories are build by `apps/storybook` and have dependencies elsewhere
       files: ['**/stories/**'],
       rules: {
         'import/no-extraneous-dependencies': 'off',
+        'jsx-a11y/anchor-is-valid': 'off',
+      },
+    },
+    {
+      // In demo files we want to use console and alert
+      files: ['**/demo/**'],
+      rules: {
+        'jsx-a11y/anchor-is-valid': 'off',
+        'no-console': 'off',
+        'no-alert': 'off',
       },
     },
     {
