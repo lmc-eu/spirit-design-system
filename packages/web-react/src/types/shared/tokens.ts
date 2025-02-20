29a141a2ea80068fdb270eb037cf3edca5042b8c
Jakub Čurda <jakub.curda@almacareer.com>
2025-02-20 11:10:46 +0100

    Feat(web-react): Types generated from custom tokens #DS-1557

--- packages/web-react/src/types/shared/tokens.ts
+++ packages/web-react/src/types/shared/tokens.ts
@@ -3,4 +3,4 @@ import { breakpoints, spaces } from '@lmc-eu/spirit-design-tokens';
 export const BREAKPOINT_MOBILE = 'mobile';
 
 export type BreakpointToken = keyof typeof breakpoints | string;
-export type SpaceToken = `${'space-'}${Extract<keyof typeof spaces, string>}` | `${'space-'}${number}`;
+export type SpaceToken = `space-${keyof typeof spaces}` | `space-${number}`;
