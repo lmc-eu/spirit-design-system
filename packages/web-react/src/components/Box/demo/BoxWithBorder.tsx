10e9b267e601f6d9db0e0a19b74cff946fd32999
Adam Kudrna <adam.kudrna@lmc.eu>
2025-02-27 11:19:07 +0100

    Fix(web-react): Remove `focus` from available border colors
    Focus color is reserved for interactive components and can only be applied in CSS.

--- packages/web-react/src/components/Box/demo/BoxWithBorder.tsx
+++ packages/web-react/src/components/Box/demo/BoxWithBorder.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
-import { BorderStyles } from '../../../constants';
 import Box from '../Box';
 
 const BoxWithBorder = () => (
   <>
-    <Box elementType="fieldset" marginBottom="space-500" borderWidth="0">
-      <legend>For demo purposes the box has custom padding</legend>
-    </Box>
+    <p>For demo purposes, the boxes have custom padding.</p>
     <Box padding="space-800" borderWidth="100">
       Without radius
     </Box>
@@ -19,10 +16,7 @@ const BoxWithBorder = () => (
     <Box padding="space-800" borderWidth="200" borderRadius="300">
       With thicker border
     </Box>
-    <Box padding="space-800" borderWidth="100" borderRadius="300" borderColor="focus">
-      With focus color
-    </Box>
-    <Box padding="space-800" borderWidth="100" borderStyle={BorderStyles.DASHED}>
+    <Box padding="space-800" borderWidth="100" borderStyle="dashed">
       With dashed border style
     </Box>
   </>
