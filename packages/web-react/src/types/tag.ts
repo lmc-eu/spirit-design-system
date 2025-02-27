24515e265c89300a28b8002dc0dbdcb22f0cedde
literat <tomas@litera.me>
2025-02-27 19:23:37 +0100

    Refactor(web-react): Fix element typing of the Tag component

--- packages/web-react/src/types/tag.ts
+++ packages/web-react/src/types/tag.ts
@@ -11,12 +11,21 @@ export type TagColor<C> = EmotionColorsDictionaryType | 'neutral' | C;
 
 export type TagSize<S> = SizeExtendedDictionaryType | S;
 
+export interface AriaTagElementTypeProps<T extends ElementType = 'span'> {
+  /**
+   * The HTML element or React element used to render the tag, e.g. 'div', 'span'.
+   *
+   * @default 'span'
+   */
+  elementType?: T;
+}
+
 export interface TagProps<E extends ElementType = 'span', C = void, S = void>
   extends ChildrenProps,
     StyleProps,
-    TransferProps {
+    TransferProps,
+    AriaTagElementTypeProps<E> {
   color?: TagColor<C>;
-  elementType?: E;
   isSubtle?: boolean;
   size?: TagSize<S>;
 }
