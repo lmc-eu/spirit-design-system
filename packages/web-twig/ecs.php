0708a6d1e6427b8c983cf9e7c8b298e114c8e058
Ondřej Machulda <ondrej.machulda@gmail.com>
2025-03-01 22:32:37 +0100

    Chore(web-twig): Replace lmc/coding-standard with almacareer/coding-standard

--- packages/web-twig/ecs.php
+++ packages/web-twig/ecs.php
@@ -2,6 +2,7 @@
 
 declare(strict_types=1);
 
+use Lmc\CodingStandard\Set\SetList;
 use PhpCsFixer\Fixer\ClassNotation\ClassAttributesSeparationFixer;
 use Symplify\EasyCodingStandard\Config\ECSConfig;
 
@@ -9,7 +10,7 @@ return ECSConfig::configure()
     ->withPaths([__DIR__ . '/src', __DIR__ . '/tests'])
     ->withRootFiles()
     ->withSets([
-        __DIR__ . '/vendor/lmc/coding-standard/ecs.php',
+        SetList::ALMACAREER,
     ])
     // Specify elements separation.
     ->withConfiguredRule(
