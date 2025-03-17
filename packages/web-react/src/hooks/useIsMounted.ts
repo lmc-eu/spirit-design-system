8412f31c1271694a71196902e2ee8049919bfc1e
literat <tomas@litera.me>
2025-03-17 14:00:35 +0100

    Refactor(web-react): Adopt useResizeObserver form the usehooks-ts library
      * remove `usehooks-ts` from dependencies
      * add `@juggle/resize-observer` polyfill which is used by the hook
    
    refs #DS-1715

---
+++ packages/web-react/src/hooks/useIsMounted.ts
@@ -0,0 +1,31 @@
+/**
+ * This implementation is a modified copy of the `usehooks-ts` library
+ *
+ * @see { @link https://usehooks-ts.com/react-hook/use-is-mounted }
+ */
+import { useCallback, useEffect, useRef } from 'react';
+
+/**
+ * Custom hook that determines if the component is currently mounted.
+ *
+ * @returns {() => boolean} A function that returns a boolean value indicating whether the component is mounted.
+ * @public
+ * @example
+ * ```tsx
+ * const isComponentMounted = useIsMounted();
+ * // Use isComponentMounted() to check if the component is currently mounted before performing certain actions.
+ * ```
+ */
+export function useIsMounted(): () => boolean {
+  const isMounted = useRef(false);
+
+  useEffect(() => {
+    isMounted.current = true;
+
+    return () => {
+      isMounted.current = false;
+    };
+  }, []);
+
+  return useCallback(() => isMounted.current, []);
+}
