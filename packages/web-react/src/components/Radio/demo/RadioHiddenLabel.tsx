4e0a018b2a84cc841e0ff9644f10138802d3f812
Pavel Klibani <pavel.klibani@almamedia.com>
2025-03-25 15:59:08 +0100

    Fix(web-react): Fix Radio demo
    - A lot of "onChange" errors in console
    - Fixed broken radio switches on click

--- packages/web-react/src/components/Radio/demo/RadioHiddenLabel.tsx
+++ packages/web-react/src/components/Radio/demo/RadioHiddenLabel.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Radio from '../Radio';
 
 const RadioHiddenLabel = () => (
-  <Radio id="radio-hidden-label" label="Radio Label" name="radioHiddenLabel" isLabelHidden />
+  <Radio id="radio-hidden-label" label="Radio Label" name="radioHiddenLabel" isLabelHidden onChange={() => {}} />
 );
 
 export default RadioHiddenLabel;
