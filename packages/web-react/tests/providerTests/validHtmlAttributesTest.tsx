d97dea9a8a9b356ffd6301e9bdc8b98904ef45f0
Jakub Čurda <jakub.curda@almacareer.com>
2025-03-26 12:48:33 +0100

    fixup! Feat(web-react): Add test for valid html attributes #DS-1711

--- packages/web-react/tests/providerTests/validHtmlAttributesTest.tsx
+++ packages/web-react/tests/providerTests/validHtmlAttributesTest.tsx
@@ -1,17 +1,32 @@
 import { render } from '@testing-library/react';
 import React from 'react';
 
+const globalAttributes = [
+  'accesskey',
+  'class',
+  'contenteditable',
+  'dir',
+  'draggable',
+  'hidden',
+  'id',
+  'lang',
+  'spellcheck',
+  'style',
+  'tabindex',
+  'title',
+  'translate',
+  'role',
+  'slot',
+  'summary',
+];
+
 const validateHTMLAttributes = (element: HTMLElement) => {
   const tagName = element.tagName.toLowerCase();
 
-  const validAttributes = Object.keys(element).filter(
-    (key) => key !== 'style' && key !== 'class' && key in document.createElement(tagName),
-  );
-
-  validAttributes.push('class', 'style');
+  const validAttributes = [...Object.keys(document.createElement(tagName)), ...globalAttributes];
 
   Array.from(element.attributes).forEach((attr) => {
-    if (!validAttributes.includes(attr.name)) {
+    if (!validAttributes.includes(attr.name) && !attr.name.startsWith('data-') && !attr.name.startsWith('aria-')) {
       throw new Error(`Invalid attribute '${attr.name}' on <${tagName}> element`);
     }
   });
@@ -23,7 +38,6 @@ const validateHTMLForComponent = (container: HTMLElement) => {
   elements.forEach(validateHTMLAttributes);
 };
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const validHtmlAttributesTest = (Component: React.ComponentType<any>, props: object = {}) => {
   test(`should render valid HTML`, () => {
     const { container } = render(<Component {...props} />);
