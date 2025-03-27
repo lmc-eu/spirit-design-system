cc7fdb063a41f994358fc4702222dfd0f52fc832
Jakub Čurda <jakub.curda@almacareer.com>
2025-03-27 11:20:16 +0100

    fixup! Feat(web-react): Add test for valid html attributes #DS-1711

--- packages/web-react/src/components/Field/__tests__/HelperText.test.tsx
+++ packages/web-react/src/components/Field/__tests__/HelperText.test.tsx
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import React from 'react';
+import { validHtmlAttributesTest } from '@local/tests';
 import '@testing-library/jest-dom';
 import HelperText from '../HelperText';
-import { validHtmlAttributesTest } from '@local/tests';
 
 describe('HelperText', () => {
   const helperText = 'Helper Text';
