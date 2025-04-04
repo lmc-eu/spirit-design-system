9230f9e76d4dbdf57444bcdfb9d54ac93103073f
Jakub Čurda <jakub.curda@almacareer.com>
2025-04-04 11:33:52 +0200

    Fix(stylelint-config): Add rule to enforce flex-start instead of start #DS-1778

--- configs/stylelint-config-spirit/rules/style.js
+++ configs/stylelint-config-spirit/rules/style.js
@@ -5,5 +5,16 @@ export default {
     // This rule can lead to less readable code
     // @see: https://github.com/stylelint/stylelint-config-recommended/issues/14
     'declaration-block-no-redundant-longhand-properties': OFF,
+    // We couldn't resolve the issue with Next.js correctly interpreting the browserslist configuration,
+    // which led to a misleading build-time warning when using "start" for "justify-content".
+    // As a workaround, we enforce this rule and recommend using "flex-start" instead.
+    'declaration-property-value-disallowed-list': [
+      {
+        'justify-content': ['start'],
+      },
+      {
+        message: 'Using "start" may trigger a warning during Next.js build. Use "flex-start" instead.',
+      },
+    ],
   },
 };
