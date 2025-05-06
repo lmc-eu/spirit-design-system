6db8ec628d596645131b8d00f536c1c8d5b8f7b4
literat <tomas@litera.me>
2025-05-06 18:17:27 +0200

    Feat(common): Remove support for web-twig package from the servers
      * we are not running e2e tests againts the web-twig demo from now

--- tests/helpers/getServerUrl.ts
+++ tests/helpers/getServerUrl.ts
@@ -2,6 +2,4 @@ import { isTesting as isTestingEnvironment } from '@lmc-eu/spirit-common/constan
 import { SERVERS, getDevelopmentEndpointUri } from '@lmc-eu/spirit-common/constants/servers';
 
 export const getServerUrl = (packageName: string) =>
-  isTestingEnvironment()
-    ? SERVERS.TESTING[packageName]
-    : getDevelopmentEndpointUri(packageName, { isDocker: packageName !== 'web-twig' });
+  isTestingEnvironment() ? SERVERS.TESTING[packageName] : getDevelopmentEndpointUri(packageName, { isDocker: true });
