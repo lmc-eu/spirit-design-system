7c3bc191b764f60a90a81731f0e3d6ad39165cb1
literat <tomas@litera.me>
2025-05-23 15:17:27 +0200

    fixup! Feat(website): Setup the app

--- apps/website/src/app/(documentation)/layout.tsx
+++ apps/website/src/app/(documentation)/layout.tsx
@@ -1,12 +1,14 @@
+'use client';
+
 import React, { ReactNode } from 'react';
 import { Header } from '@local/ui/Header';
 import { Cover } from '@local/ui/Cover';
 import { Container, Footer } from '@lmc-eu/spirit-web-react';
-import { useRouter } from 'next/router';
+import { usePathname } from 'next/navigation';
 import clsx from 'clsx';
 
 const DocumentationLayout = ({ children }: { children: ReactNode }) => {
-  const router = useRouter();
+  const pathname = usePathname();
 
   return (
     <>
@@ -17,7 +19,7 @@ const DocumentationLayout = ({ children }: { children: ReactNode }) => {
       </main>
       <Footer
         UNSAFE_className={clsx('bg-secondary mt-1200 mt-tablet-1700 pb-1200 text-center', {
-          'hide-from-visual-tests': router.asPath.includes('components'),
+          'hide-from-visual-tests': pathname.includes('components'),
         })}
       >
         <Container>© Alma Career Oy and its subsidiaries</Container>
