488003050d56251bd7740104f1811cb6a7b98f55
literat <tomas@litera.me>
2025-05-26 22:04:48 +0200

    Feat(website): Add card linking to components on home page

--- apps/website/src/app/(documentation)/page.tsx
+++ apps/website/src/app/(documentation)/page.tsx
@@ -1,10 +1,46 @@
-import { Heading } from '@lmc-eu/spirit-web-react';
+import {
+  Card,
+  CardArtwork,
+  CardBody,
+  CardFooter,
+  CardLink,
+  CardTitle,
+  Container,
+  Grid,
+  Icon,
+  Link,
+  Section,
+} from '@lmc-eu/spirit-web-react';
 import { NextPage } from 'next';
+import NextLink from 'next/link';
 
 const Home: NextPage = () => (
-  <Heading elementType="h2" size="large">
-    Spirit App Router
-  </Heading>
+  <Container>
+    <Section>
+      <Grid cols={{ mobile: 1, tablet: 2 }}>
+        <Card direction="horizontal" isBoxed>
+          <CardArtwork>
+            <Icon name="file" />
+          </CardArtwork>
+          <CardBody>
+            <CardTitle isHeading>
+              <CardLink elementType={NextLink} href="/components">
+                Components
+              </CardLink>
+            </CardTitle>
+            <p>
+              Our components are collection of interface elements that can be reused across the Spirit Design System.
+            </p>
+          </CardBody>
+          <CardFooter alignmentX="right">
+            <Link elementType={NextLink} href="/components" color="primary">
+              See all components
+            </Link>
+          </CardFooter>
+        </Card>
+      </Grid>
+    </Section>
+  </Container>
 );
 
 export default Home;
