7445171fd3aa4b167696638691e971377dfe7b60
literat <tomas@litera.me>
2025-06-04 15:48:03 +0200

    Feat(website): Introduce multiple views over one component

---
+++ apps/website/src/app/(documentation)/components/[component]/@views/web-preview/page.tsx
@@ -0,0 +1,12 @@
+import { Container, TabPane } from '@lmc-eu/spirit-web-react';
+import React from 'react';
+
+const WebPreviewTabPage = async () => {
+  return (
+    <TabPane id="web-preview">
+      <Container>web preview</Container>
+    </TabPane>
+  );
+};
+
+export default WebPreviewTabPage;
---
+++ apps/website/src/app/(documentation)/components/[component]/@views/web/page.tsx
@@ -0,0 +1,12 @@
+import { Container, TabPane } from '@lmc-eu/spirit-web-react';
+import React from 'react';
+
+const WebTabPage = async () => {
+  return (
+    <TabPane id="web">
+      <Container>web</Container>
+    </TabPane>
+  );
+};
+
+export default WebTabPage;
