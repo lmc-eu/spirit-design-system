127843248c48d7871634700a649fab6f7d62e00e
Kateřina Řehořková <149075264+katerinarehorkova@users.noreply.github.com>
2025-06-06 14:16:30 +0200

    Refactor(web-react): Update demo emotion, accent colors in `Heading` #DS-1774

--- packages/web-react/src/components/Heading/demo/HeadingTextColor.tsx
+++ packages/web-react/src/components/Heading/demo/HeadingTextColor.tsx
@@ -1,27 +1,41 @@
 import React from 'react';
-import DocsBox from '../../../../docs/DocsBox';
+import DocsStack from '../../../../docs/DocsStack';
 import { TextColors } from '../../../constants';
+import { HeadingColorsType } from '../../../types';
 import { getAccentTextColors, getEmotionTextColors } from '../../../utils/colorObjectGenerators';
+import { Grid } from '../../Grid';
+import Text from '../../Text/Text';
 import Heading from '../Heading';
 
-const accentColorsObject = getAccentTextColors();
-const emotionColorsObject = getEmotionTextColors();
+const colorList = {
+  text: Object.values(TextColors),
+  accent: Object.values(getAccentTextColors()),
+  emotion: Object.values(getEmotionTextColors()),
+};
 
 const HeadingTextColor = () => (
   <>
-    {Object.values(TextColors).map((textColor) => (
-      <Heading elementType="h2" key={textColor} textColor={textColor}>
-        Heading {textColor}
-      </Heading>
-    ))}
+    <Text>For demo purposes, the heading has custom size, background and padding.</Text>
+    <Grid cols={{ mobile: 1, desktop: 3 }} alignmentY="top" spacingY="space-1100">
+      {Object.entries(colorList).map(([key, colors]: [string, HeadingColorsType[]]) => (
+        <DocsStack key={`heading-${key}`} stackAlignment="start">
+          <h3>{`${key.charAt(0).toUpperCase()}${key.slice(1)} `} colors</h3>
 
-    {Object.values([...Object.values(accentColorsObject), ...Object.values(emotionColorsObject)]).map((textColor) => (
-      <DocsBox key={textColor}>
-        <Heading elementType="h2" textColor={textColor}>
-          Heading {textColor}
-        </Heading>
-      </DocsBox>
-    ))}
+          {colors?.map((color) => {
+            const bgColor = color?.replace(/basic|subtle/, (match) => (match === 'basic' ? 'subtle' : 'basic'));
+            const boxClass = key !== 'text' ? `bg-${bgColor} p-800` : 'mb-800';
+
+            return (
+              <div key={`heading-item-${color}`} className={boxClass}>
+                <Heading elementType="h2" textColor={color} size="xsmall">
+                  Heading {color}
+                </Heading>
+              </div>
+            );
+          })}
+        </DocsStack>
+      ))}
+    </Grid>
   </>
 );
 
