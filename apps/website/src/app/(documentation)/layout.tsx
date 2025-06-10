e2c446ea4934208e1ebedb7963aa04bcbe6a3f3e
literat <tomas@litera.me>
2025-06-10 21:35:35 +0200

    Feat(website): Create component cover with breadcrumbs

--- apps/website/src/app/(documentation)/layout.tsx
+++ apps/website/src/app/(documentation)/layout.tsx
@@ -1,19 +1,17 @@
 'use client';
 
-import React, { ReactNode } from 'react';
 import { Container, Footer } from '@lmc-eu/spirit-web-react';
+import ComponentCover from '@local/features/components/ui/ComponentCover';
 import useIsPage from '@local/hooks/useIsPage';
-import { Header } from '@local/ui/Header';
-import { Cover } from '@local/ui/Cover';
 import clsx from 'clsx';
+import { ReactNode } from 'react';
 
 const DocumentationLayout = ({ children }: { children: ReactNode }) => {
   const isComponentsPage = useIsPage('components');
 
   return (
     <>
-      <Header />
-      <Cover />
+      <ComponentCover isUnstable={false} />
       <main className="py-1100 pt-tablet-1600">{children}</main>
       <Footer
         UNSAFE_className={clsx('bg-secondary mt-1200 mt-tablet-1700 pb-1200 text-center', {
