55f8a6a0cf7493038cf25cf947979f7e67a1d345
literat <tomas@litera.me>
2025-06-10 21:35:35 +0200

    Feat(website): Create component cover with breadcrumbs

--- apps/website/src/lib/helpers.ts
+++ apps/website/src/lib/helpers.ts
@@ -1,3 +1,10 @@
-export const ucFirst = (str: string): string => {
-  return str.charAt(0).toUpperCase() + str.slice(1);
+export const ucFirst = (value: string): string => {
+  return value.charAt(0).toUpperCase() + value.slice(1);
+};
+
+export const ucFirstAll = (value: string, delimiter: string = '-'): string => {
+  return value
+    .split(delimiter)
+    .map((word: string) => ucFirst(word))
+    .join(delimiter);
 };
