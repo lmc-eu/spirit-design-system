3e82e37357c99e30be94780ab6db9060d13bb219
literat <tomas@litera.me>
2025-06-10 22:09:45 +0200

    Feat(docsite): Normalize component name on page

--- apps/website/src/lib/constants.ts
+++ apps/website/src/lib/constants.ts
@@ -1,1 +1,17 @@
 export const COMPONENT_SEGMENTS = ['design', 'web', 'react', 'web-preview', 'react-preview'];
+
+export const COMPONENT_NAME_SEGMENTS = [
+  'control',
+  'button',
+  'link',
+  'hidden',
+  'text',
+  'field',
+  'base',
+  'group',
+  'state',
+  'logo',
+  'icon',
+  'ssr',
+  'area',
+];
