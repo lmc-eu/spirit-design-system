e70717df8ab42867bd9347764296b7beb292d232
literat <tomas@litera.me>
2025-06-10 22:09:45 +0200

    Feat(website): Normalize component name on page

--- apps/website/src/lib/helpers.ts
+++ apps/website/src/lib/helpers.ts
@@ -8,3 +8,19 @@ export const ucFirstAll = (value: string, delimiter: string = '-'): string => {
     .map((word: string) => ucFirst(word))
     .join(delimiter);
 };
+
+export const ucAll = (value: string): string => {
+  return value.toUpperCase();
+};
+
+export const normalizeComponentName = (componentName: string): string => {
+  let normalizedComponentName = componentName;
+
+  if (normalizedComponentName.includes('_')) {
+    const [firstPart, rest] = componentName.split('_');
+
+    normalizedComponentName = `${ucAll(firstPart)}_${ucFirst(rest)}`;
+  }
+
+  return ucFirstAll(normalizedComponentName);
+};
