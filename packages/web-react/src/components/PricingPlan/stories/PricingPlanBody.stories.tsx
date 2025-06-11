74bc9b6870a273ae88a52cb057dd4c3d9b8333b6
Jakub Čurda <jakub.curda@almacareer.com>
2025-06-11 13:34:58 +0200

    fixup! Feat(web-react): Introduce PricingPlan component #DS-1953

--- packages/web-react/src/components/PricingPlan/stories/PricingPlanBody.stories.tsx
+++ packages/web-react/src/components/PricingPlan/stories/PricingPlanBody.stories.tsx
@@ -1,18 +1,11 @@
 import { Markdown } from '@storybook/blocks';
 import type { Meta, StoryObj } from '@storybook/react';
 import React from 'react';
-import { ButtonLink } from '../../ButtonLink';
 import PricingPlan from '../PricingPlan';
 import PricingPlanBody from '../PricingPlanBody';
+import PricingPlanFooter from '../PricingPlanFooter';
 import PricingPlanHeader from '../PricingPlanHeader';
 import ReadMe from '../README.md';
-
-const featureSingle = [
-  {
-    title: 'Feature name',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-  },
-];
 
 const meta: Meta<typeof PricingPlanBody> = {
   title: 'Components/PricingPlan',
@@ -38,7 +31,16 @@ const meta: Meta<typeof PricingPlanBody> = {
   },
   args: {
     description: 'This is a description for the pricing plan body:',
-    features: featureSingle, // nebo featureMultiple, podle toho co chceš jako default
+    features: [
+      {
+        title: 'Feature name',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      },
+      {
+        title: 'Feature name 2',
+        description: 'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      },
+    ],
   },
 };
 
@@ -49,8 +51,9 @@ export const PricingPlanBodyPlayground: Story = {
   name: 'PricingPlanBody',
   render: (args) => (
     <PricingPlan>
-      <PricingPlanHeader title="Example Header Title" />
+      <PricingPlanHeader title="Example Header" />
       <PricingPlanBody {...args} />
+      <PricingPlanFooter>Example Footer</PricingPlanFooter>
     </PricingPlan>
   ),
 };
