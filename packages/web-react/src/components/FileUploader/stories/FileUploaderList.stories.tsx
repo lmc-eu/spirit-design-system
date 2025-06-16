5d8ca775c356736895efcf9e10a8ccc4d5d9c5ab
literat <tomas@litera.me>
2025-06-16 10:01:24 +0200

    Refactor(web-react): Migrating stories to Storybook v9
      * import types from used storybook framework

--- packages/web-react/src/components/FileUploader/stories/FileUploaderList.stories.tsx
+++ packages/web-react/src/components/FileUploader/stories/FileUploaderList.stories.tsx
@@ -1,4 +1,4 @@
-import type { Meta, StoryObj } from '@storybook/react';
+import type { Meta, StoryObj } from '@storybook/react-vite';
 import React from 'react';
 import { type SpiritFileUploaderAttachmentProps, type SpiritFileUploaderListProps } from '../../../types';
 import { FileUploader, FileUploaderAttachment, FileUploaderInput, FileUploaderList, useFileQueue } from '..';
--- packages/web-react/src/components/Heading/stories/Heading.stories.tsx
+++ packages/web-react/src/components/Heading/stories/Heading.stories.tsx
@@ -1,5 +1,5 @@
-import { Markdown } from '@storybook/blocks';
-import type { Meta, StoryObj } from '@storybook/react';
+import { Markdown } from '@storybook/addon-docs/blocks';
+import type { Meta, StoryObj } from '@storybook/react-vite';
 import React from 'react';
 import { Emphasis, SizesExtended, TextAlignments, TextColors, TextHyphens, TextWordBreaks } from '../../../constants';
 import { getAccentTextColors, getEmotionTextColors } from '../../../utils';
