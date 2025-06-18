a913ef4101ca450f46d03f235a2889f4fdbfdca3
literat <tomas@litera.me>
2025-06-18 14:10:59 +0200

    Test(web-react): Colors are represented in the rgb using jest dom
      * it is the same way as the browsers do

--- packages/web-react/src/components/Box/__tests__/Box.test.tsx
+++ packages/web-react/src/components/Box/__tests__/Box.test.tsx
@@ -123,7 +123,7 @@ describe('Box', () => {
   it('should render with background color', () => {
     render(<Box backgroundColor="primary">Content</Box>);
 
-    expect(screen.getByText('Content')).toHaveStyle('background-color: var(--color-primary)');
+    expect(screen.getByText('Content')).toHaveStyle('background-color: rgb(0, 0, 0, 0)');
   });
 
   it('should render with border radius and width', () => {
