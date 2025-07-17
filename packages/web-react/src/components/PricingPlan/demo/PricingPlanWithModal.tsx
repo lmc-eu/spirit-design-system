0a893aa514da5911cb3808d052d367c574610662
Jan Kryšpín <crishpeen@gmail.com>
2025-07-17 16:19:29 +0200

    Feat(web,web-react): Introduce Modal additional info to PricingPlan Features #DS-2050

---
+++ packages/web-react/src/components/PricingPlan/demo/PricingPlanWithModal.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { ButtonLink } from '../../ButtonLink';
+import PricingPlan from '../PricingPlan';
+import PricingPlanBody from '../PricingPlanBody';
+import PricingPlanFooter from '../PricingPlanFooter';
+import PricingPlanHeader from '../PricingPlanHeader';
+
+const PricingPlanWithModal = () => (
+  <PricingPlan>
+    <PricingPlanHeader
+      action={
+        <ButtonLink href="#" size="large">
+          Get started
+        </ButtonLink>
+      }
+      title="Plan 1"
+      subtitle="Supporting text or message"
+      price="39 EUR"
+      note="Another supporting text or message"
+    />
+    <PricingPlanBody
+      features={[
+        {
+          title: 'Feature name',
+          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+          modalContent: (
+            <>
+              {[...Array(20)].map((_, index) => {
+                const key = `paragraph-${index}`;
+
+                return <p key={key}>This is a really long text that should be wrapped in a paragraph.</p>;
+              })}
+            </>
+          ),
+        },
+      ]}
+    />
+    <PricingPlanFooter>footer</PricingPlanFooter>
+  </PricingPlan>
+);
+
+export default PricingPlanWithModal;
