250557369915c9870e1d37879bed4978a1939e30
Pavel Klibani <pavel.klibani@almamedia.com>
2025-08-04 13:29:43 +0200

    Feat(icons): Support build of dualtones icons #DS-1906
    - Build script for icons now supports dualtone icons

--- packages/icons/scripts/buildSvg.js
+++ packages/icons/scripts/buildSvg.js
@@ -1,12 +1,36 @@
 const fs = require('fs');
 const path = require('path');
-const { filterSvgFiles } = require('./shared');
+const { cssVariablePrefix } = require('@lmc-eu/spirit-design-tokens');
+const { filterSvgFiles, getIconType, ICON_TYPE_DUALTONE, ICON_TYPE_COLORED } = require('./shared');
 
 const svgSrcDir = path.resolve(__dirname, '../src/svg');
 const svgDistDir = path.resolve(__dirname, '../dist/svg');
 
-const normalizeSvgColors = (fileName, svgContent) =>
-  fileName.endsWith('-colored.svg') ? svgContent : svgContent.replace(/fill="#\w+"/g, 'fill="currentColor"');
+const DUALTONE_COLOR_BACKGROUND_DEFAULT = '#F2F2F2'; // Gray95
+const DUALTONE_COLOR_BORDER_DEFAULT = '#202020'; // DarkGray
+
+const normalizeSvgColors = (fileName, svgContent) => {
+  const iconType = getIconType(fileName);
+
+  switch (iconType) {
+    case ICON_TYPE_DUALTONE:
+      return svgContent
+        .replace(
+          new RegExp(DUALTONE_COLOR_BACKGROUND_DEFAULT, 'g'),
+          `var(--${cssVariablePrefix}icon-dualtone-color-background, ${DUALTONE_COLOR_BACKGROUND_DEFAULT})`,
+        )
+        .replace(
+          new RegExp(DUALTONE_COLOR_BORDER_DEFAULT, 'g'),
+          `var(--${cssVariablePrefix}icon-dualtone-color-border, ${DUALTONE_COLOR_BORDER_DEFAULT})`,
+        );
+
+    case ICON_TYPE_COLORED:
+      return svgContent;
+
+    default:
+      return svgContent.replace(/fill="#\w+"/g, 'fill="currentColor"');
+  }
+};
 
 const normalizeAndCopySvg = (srcDir, distDir) => {
   fs.readdir(srcDir, (err, files) => {
