61f5a61a04e4b7737b1571330c43ae79a265ee3d
Kateřina Řehořková <149075264+katerinarehorkova@users.noreply.github.com>
2025-08-13 12:35:24 +0200

    Fix(exporter-tokens): Fix missing breakpoint map for devices #DS-2103

--- exporters/tokens/tests/fixtures/exampleDeviceTokens.ts
+++ exporters/tokens/tests/fixtures/exampleDeviceTokens.ts
@@ -39,11 +39,69 @@ const testToken = {
   propertyValues: { collection: 'device-tokens-id' },
 };
 
+const testTokenBreakpoint = {
+  name: 'breakpoint',
+  tokenType: TokenType.dimension,
+  parentGroupId: '0',
+  origin: {
+    name: 'breakpoint',
+  },
+  tokenPath: ['Measure'],
+  value: {
+    unit: 'Pixels',
+    measure: 0,
+    referencedTokenId: null,
+  },
+  properties: [
+    {
+      name: 'Collection',
+      options: [
+        {
+          id: 'theme-tokens-id',
+          name: 'Theme tokens',
+        },
+        {
+          id: 'primitives-id',
+          name: 'Primitives',
+        },
+        {
+          id: 'global-tokens-id',
+          name: 'Global tokens',
+        },
+        {
+          id: 'device-tokens-id',
+          name: 'Device',
+        },
+      ],
+    },
+  ],
+  propertyValues: {
+    collection: 'device-tokens-id',
+    device: 'mobile',
+  },
+};
+
 const testTokenDeviceUpdated = {
   ...testToken,
   propertyValues: {
     collection: 'device-tokens-id',
     device: 'mobile',
+  },
+};
+
+const testTokenBreakpointUpdated = {
+  ...testTokenBreakpoint,
+  name: 'breakpoint-test',
+  origin: {
+    name: 'breakpoint-test',
+  },
+};
+
+const testTokenDeviceBreakpointCustom = {
+  ...testTokenBreakpoint,
+  propertyValues: {
+    collection: 'device-tokens-id',
+    device: 'Mobile-Custom',
   },
 };
 
@@ -56,3 +114,12 @@ export const exampleDeviceUpdatedTokens = new Map<string, Token>();
 exampleDeviceUpdatedTokens.set('stringRef1', testTokenDeviceUpdated as unknown as DimensionToken);
 exampleDeviceUpdatedTokens.set('stringRef2', testTokenDeviceUpdated as unknown as DimensionToken);
 exampleDeviceUpdatedTokens.set('stringRef3', testTokenDeviceUpdated as unknown as DimensionToken);
+
+export const exampleDeviceTokenBreakpointCustom = new Map<string, Token>();
+exampleDeviceTokenBreakpointCustom.set('customRef1', testTokenDeviceBreakpointCustom as unknown as DimensionToken);
+
+export const exampleDeviceTokenBreakpoint = new Map<string, Token>();
+exampleDeviceTokenBreakpoint.set('breakpointRef1', testTokenBreakpoint as unknown as DimensionToken);
+
+export const exampleDeviceTokenMultiWord = new Map<string, Token>();
+exampleDeviceTokenMultiWord.set('multipleRef1', testTokenBreakpointUpdated as unknown as DimensionToken);
