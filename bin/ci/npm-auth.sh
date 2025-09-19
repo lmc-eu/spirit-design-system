2a33d17e2de14d5207fc2ec8fe49c78451329a40
literat <tomas@litera.me>
2025-09-19 16:16:26 +0200

    Revert(ci): Publishing using automation token until Lerna supports it

--- bin/ci/npm-auth.sh
+++ bin/ci/npm-auth.sh
@@ -2,7 +2,18 @@
 
 set -o errexit
 
-# Authorise npm for publishing
+# Lerna always uses npm to publish packages. If you use a package manager other than npm,
+# you will need to still add the appropriate publishing configuration to .npmrc,
+# even if npmClient is set to something other than npm in lerna.json.
+# @see: https://lerna.js.org/docs/features/version-and-publish#from-package
+
+# @TODO: remove automation token when Lerna adds support for OIDC and trusted publishing
+# @see: https://github.com/lerna/lerna/issues/4219
+
+# Authorize npm for publishing
 cat <<NPMRC >> .npmrc
 @lmc-eu:registry=https://registry.npmjs.org/
+//registry.npmjs.org/:_authToken=${NPM_TOKEN}
 NPMRC
+
+npm whoami
