a9870b1a4630f3b278e5cbb219116a53c0f7870e
literat <tomas@litera.me>
2025-09-19 13:52:54 +0200

    Chore(ci): Set back establishing of .npmrc while publishing

--- bin/ci/npm-auth.sh
+++ bin/ci/npm-auth.sh
@@ -5,7 +5,6 @@ set -o errexit
 # Authorise npm for publishing
 cat <<NPMRC >> .npmrc
 @lmc-eu:registry=https://registry.npmjs.org/
-//registry.npmjs.org/:_authToken=${NPM_TOKEN}
 NPMRC
 
 npm whoami
