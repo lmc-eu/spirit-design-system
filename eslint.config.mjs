7a94d502dc379011077c42137f580b96ded02eac
literat <tomas@litera.me>
2025-10-01 21:22:08 +0200

    Style(repo): Migrate root ESLint config to flat config style
      * remove legacy ESLint config file
      * remove legacy ESLint ignore file

---
+++ eslint.config.mjs
@@ -0,0 +1,62 @@
+import { fixupConfigRules, fixupPluginRules } from '@eslint/compat';
+import { FlatCompat } from '@eslint/eslintrc';
+import importPlugin from 'eslint-plugin-import';
+
+const compat = new FlatCompat({
+  baseDirectory: import.meta.dirname,
+});
+
+export default [
+  {
+    // # .eslintignore
+    ignores: [
+      // # NOTE:
+      // # The following directives are only relevant when linting the whole
+      // # project directory, ie. running `eslint .` ⚠️
+      // # If you compile JavaScript into some output folder, exclude it here
+
+      // Generated files or folders
+      '**/node_modules/**',
+      '**/dist/**',
+      '**/build/**',
+      '**/.nx/**',
+      '**/.yarn/**',
+      // Ignore the playwright-report directory
+      'playwright-report',
+      // Ignore vendor directory in `packages/web-twig`
+      '**/vendor/**',
+
+      // Internal output folders
+      'static',
+
+      // Skip packages that don’t need linting from root
+      'examples/*',
+      'exporters/js',
+      'exporters/scss',
+      'exporters/tokens',
+      'packages/analytics',
+      'packages/codemods',
+      'packages/common',
+      'packages/design-tokens',
+      'packages/form-validations',
+      'packages/web',
+      'packages/web-react',
+      'scripts',
+
+      // # Highly recommended to re-include JavaScript dotfiles to lint them
+      // # (This will cause .eslintrc.js to be linted by ESLint 🤘)
+      '!.*.js',
+
+      // # Some tools use this pattern for their configuration files. Lint them!
+      '!*.config.js',
+    ],
+  },
+  {
+    // @TODO: remove `files` and `plugins` when all configs are flat
+    files: ['**/*.{js,mjs,cjs}'],
+    plugins: {
+      import: fixupPluginRules(importPlugin),
+    },
+    ...fixupConfigRules(compat.extends('eslint-config-spirit'))[0],
+  },
+];
