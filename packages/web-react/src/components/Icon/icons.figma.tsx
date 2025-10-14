49efa266a2506f3bffc3a31cbb078af76355a3df
Jan Kryšpín <crishpeen@gmail.com>
2025-10-14 16:26:37 +0200

    Feat(web-react): Introduce Figma Code Connect #DS-2192

---
+++ packages/web-react/src/components/Icon/icons.figma.tsx
@@ -0,0 +1,347 @@
+import figma from '@figma/code-connect';
+import React from 'react';
+import Icon from './Icon';
+
+// add
+figma.connect(Icon, '<FIGMA_FILE_ID>?node-id=5920-3615', {
+  props: {
+    name: 'add',
+  },
+  example: ({ name }) => <Icon name={name} />,
+});
+
+// bullet-outline
+figma.connect(Icon, '<FIGMA_FILE_ID>?node-id=31224-87', {
+  props: {
+    name: 'bullet-outline',
+  },
+  example: ({ name }) => <Icon name={name} />,
+});
+
+// bullet
+figma.connect(Icon, '<FIGMA_FILE_ID>?node-id=31224-86', {
+  props: {
+    name: 'bullet',
+  },
+  example: ({ name }) => <Icon name={name} />,
+});
+
+// check-plain
+figma.connect(Icon, '<FIGMA_FILE_ID>?node-id=5920-3621', {
+  props: {
+    name: 'check-plain',
+  },
+  example: ({ name }) => <Icon name={name} />,
+});
+
+// chevron-down
+figma.connect(Icon, '<FIGMA_FILE_ID>?node-id=5920-3633', {
+  props: {
+    name: 'chevron-down',
+  },
+  example: ({ name }) => <Icon name={name} />,
+});
+
+// chevron-left
+figma.connect(Icon, '<FIGMA_FILE_ID>?node-id=5920-3624', {
+  props: {
+    name: 'chevron-left',
+  },
+  example: ({ name }) => <Icon name={name} />,
+});
+
+// chevron-right
+figma.connect(Icon, '<FIGMA_FILE_ID>?node-id=5920-3627', {
+  props: {
+    name: 'chevron-right',
+  },
+  example: ({ name }) => <Icon name={name} />,
+});
+
+// chevron-up
+figma.connect(Icon, '<FIGMA_FILE_ID>?node-id=5920-3630', {
+  props: {
+    name: 'chevron-up',
+  },
+  example: ({ name }) => <Icon name={name} />,
+});
+
+// close
+figma.connect(Icon, '<FIGMA_FILE_ID>?node-id=5920-3618', {
+  props: {
+    name: 'close',
+  },
+  example: ({ name }) => <Icon name={name} />,
+});
+
+// danger
+figma.connect(Icon, '<FIGMA_FILE_ID>?node-id=5920-3663', {
+  props: {
+    name: 'danger',
+  },
+  example: ({ name }) => <Icon name={name} />,
+});
+
+// download
+figma.connect(Icon, '<FIGMA_FILE_ID>?node-id=11847-9707', {
+  props: {
+    name: 'download',
+  },
+  example: ({ name }) => <Icon name={name} />,
+});
+
+// edit
+figma.connect(Icon, '<FIGMA_FILE_ID>?node-id=13545-10470', {
+  props: {
+    name: 'edit',
+  },
+  example: ({ name }) => <Icon name={name} />,
+});
+
+// file
+figma.connect(Icon, '<FIGMA_FILE_ID>?node-id=11847-9772', {
+  props: {
+    name: 'file',
+  },
+  example: ({ name }) => <Icon name={name} />,
+});
+
+// folder-dualtone
+figma.connect(Icon, '<FIGMA_FILE_ID>?node-id=32935-1175', {
+  props: {
+    name: 'folder-dualtone',
+  },
+  example: ({ name }) => <Icon name={name} />,
+});
+
+// hamburger
+figma.connect(Icon, '<FIGMA_FILE_ID>?node-id=5920-3650', {
+  props: {
+    name: 'hamburger',
+  },
+  example: ({ name }) => <Icon name={name} />,
+});
+
+// help
+figma.connect(Icon, '<FIGMA_FILE_ID>?node-id=5920-3648', {
+  props: {
+    name: 'help',
+  },
+  example: ({ name }) => <Icon name={name} />,
+});
+
+// info
+figma.connect(Icon, '<FIGMA_FILE_ID>?node-id=5920-3649', {
+  props: {
+    name: 'info',
+  },
+  example: ({ name }) => <Icon name={name} />,
+});
+
+// link
+figma.connect(Icon, '<FIGMA_FILE_ID>?node-id=5888-3640', {
+  props: {
+    name: 'link',
+  },
+  example: ({ name }) => <Icon name={name} />,
+});
+
+// lock
+figma.connect(Icon, '<FIGMA_FILE_ID>?node-id=33631-145', {
+  props: {
+    name: 'lock',
+  },
+  example: ({ name }) => <Icon name={name} />,
+});
+
+// logo-apple-colored
+figma.connect(Icon, '<FIGMA_FILE_ID>?node-id=18856-34441', {
+  props: {
+    name: 'logo-apple-colored',
+  },
+  example: ({ name }) => <Icon name={name} />,
+});
+
+// logo-apple
+figma.connect(Icon, '<FIGMA_FILE_ID>?node-id=18856-34442', {
+  props: {
+    name: 'logo-apple',
+  },
+  example: ({ name }) => <Icon name={name} />,
+});
+
+// logo-facebook-colored
+figma.connect(Icon, '<FIGMA_FILE_ID>?node-id=16444-956', {
+  props: {
+    name: 'logo-facebook-colored',
+  },
+  example: ({ name }) => <Icon name={name} />,
+});
+
+// logo-facebook
+figma.connect(Icon, '<FIGMA_FILE_ID>?node-id=16444-955', {
+  props: {
+    name: 'logo-facebook',
+  },
+  example: ({ name }) => <Icon name={name} />,
+});
+
+// logo-google-colored
+figma.connect(Icon, '<FIGMA_FILE_ID>?node-id=16441-195', {
+  props: {
+    name: 'logo-google-colored',
+  },
+  example: ({ name }) => <Icon name={name} />,
+});
+
+// logo-google
+figma.connect(Icon, '<FIGMA_FILE_ID>?node-id=16441-193', {
+  props: {
+    name: 'logo-google',
+  },
+  example: ({ name }) => <Icon name={name} />,
+});
+
+// logo-linkedin-colored
+figma.connect(Icon, '<FIGMA_FILE_ID>?node-id=16444-960', {
+  props: {
+    name: 'logo-linkedin-colored',
+  },
+  example: ({ name }) => <Icon name={name} />,
+});
+
+// logo-linkedin
+figma.connect(Icon, '<FIGMA_FILE_ID>?node-id=16444-959', {
+  props: {
+    name: 'logo-linkedin',
+  },
+  example: ({ name }) => <Icon name={name} />,
+});
+
+// logo-x-colored
+figma.connect(Icon, '<FIGMA_FILE_ID>?node-id=16444-958', {
+  props: {
+    name: 'logo-x-colored',
+  },
+  example: ({ name }) => <Icon name={name} />,
+});
+
+// logo-x
+figma.connect(Icon, '<FIGMA_FILE_ID>?node-id=16444-957', {
+  props: {
+    name: 'logo-x',
+  },
+  example: ({ name }) => <Icon name={name} />,
+});
+
+// logo-youtube-colored
+figma.connect(Icon, '<FIGMA_FILE_ID>?node-id=16340-414', {
+  props: {
+    name: 'logo-youtube-colored',
+  },
+  example: ({ name }) => <Icon name={name} />,
+});
+
+// logo-youtube
+figma.connect(Icon, '<FIGMA_FILE_ID>?node-id=16340-415', {
+  props: {
+    name: 'logo-youtube',
+  },
+  example: ({ name }) => <Icon name={name} />,
+});
+
+// more
+figma.connect(Icon, '<FIGMA_FILE_ID>?node-id=5920-3651', {
+  props: {
+    name: 'more',
+  },
+  example: ({ name }) => <Icon name={name} />,
+});
+
+// placeholder
+figma.connect(Icon, '<FIGMA_FILE_ID>?node-id=33631-6434', {
+  props: {
+    name: 'placeholder',
+  },
+  example: ({ name }) => <Icon name={name} />,
+});
+
+// profile
+figma.connect(Icon, '<FIGMA_FILE_ID>?node-id=5920-3652', {
+  props: {
+    name: 'profile',
+  },
+  example: ({ name }) => <Icon name={name} />,
+});
+
+// reload
+figma.connect(Icon, '<FIGMA_FILE_ID>?node-id=5920-3653', {
+  props: {
+    name: 'reload',
+  },
+  example: ({ name }) => <Icon name={name} />,
+});
+
+// search
+figma.connect(Icon, '<FIGMA_FILE_ID>?node-id=5920-3654', {
+  props: {
+    name: 'search',
+  },
+  example: ({ name }) => <Icon name={name} />,
+});
+
+// shield-dualtone
+figma.connect(Icon, '<FIGMA_FILE_ID>?node-id=32935-1174', {
+  props: {
+    name: 'shield-dualtone',
+  },
+  example: ({ name }) => <Icon name={name} />,
+});
+
+// spinner
+figma.connect(Icon, '<FIGMA_FILE_ID>?node-id=11441-8832', {
+  props: {
+    name: 'spinner',
+  },
+  example: ({ name }) => <Icon name={name} />,
+});
+
+// success
+figma.connect(Icon, '<FIGMA_FILE_ID>?node-id=27435-4350', {
+  props: {
+    name: 'success',
+  },
+  example: ({ name }) => <Icon name={name} />,
+});
+
+// upload
+figma.connect(Icon, '<FIGMA_FILE_ID>?node-id=11847-9706', {
+  props: {
+    name: 'upload',
+  },
+  example: ({ name }) => <Icon name={name} />,
+});
+
+// visibility-off
+figma.connect(Icon, '<FIGMA_FILE_ID>?node-id=5920-3662', {
+  props: {
+    name: 'visibility-off',
+  },
+  example: ({ name }) => <Icon name={name} />,
+});
+
+// visibility-on
+figma.connect(Icon, '<FIGMA_FILE_ID>?node-id=5920-3661', {
+  props: {
+    name: 'visibility-on',
+  },
+  example: ({ name }) => <Icon name={name} />,
+});
+
+// warning
+figma.connect(Icon, '<FIGMA_FILE_ID>?node-id=17261-10794', {
+  props: {
+    name: 'warning',
+  },
+  example: ({ name }) => <Icon name={name} />,
+});
