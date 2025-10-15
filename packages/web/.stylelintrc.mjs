f64740a39594e36c07bd7d7ab36946ba9013eb80
literat <tomas@litera.me>
2025-10-15 14:48:48 +0200

    Style(web): Migrate ESLint config to flat config style

--- packages/web/.stylelintrc.mjs
+++ packages/web/.stylelintrc.mjs
@@ -1,3 +1,4 @@
+// eslint-disable-next-line import/no-extraneous-dependencies -- stylelint is a devDependency
 import stylelintConfig from 'stylelint-config-spirit';
 
 export default stylelintConfig;
