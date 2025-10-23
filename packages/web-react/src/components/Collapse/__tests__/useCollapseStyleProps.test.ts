c32da496accc0836a5828d6778bbcce0f26a0a77
Jan Kryšpín <crishpeen@gmail.com>
2025-10-23 12:51:01 +0200

    Feat(web-react): Allow inline Collapse #DS-2206

--- packages/web-react/src/components/Collapse/__tests__/useCollapseStyleProps.test.ts
+++ packages/web-react/src/components/Collapse/__tests__/useCollapseStyleProps.test.ts
@@ -1,14 +1,18 @@
 import { renderHook } from '@testing-library/react';
+import { type ElementType } from 'react';
 import { useCollapseStyleProps } from '../useCollapseStyleProps';
 
 describe('useCollapseStyleProps', () => {
   it.each([
-    [true, 'Collapse is-open'],
-    [false, 'Collapse'],
-  ])('should return defaults', (isCollapsed, rootClass) => {
-    const { result } = renderHook(() => useCollapseStyleProps(isCollapsed));
+    [true, 'Collapse is-open', 'div', '100px', { height: '100px' }],
+    [false, 'Collapse', 'div', '100px', { height: 0 }],
+    [true, 'Collapse is-open', 'span', undefined, {}],
+    [false, 'Collapse', 'span', undefined, {}],
+  ])('should return defaults', (isOpen, rootClass, elementType, collapseHeight, expectedStyleProps) => {
+    const { result } = renderHook(() => useCollapseStyleProps(isOpen, elementType as ElementType, collapseHeight));
 
     expect(result.current.classProps.root).toBe(rootClass);
     expect(result.current.classProps.content).toBe('Collapse__content');
+    expect(result.current.styleProps).toEqual(expectedStyleProps);
   });
 });
--- packages/web-react/src/components/Collapse/demo/CollapseHideTrigger.tsx
+++ packages/web-react/src/components/Collapse/demo/CollapseHideTrigger.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from '../../Link';
 import Collapse from '../Collapse';
 import { useCollapse } from '../useCollapse';
 
@@ -14,10 +15,14 @@ const CollapseHideTrigger = () => {
       auctor. Vel integer, placerat pulvinar nisi nunc donec felis erat quis, phasellus ac nec phasellus ac nibh.
       Scelerisque fringilla, viverra nisl vel purus sed porttitor porttitor et iaculis commodo, vehicula urna purus
       ultrices.{' '}
-      <a href="#" role="button" onClick={toggleHandler} aria-expanded={isOpen}>
-        <span className="accessibility-closed">… more</span>
-      </a>
-      <Collapse id="collapse-hide-trigger-id" isOpen={isOpen}>
+      {!isOpen ? (
+        <Link elementType="button" type="button" onClick={toggleHandler} aria-expanded={isOpen}>
+          … more
+        </Link>
+      ) : (
+        ' '
+      )}
+      <Collapse id="collapse-hide-trigger-id" elementType="span" isOpen={isOpen}>
         Commodo metus a lorem, a aliquet vestibulum rutrum pharetra sapien sed, ullamcorper quis odio dolor ut aliquam.
         Rutrum suspendisse, fermentum tellus metus a lorem cursus volutpat proin bibendum, sed diam a duis id dui et
         tempus. Ligula non, sapien augue libero eget aliquam semper varius, posuere urna leo vitae ullamcorper.
