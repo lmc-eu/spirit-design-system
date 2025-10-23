94d3f7982d66e6cb408f0a997978bdef713e91f8
Jan Kryšpín <crishpeen@gmail.com>
2025-10-23 12:51:01 +0200

    Feat(web-react): Allow inline Collapse #DS-2206

--- packages/web-react/src/components/Collapse/useCollapseStyleProps.ts
+++ packages/web-react/src/components/Collapse/useCollapseStyleProps.ts
@@ -1,4 +1,5 @@
 import classNames from 'classnames';
+import { type ElementType } from 'react';
 import { CLASS_NAME_OPEN } from '../../constants';
 import { useClassNamePrefix } from '../../hooks';
 
@@ -8,18 +9,31 @@ export interface CollapseStyles {
     root: string;
     content: string;
   };
+  /** style props */
+  styleProps?: {
+    height?: string | number;
+  };
 }
 
-export const useCollapseStyleProps = (isOpen: boolean): CollapseStyles => {
+export const useCollapseStyleProps = (
+  isOpen: boolean,
+  elementType: ElementType,
+  collapseHeight?: string,
+): CollapseStyles => {
   const collapseClass = useClassNamePrefix('Collapse');
   const collapseContentClass = `${collapseClass}__content`;
   const openClass = isOpen ? CLASS_NAME_OPEN : '';
   const rootClass = classNames(collapseClass, openClass);
+
+  // For span elements, don't use height-based transitions as they use display: none/inline
+  const isSpanElement = elementType === 'span';
+  const styleProps = isSpanElement ? {} : { height: isOpen ? collapseHeight : 0 };
 
   return {
     classProps: {
       root: rootClass,
       content: collapseContentClass,
     },
+    styleProps,
   };
 };
