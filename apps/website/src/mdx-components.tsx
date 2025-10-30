b3448723a0ede32fc2c3b98e87b4a58851b17a34
literat <tomas@litera.me>
2025-10-30 09:29:07 +0100

    Feat(website): Use Table component for markdown tables

--- apps/website/src/mdx-components.tsx
+++ apps/website/src/mdx-components.tsx
@@ -1,7 +1,13 @@
+import { UNSTABLE_Table } from '@lmc-eu/spirit-web-react';
 import type { MDXComponents } from 'mdx/types';
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
+    table: ({ children, ...props }) => (
+      <UNSTABLE_Table isBordered isStriped {...props}>
+        {children}
+      </UNSTABLE_Table>
+    ),
     ...components,
   };
 }
