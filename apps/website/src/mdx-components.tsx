4fff6b7ff5d2292bb771b7e196329ebd07c7af8b
literat <tomas@litera.me>
2025-10-30 09:37:52 +0100

    Feat(website): Apply Heading component to semantic html tags in markdown

--- apps/website/src/mdx-components.tsx
+++ apps/website/src/mdx-components.tsx
@@ -1,8 +1,38 @@
-import { UNSTABLE_Table } from '@lmc-eu/spirit-web-react';
+import { Heading, SizesExtended, UNSTABLE_Table } from '@lmc-eu/spirit-web-react';
 import type { MDXComponents } from 'mdx/types';
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
+    h1: ({ children, ...props }) => (
+      <Heading elementType="h1" size="xlarge" {...props}>
+        {children}
+      </Heading>
+    ),
+    h2: ({ children, ...props }) => (
+      <Heading elementType="h2" size="large" {...props}>
+        {children}
+      </Heading>
+    ),
+    h3: ({ children, ...props }) => (
+      <Heading elementType="h3" size="medium" {...props}>
+        {children}
+      </Heading>
+    ),
+    h4: ({ children, ...props }) => (
+      <Heading elementType="h4" size="small" {...props}>
+        {children}
+      </Heading>
+    ),
+    h5: ({ children, ...props }) => (
+      <Heading elementType="h5" size="xsmall" {...props}>
+        {children}
+      </Heading>
+    ),
+    h6: ({ children, ...props }) => (
+      <Heading elementType="h6" size="xsmall" {...props}>
+        {children}
+      </Heading>
+    ),
     table: ({ children, ...props }) => (
       <UNSTABLE_Table isBordered isStriped {...props}>
         {children}
