0ad48f5281469e9b131917a1f73311c91e8e87b5
literat <tomas@litera.me>
2025-11-01 16:10:36 +0100

    Style(web-react): Migrate ESLint config to flat config style

--- packages/web-react/src/types/item.ts
+++ packages/web-react/src/types/item.ts
@@ -1,5 +1,4 @@
 import { type ElementType, type JSXElementConstructor } from 'react';
-import type React from 'react';
 import { type StyleProps, type TransferProps } from './shared';
 
 export interface AriaItemElementTypeProps<T extends ElementType = 'button'> {
