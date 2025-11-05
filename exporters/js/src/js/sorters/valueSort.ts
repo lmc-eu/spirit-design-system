4a7411d2234f49f93c778d0e515be5e99754cee7
literat <tomas@litera.me>
2025-11-05 15:03:45 +0100

    BREAKING CHANGE(exporter-js): Abandon Supernova JS exporter
      * use tokens exporter instead

--- exporters/js/src/js/sorters/valueSort.ts
+++
@@ -1,7 +0,0 @@
-import type { Token } from '../index';
-
-export function valueSort(a: Token, b: Token) {
-  // Value is defined as `Record<string, unknown>;`
-  // @ts-expect-error TS2571: Object is of type 'unknown'.
-  return +a.value.text - +b.value.text;
-}
