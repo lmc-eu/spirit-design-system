c0426afa5c7eac8bf93dab759f0deaf1b51e76df
literat <tomas@litera.me>
2025-11-05 15:03:45 +0100

    BREAKING CHANGE(exporter-js): Abandon Supernova JS exporter
      * use tokens exporter instead

--- exporters/js/src/js/generators/__tests__/simple.test.ts
+++
@@ -1,9 +0,0 @@
-import { Token } from '../../index';
-import simpleTokens from '../__fixtures__/simpleTokens.json';
-import { generateSimple } from '../simple';
-
-describe('generateSimple', () => {
-  it.each([[simpleTokens]])('should generate simple output', (allTokens: Array<Token>) => {
-    expect(generateSimple('test', allTokens)).toMatchSnapshot();
-  });
-});
