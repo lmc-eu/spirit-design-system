a6def54f085a6e0996b5cde4a131b9ea84a021b6
Kateřina Dlouhá <katerina.dlouha@almamedia.com>
2025-12-17 13:05:02 +0100

    Feat(test): Introduce accessibility E2E tests #DS-2199

--- tests/helpers/getServerUrl.ts
+++ tests/helpers/getServerUrl.ts
@@ -1,5 +1,14 @@
 import { isTesting as isTestingEnvironment } from '@alma-oss/spirit-common/constants/environments';
 import { SERVERS, getDevelopmentEndpointUri } from '@alma-oss/spirit-common/constants/servers';
 
-export const getServerUrl = (packageName: string) =>
-  isTestingEnvironment() ? SERVERS.TESTING[packageName] : getDevelopmentEndpointUri(packageName, { isDocker: true });
+export const getServerUrl = (packageName: string) => {
+  if (isTestingEnvironment()) {
+    return SERVERS.TESTING[packageName];
+  }
+
+  // When running in Docker with host web server, use host.docker.internal
+  // Otherwise (running locally), use localhost
+  const isDocker = process.env.USE_DOCKER_HOST_WEBSERVER === 'true';
+
+  return getDevelopmentEndpointUri(packageName, { isDocker });
+};
