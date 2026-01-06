f5382970de21eda8f6a1c12cfffb274650fbf3ff
Jan Kryšpín <crishpeen@gmail.com>
2026-01-06 13:45:50 +0100

    Fix(web-react): Drop TransferProps from Footer #DS-2282

--- packages/web-react/src/types/footer.ts
+++ packages/web-react/src/types/footer.ts
@@ -4,14 +4,13 @@ import {
   type SpaceToken,
   type StyleProps,
   type TextAlignmentType,
-  type TransferProps,
 } from './shared';
 
-export interface FooterStyleProps {
+export interface FooterProps {
   paddingTop?: SpaceToken;
   paddingBottom?: SpaceToken;
   backgroundColor?: BackgroundColorsDictionaryType;
   textAlignment?: TextAlignmentType;
 }
 
-export interface SpiritFooterProps extends FooterStyleProps, ChildrenProps, StyleProps, TransferProps {}
+export interface SpiritFooterProps extends FooterProps, ChildrenProps, StyleProps {}
