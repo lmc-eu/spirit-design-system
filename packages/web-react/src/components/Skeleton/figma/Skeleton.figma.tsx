73dc6c34e00cfb611a3aae42f1915e0f9befb987
Kateřina Dlouhá <katerina.dlouha@almamedia.com>
2026-01-20 10:30:42 +0100

    Fix(web-react): Fix FCC with variants for Skeleton #DS-2343

--- packages/web-react/src/components/Skeleton/figma/Skeleton.figma.tsx
+++ packages/web-react/src/components/Skeleton/figma/Skeleton.figma.tsx
@@ -2,31 +2,28 @@ import figma from '@figma/code-connect';
 import React from 'react';
 import { SkeletonShape, SkeletonText } from '..';
 
-const SKELETON_NODE_URL = '<FIGMA_FILE_ID>?node-id=35661%3A27096';
+const SKELETON_NODE_URL = '<FIGMA_FILE_ID>?node-id=27632%3A5746';
 
 figma.connect(SkeletonText, SKELETON_NODE_URL, {
-  props: {
-    variant: {
-      Type: 'rectangle',
-    },
+  props: {},
+  variant: {
+    Type: 'Rectangle',
   },
   example: (props) => <SkeletonText {...props} />,
 });
 
 figma.connect(SkeletonShape, SKELETON_NODE_URL, {
-  props: {
-    variant: {
-      Type: 'circle',
-    },
+  props: {},
+  variant: {
+    Type: 'Circle',
   },
   example: (props) => <SkeletonShape width={58} height={58} borderRadius="full" {...props} />,
 });
 
 figma.connect(SkeletonShape, SKELETON_NODE_URL, {
-  props: {
-    variant: {
-      Type: 'square',
-    },
+  props: {},
+  variant: {
+    Type: 'Square',
   },
   example: (props) => <SkeletonShape width={58} height={58} {...props} />,
 });
