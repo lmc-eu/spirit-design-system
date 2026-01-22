09ea3e3fcff2f8eb3fbdd8ffaec0698400f1064b
Adam Kudrna <adam.kudrna@lmc.eu>
2026-01-22 11:22:46 +0100

    chore(web-react): delete unused `useDirectionClass` hook
    Random glancing over our hooks revealed there is some dead code.

--- packages/web-react/src/hooks/useDirectionClass.ts
+++
@@ -1,6 +0,0 @@
-import { type FlexDirectionType } from '../types';
-import { generateStylePropsClassNames } from '../utils';
-
-export function useDirectionClass(componentClass: string, property: FlexDirectionType) {
-  return generateStylePropsClassNames(componentClass, property);
-}
